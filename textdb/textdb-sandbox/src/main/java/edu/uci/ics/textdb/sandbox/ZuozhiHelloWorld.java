5b1a009448d967518073b84d5962e4dbe0b615d4
zuozhiw <zuozhiw@uci.edu>
2016-03-29 13:30:20 -0700

    really bad code

---
+++ textdb/textdb-sandbox/src/main/java/edu/uci/ics/textdb/sandbox/ZuozhiHelloWorld.java
@@ -0,0 +1,19 @@
+package edu.uci.ics.textdb.sandbox;
+
+/**
+ * Hello world!
+ *
+ */
+public class DummyHelloWorld
+{
+    public static void main( String[] args )
+    {
+        //Bad comments
+        System.out.println( "Zuozhi's bad Hello World!" );
+        System.out.println( "Waiting to be reviewed by prakul" );
+        // TOBE deleted
+        while (true){
+            System.out.println( "Stop me!" );
+        }
+    }
+}
