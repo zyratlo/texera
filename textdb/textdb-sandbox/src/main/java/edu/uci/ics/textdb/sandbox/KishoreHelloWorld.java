e5ca9095e730d89ade160d41a9d21ba475c2aaf8
Kishore Narendran <kishore.narendran09@gmail.com>
2016-03-29 16:47:59 -0700

    Added a bad hello world class

---
+++ textdb/textdb-sandbox/src/main/java/edu/uci/ics/textdb/sandbox/KishoreHelloWorld.java
@@ -0,0 +1,21 @@
+package edu.uci.ics.textdb.sandbox;
+
+/**
+ * Created by kishorenarendran on 29/03/16.
+ */
+public class KishoreHelloWorld {
+
+    public static void main(String args[]) {
+
+        //Really bad Hello World program
+        for(int i = 0; i < 1; i++) {
+            if(true) {
+                System.out.println("Hello, World!");
+            }
+        }
+
+        //Unecessary comments and assignments
+        int a = 10;
+        String c = new String();
+    }
+}
