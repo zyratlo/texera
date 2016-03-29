16d205ec534b8fdff54cc29ca3994bfaa261e327
Sandeep Reddy Madugula <reddy602.sandeep@gmail.com>
2016-03-28 23:55:13 -0700

    Added SandeepHelloWorld.java

---
+++ textdb/textdb-sandbox/src/main/java/edu/uci/ics/textdb/sandbox/SandeepHelloWorld.java
@@ -0,0 +1,12 @@
+package edu.uci.ics.textdb.sandbox;
+
+/**
+ * @author sandeepreddy602
+ *
+ */
+public class SandeepHelloWorld {
+	public static void main(String[] args) {
+		//Printing Hellow World! to console
+		System.out.println("Hello World!");
+	}
+}
