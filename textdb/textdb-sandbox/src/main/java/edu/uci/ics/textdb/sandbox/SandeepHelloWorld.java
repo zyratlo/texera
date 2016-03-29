ac0096972060ed2336923af38986688d7574fa0e
Sandeep Reddy Madugula <reddy602.sandeep@gmail.com>
2016-03-29 00:38:28 -0700

    Added the bad hello world code

--- textdb/textdb-sandbox/src/main/java/edu/uci/ics/textdb/sandbox/SandeepHelloWorld.java
+++ textdb/textdb-sandbox/src/main/java/edu/uci/ics/textdb/sandbox/SandeepHelloWorld.java
@@ -1,12 +1,18 @@
 package edu.uci.ics.textdb.sandbox;
 
 /**
- * @author sandeepreddy602
+ * Hello world!
  *
  */
-public class SandeepHelloWorld {
-	public static void main(String[] args) {
-		//Printing Hellow World! to console
-		System.out.println("Hello World!");
-	}
+public class SandeepHelloWorld
+{
+    public static void main( String[] args )
+    {
+        //Bad comments
+        System.out.println( "Hello World!" );
+        // TOBE deleted
+        while (true){
+            System.out.println( "Stop me!" );
+        }
+    }
 }
