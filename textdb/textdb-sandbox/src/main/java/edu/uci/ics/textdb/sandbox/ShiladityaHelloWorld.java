da8584a7c15934e922c768e88968427f77c795c3
shiladitya <shiladis@uci.edu>
2016-03-30 22:58:56 -0700

    Added bad HelloWorld.java

---
+++ textdb/textdb-sandbox/src/main/java/edu/uci/ics/textdb/sandbox/ShiladityaHelloWorld.java
@@ -0,0 +1,21 @@
+//package edu.uci.ics.textdb.sandbox;
+
+/**
+ * Hello world!
+ * Shiladitya Sen
+ * 03/30/2016
+ */
+public class ShiladityaHelloWorld
+{
+    public static void main( String[] args )
+    {
+        //Australia celebrates Christmas in summer
+
+        System.out.println( "Hello" );
+
+        if(args.length != 0)
+        	System.out.println("Command line arguments should not be passed");
+
+        System.out.println("World!");
+    }
+}
