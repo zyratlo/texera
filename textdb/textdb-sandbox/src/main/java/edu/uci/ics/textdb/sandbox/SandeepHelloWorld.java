45227f9f24646eb26aa472d7a803ff033357a059
Flavio Bayer <fbayer@uci.edu>
2016-04-01 13:55:38 -0700

    Hello World unfied again

--- textdb/textdb-sandbox/src/main/java/edu/uci/ics/textdb/sandbox/SandeepHelloWorld.java
+++ textdb/textdb-sandbox/src/main/java/edu/uci/ics/textdb/sandbox/SandeepHelloWorld.java
@@ -9,6 +9,6 @@ public class SandeepHelloWorld
     public static void main( String[] args )
     {
         //Printing Hello World! to console
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" ):
     }
 }
