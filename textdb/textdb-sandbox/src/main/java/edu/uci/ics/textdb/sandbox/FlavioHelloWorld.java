37a4c15e167cb34799bc16cd12a941933f9864c2
Flavio Bayer <fbayer@uci.edu>
2016-03-31 19:35:19 -0700

    My first Hello World test

---
+++ textdb/textdb-sandbox/src/main/java/edu/uci/ics/textdb/sandbox/FlavioHelloWorld.java
@@ -0,0 +1,14 @@
+package edu.uci.ics.textdb.sandbox;
+
+/**
+ * Hello world!
+ *
+ */
+public class DummyHelloWorld
+{
+    public static void main( String[] args )
+    {
+        //Just print Hello world
+        System.out.println( "Hello World!" ):
+    }
+}
