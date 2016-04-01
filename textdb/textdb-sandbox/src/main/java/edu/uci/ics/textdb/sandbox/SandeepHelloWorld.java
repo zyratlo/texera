5f0d4be6a7b39602934c0deaeac869a3ad4161db
zuozhi <zuozhiw@gmail.com>
2016-04-01 14:06:19 -0700

    add comments

--- textdb/textdb-sandbox/src/main/java/edu/uci/ics/textdb/sandbox/SandeepHelloWorld.java
+++ textdb/textdb-sandbox/src/main/java/edu/uci/ics/textdb/sandbox/SandeepHelloWorld.java
@@ -9,6 +9,7 @@ public class SandeepHelloWorld
     public static void main( String[] args )
     {
         //Printing Hello World! to console
+        // Zuozhi: please change the colon to semi-colon
         System.out.println( "Hello World!" ):
     }
 }
