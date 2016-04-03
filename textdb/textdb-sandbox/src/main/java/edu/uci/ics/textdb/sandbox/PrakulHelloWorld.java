060fd52776a202d3c4e43989fdde6704c8c0315e
shiladityasen <shiladitya.sen256@gmail.com>
2016-04-03 14:00:07 -0700

    Update PrakulHelloWorld.java
    Suggested edits marked by ***Review comments

--- textdb/textdb-sandbox/src/main/java/edu/uci/ics/textdb/sandbox/PrakulHelloWorld.java
+++ textdb/textdb-sandbox/src/main/java/edu/uci/ics/textdb/sandbox/PrakulHelloWorld.java
@@ -1,5 +1,5 @@
 package edu.uci.ics.textdb.sandbox;
-
+// Suggested edits are marked by ***Review comment
 /**
  * Hello world!
  *
@@ -8,8 +8,10 @@ public class PrakulHelloWorld
 {
     public static void main( String[] args )
     {
+        //***Review: Delete the comment in the following line - irrelevant
         //Bad comments
         System.out.println( "Hello World!" );
+        //***Review: Delete this while loop - not necessary
         // TOBE deleted
         while (true){
             System.out.println( "Stop me!" );
