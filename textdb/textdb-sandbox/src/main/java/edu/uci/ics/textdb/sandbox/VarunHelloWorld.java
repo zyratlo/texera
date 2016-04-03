66066228f504f004393f47f1fd780f14ee023aaf
Varun Bharill <varunbharill@Varuns-MacBook-Pro.local>
2016-04-03 15:53:13 -0700

    Removing inf. loop and bad comments.

--- textdb/textdb-sandbox/src/main/java/edu/uci/ics/textdb/sandbox/VarunHelloWorld.java
+++ textdb/textdb-sandbox/src/main/java/edu/uci/ics/textdb/sandbox/VarunHelloWorld.java
@@ -3,11 +3,7 @@ package edu.uci.ics.textdb.sandbox;
 public class VarunHelloWorld {
 	public static void main( String[] args )
     {
-        //Bad comments
-        System.out.println( "Hello World!" );
-        // TOBE deleted
-        while (true){
-            System.out.println( "Stop me!" );
-        }
+		//printing the hello world statement.
+		System.out.println( "Hello World!" );
     }
 }
