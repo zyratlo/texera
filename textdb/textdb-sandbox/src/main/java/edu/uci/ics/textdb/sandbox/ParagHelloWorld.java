1298ed174b8b804eab14571a081e29be09a6b5f8
Parag Saraogi <paragsaraogi@gmail.com>
2016-04-04 16:10:15 -0700

    Create ParagHelloWorld.java
    * Create ParagHelloWorld.java
    
    * Update ParagHelloWorld.java
    
    * Update ParagHelloWorld.java

---
+++ textdb/textdb-sandbox/src/main/java/edu/uci/ics/textdb/sandbox/ParagHelloWorld.java
@@ -0,0 +1,13 @@
+package edu.uci.ics.textdb.sandbox;
+
+/**
+ * Hello world!
+ *
+ */
+public class ParagHelloWorld
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
