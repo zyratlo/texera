68f10aa2cb633c247137f63e30ac3e89b5f1fcdb
Sam Hong <sam0227@gmail.com>
2016-04-04 09:34:44 -0700

    Problem Fixed

--- textdb/textdb-sandbox/src/main/java/edu/uci/ics/textdb/sandbox/FengHelloWorld.java
+++ textdb/textdb-sandbox/src/main/java/edu/uci/ics/textdb/sandbox/FengHelloWorld.java
@@ -8,11 +8,6 @@ public class FengHelloWorld
 {
     public static void main( String[] args )
     {
-        //Bad comments
         System.out.println( "Hello World!" );
-        // Bad loop fixed
-
-        //For Review:
-        System.out.println("SOMETHING");
     }
 }
