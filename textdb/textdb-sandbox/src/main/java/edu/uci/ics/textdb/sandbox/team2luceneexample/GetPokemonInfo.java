85b3d0194144b17fec1fcf71f4c49d41bdd43f9b
Kishore Narendran <kishore.narendran09@gmail.com>
2016-04-08 12:26:23 -0700

    Minor refactors and added some documentation

--- textdb/textdb-sandbox/src/main/java/edu/uci/ics/textdb/sandbox/team2luceneexample/GetPokemonInfo.java
+++ textdb/textdb-sandbox/src/main/java/edu/uci/ics/textdb/sandbox/team2luceneexample/GetPokemonInfo.java
@@ -4,6 +4,7 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -13,27 +14,60 @@ import java.net.URL;
  */
 
 public class GetPokemonInfo {
-    private static String apiUrl = "http://pokeapi.co/api/v2/pokemon/";
+
+    //Intiializing base URL of the Poke API and User agent
+    private static final String API_URL = "http://pokeapi.co/api/v2/pokemon/";
     private final String USER_AGENT = "Mozilla/5.0";
 
+    //Data Members
     public int numberOfPokemon;
+    public Pokemon[] pokemonInfo;
 
+    /**
+     * Parameterized constructor for the class which assigns number of pokemon
+     * and initializes the number of pokemonInfo objects
+     * @param numberOfPokemon - Parameter to initialize an object of GetPokemonInfo
+     */
     public GetPokemonInfo(int numberOfPokemon) {
         this.numberOfPokemon = numberOfPokemon;
+        this.pokemonInfo = new Pokemon[numberOfPokemon];
     }
 
+    /**
+     * Getter method for the number of pokemon data member
+     * @return - numberOfPokemon data member
+     */
     public int getNumberOfPokemon() {
         return numberOfPokemon;
     }
 
+    /**
+     * Setter method for the number of pokemon data member
+     * @param numberOfPokemon - Parameter to set the numberOfPokemon data member
+     */
     public void setNumberOfPokemon(int numberOfPokemon) {
         this.numberOfPokemon = numberOfPokemon;
     }
 
-    private String sendPokemonInfoGetRequest(int id) throws Exception {
+    /**
+     * Getter method
+     * @return - pokemonInfo data member
+     */
+    public Pokemon[] getPokemonInfo() {
+        return pokemonInfo;
+    }
+
+    /**
+     * This function sends a GET request to the PokeAPI to get
+     * information about a Pokemon of a specific ID
+     * @param id - Pokemon ID, for which information is request
+     * @return - String response from GET request
+     * @throws IOException
+     */
+    private String sendPokemonInfoGetRequest(int id) throws IOException {
 
         //Performing GET request to PokeAPI
-        String requestUrl = apiUrl.concat(Integer.toString(id));
+        String requestUrl = API_URL.concat(Integer.toString(id));
         URL obj = new URL(requestUrl);
         HttpURLConnection con = (HttpURLConnection) obj.openConnection();
         StringBuffer response = new StringBuffer();
@@ -61,20 +95,45 @@ public class GetPokemonInfo {
         }
     }
 
-    private String[] aggregatePokemonInfo() throws Exception {
+    /**
+     * Getting an array of pokemon info response strings on pinging
+     * the Poke API
+     * @return - Array of string HTTP GET responses for all the pokemon IDs
+     * @throws IOException
+     */
+    private String[] getPokemonInfoStrings() throws IOException {
         String[] allPokemonInfo = new String[numberOfPokemon];
         for(int i = 1; i <= numberOfPokemon; i++) {
-            allPokemonInfo[i-1] = sendPokemonInfoGetRequest(i);
+            String pokemonInfoStr = sendPokemonInfoGetRequest(i);
+            if(pokemonInfoStr == null) {
+                allPokemonInfo[i-1] = "";
+            }
+            else {
+                allPokemonInfo[i-1] = pokemonInfoStr;
+            }
+
         }
         return allPokemonInfo;
     }
 
-    private Pokemon[] getPokemonInfoObjects() throws Exception{
-        String[] pokemonInfoStrs = aggregatePokemonInfo();
-        Pokemon[] pokemonInfos = new Pokemon[pokemonInfoStrs.length];
+    /**
+     * Parses the pokemon info response strings as JSON objects,
+     * and then aggregating an array of Pokemon objects as the pokemonInfo data member
+     * with necessary information as described by the data members of the Pokemon class
+     * @throws IOException
+     */
+    private void aggregatePokemonInfo() throws IOException {
+        String[] pokemonInfoStrs = getPokemonInfoStrings();
         for(int i = 1; i <= numberOfPokemon; i++) {
             //Converting the info string to JSON object
             String pokemonInfoStr = pokemonInfoStrs[i-1];
+
+            //Skipping JSON parsing for empty/malformed response strings
+            if(pokemonInfoStr.equals("")) {
+                pokemonInfo[i-1] = null;
+                continue;
+            }
+
             JSONObject pokemonInfoJsonObj = new JSONObject(pokemonInfoStr);
 
             //Parsing the JSON object for necessary information
@@ -97,10 +156,9 @@ public class GetPokemonInfo {
             pokemonTypesStr = pokemonTypesStr.substring(0, pokemonTypesStr.length()-2);
 
             //Creating a Pokemon object after parsing the JSON
-            Pokemon pokemonInfo = pokemonInfos[i-1];
-            pokemonInfo = new Pokemon(pokemonId, pokemonName, pokemonMovesStr, pokemonTypesStr);
-            pokemonInfos[i-1] = pokemonInfo;
+            Pokemon pokemon = pokemonInfo[i-1];
+            pokemon = new Pokemon(pokemonId, pokemonName, pokemonMovesStr, pokemonTypesStr);
+            pokemonInfo[i-1] = pokemon;
         }
-        return pokemonInfos;
     }
 }
