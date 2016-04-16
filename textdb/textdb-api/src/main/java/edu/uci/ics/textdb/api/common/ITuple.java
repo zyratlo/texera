1cacfad2387b55b47af317265dd0c778ab79278d
Sudeep Meduri <sudeep.m91@gmail.com>
2016-04-16 16:13:15 -0700

    Implemented DictionaryMatcher

--- textdb/textdb-api/src/main/java/edu/uci/ics/textdb/api/common/ITuple.java
+++ textdb/textdb-api/src/main/java/edu/uci/ics/textdb/api/common/ITuple.java
@@ -6,7 +6,13 @@ import java.util.List;
  * Created by chenli on 3/25/16.
  */
 public interface ITuple {
-    IField getField(int index);
-    IField getField(String fieldName);
-    List<IField> getFields();
+	IField getField(int index);
+
+	IField getField(String fieldName);
+
+	List<IField> getFields();
+
+	List<Attribute> getSchema();
+
+	void addField(Attribute attribute, IField field);
 }
--- textdb/textdb-dataflow/src/main/java/edu/uci/ics/textdb/dataflow/dictionarymatcher/DictionaryMatcher.java
+++ textdb/textdb-dataflow/src/main/java/edu/uci/ics/textdb/dataflow/dictionarymatcher/DictionaryMatcher.java
@@ -1,30 +1,44 @@
 
 package edu.uci.ics.textdb.dataflow.dictionarymatcher;
 
+import edu.uci.ics.textdb.api.common.Attribute;
 import edu.uci.ics.textdb.api.common.IDictionary;
+import edu.uci.ics.textdb.api.common.IField;
 import edu.uci.ics.textdb.api.common.ITuple;
 import edu.uci.ics.textdb.api.dataflow.IOperator;
+import edu.uci.ics.textdb.common.constants.SchemaConstants;
 import edu.uci.ics.textdb.common.exception.DataFlowException;
+import edu.uci.ics.textdb.common.field.IntegerField;
+import edu.uci.ics.textdb.common.field.StringField;
 
 /**
  * @author Sudeep [inkudo]
- *
+ * 
  */
 public class DictionaryMatcher implements IOperator {
 
 	private IOperator operator;
 	private IDictionary dict;
+	private String dictValue;
+	private int positionIndex;
+	private int fieldIndex;
+	private int spanIndexVal;
+	private ITuple spanTuple;
 
 	public DictionaryMatcher(IDictionary dict, IOperator operator) {
 		this.operator = operator;
 		this.dict = dict;
-
 	}
 
 	@Override
 	public void open() throws Exception {
 		try {
+			positionIndex = 0;
+			fieldIndex = 0;
 			operator.open();
+			dictValue = dict.getNextTuple();
+			spanTuple = operator.getNextTuple();
+
 		} catch (Exception e) {
 			e.printStackTrace();
 			throw new DataFlowException(e.getMessage(), e);
@@ -33,11 +47,54 @@ public class DictionaryMatcher implements IOperator {
 
 	@Override
 	public ITuple getNextTuple() throws Exception {
+		IField field;
+		if ((field = spanTuple.getField(fieldIndex)) != null) {
+			if (field instanceof StringField) {
+				String fieldValue = ((StringField) field).getValue();
+				if ((spanIndexVal = fieldValue.indexOf(dictValue, positionIndex)) != -1) {
+
+					positionIndex = spanIndexVal + fieldValue.length();
+
+					Attribute attribute = spanTuple.getSchema().get(fieldIndex);
+					String fieldName = attribute.getFieldName();
+
+					spanTuple.addField(SchemaConstants.SPAN_FIELD_NAME_ATTRIBUTE, new StringField(fieldName));
+					spanTuple.addField(SchemaConstants.SPAN_KEY_ATTRIBUTE, new StringField(dictValue));
+					spanTuple.addField(SchemaConstants.SPAN_BEGIN_ATTRIBUTE, new IntegerField(spanIndexVal));
+					spanTuple.addField(SchemaConstants.SPAN_END_ATTRIBUTE, new IntegerField(positionIndex - 1));
+					return spanTuple;
+
+				} else {
+					fieldIndex++;
+					positionIndex = 0;
+					return getNextTuple();
+				}
+			}
+		} else if ((spanTuple = operator.getNextTuple()) != null) {
+			fieldIndex = 0;
+			positionIndex = 0;
+			return getNextTuple();
+
+		} else if ((dictValue = dict.getNextTuple()) != null) {
+			fieldIndex = 0;
+			positionIndex = 0;
+
+			operator.close();
+			operator.open();
+			spanTuple = operator.getNextTuple();
+			return getNextTuple();
+		}
+
 		return null;
 	}
 
 	@Override
-	public void close() throws Exception {
-
+	public void close() throws DataFlowException {
+		try {
+			operator.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new DataFlowException(e.getMessage(), e);
+		}
 	}
 }
