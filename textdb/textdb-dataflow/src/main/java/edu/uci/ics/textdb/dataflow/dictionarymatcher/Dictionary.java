562d38b3e445f32446b8b4109288336b2b3fea8c
Sudeep Meduri <sudeep.m91@gmail.com>
2016-04-16 00:21:21 -0700

    Implementation of Dictionary.java

--- textdb/textdb-dataflow/src/main/java/edu/uci/ics/textdb/dataflow/dictionarymatcher/Dictionary.java
+++ textdb/textdb-dataflow/src/main/java/edu/uci/ics/textdb/dataflow/dictionarymatcher/Dictionary.java
@@ -4,6 +4,7 @@
 package edu.uci.ics.textdb.dataflow.dictionarymatcher;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -12,7 +13,11 @@ import java.util.List;
  */
 public class Dictionary {
 
+	private int cursor = -1;
+
 	public Dictionary() {
-		List<String> dict = new ArrayList<String>();
+		List<String> dict = new ArrayList<String>(Arrays.asList("l5", "l2", "l4"));
+		cursor = 0;
 	}
+
 }
