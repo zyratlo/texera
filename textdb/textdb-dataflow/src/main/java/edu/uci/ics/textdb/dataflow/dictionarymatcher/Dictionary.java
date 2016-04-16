8cafedd43015967927fa115a879d338cc8d58ee4
Sudeep Meduri <sudeep.m91@gmail.com>
2016-04-16 00:17:59 -0700

    Implemented Dictionary.java

---
+++ textdb/textdb-dataflow/src/main/java/edu/uci/ics/textdb/dataflow/dictionarymatcher/Dictionary.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package edu.uci.ics.textdb.dataflow.dictionarymatcher;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Sudeep [inkudo]
+ *
+ */
+public class Dictionary {
+
+	public Dictionary() {
+		List<String> dict = new ArrayList<String>();
+	}
+}
---
+++ textdb/textdb-dataflow/src/main/java/edu/uci/ics/textdb/dataflow/dictionarymatcher/DictionaryMatcher.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package edu.uci.ics.textdb.dataflow.dictionarymatcher;
+
+/**
+ * @author Sudeep [inkudo]
+ *
+ */
+public class DictionaryMatcher {
+
+}
