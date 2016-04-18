4c27d4294e973113d577e867141b41f9329afe2e
Sudeep Meduri <sudeep.m91@gmail.com>
2016-04-17 19:05:39 -0700

    Indentation changed to four space

--- textdb/textdb-dataflow/src/test/java/edu/uci/ics/textdb/dataflow/dictionarymatcher/DictionaryMatcherTest.java
+++ textdb/textdb-dataflow/src/test/java/edu/uci/ics/textdb/dataflow/dictionarymatcher/DictionaryMatcherTest.java
@@ -1,8 +1,6 @@
 
 package edu.uci.ics.textdb.dataflow.dictionarymatcher;
 
-
-
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -34,7 +32,7 @@ public class DictionaryMatcherTest {
     private LuceneDataStore dataStore;
     private IDataWriter dataWriter;
     private IDataReader dataReader;
-    
+
     @Before
     public void setUp() throws Exception {
 
@@ -44,83 +42,80 @@ public class DictionaryMatcherTest {
         dataWriter.writeData(TestConstants.getSamplePeopleTuples());
         dataReader = new LuceneDataReader(dataStore, LuceneConstants.SCAN_QUERY,
                 TestConstants.SAMPLE_SCHEMA_PEOPLE.get(0).getFieldName());
-        
+
     }
 
     @After
     public void cleanUp() throws Exception {
         dataWriter.clearData();
     }
-    
+
     /**
-   	 * Scenario S1:verifies GetNextTuple of Dictionary
-   	 */
+     * Scenario S1:verifies GetNextTuple of Dictionary
+     */
 
-       @Test
-       public void testGetNextDictionaryItem() throws Exception {
+    @Test
+    public void testGetNextDictionaryItem() throws Exception {
 
-           ArrayList<String> names = new ArrayList<String>(
-                   Arrays.asList("brad","clooney","george", "lee"));
-           IDictionary dict = new Dictionary(names);
-           int numTuples = 0;
-           while ( dict.getNextDictValue()!= null) {
-             numTuples++;
-           }
-           Assert.assertEquals(4, numTuples);
-          
-       }
+        ArrayList<String> names = new ArrayList<String>(Arrays.asList("brad", "clooney", "george", "lee"));
+        IDictionary dict = new Dictionary(names);
+        int numTuples = 0;
+        while (dict.getNextDictValue() != null) {
+            numTuples++;
+        }
+        Assert.assertEquals(4, numTuples);
 
-     /**
-	 * Scenario S2:verifies GetNextTuple of DictionaryMatcher
-	 */
+    }
+
+    /**
+     * Scenario S2:verifies GetNextTuple of DictionaryMatcher
+     */
 
     @Test
     public void testGetNextTuple() throws Exception {
 
-        ArrayList<String> names = new ArrayList<String>(
-                Arrays.asList("bruce","tom","lee", "brad","cena"));
+        ArrayList<String> names = new ArrayList<String>(Arrays.asList("bruce", "tom", "lee", "brad", "cena"));
         IDictionary dict = new Dictionary(names);
         ISourceOperator sourceOperator = new ScanBasedSourceOperator(dataReader);
 
         dictionaryMatcher = new DictionaryMatcher(dict, sourceOperator);
         dictionaryMatcher.open();
-        
+
         int numTuples = 0;
-        while ( dictionaryMatcher.getNextTuple()!= null) {
-          numTuples++;
+        while (dictionaryMatcher.getNextTuple() != null) {
+            numTuples++;
         }
         Assert.assertEquals(4, numTuples);
         dictionaryMatcher.close();
     }
-    
+
     /**
-   	 * Scenario S3:verifies ITuple returned by DictionaryMatcher
-   	 */
+     * Scenario S3:verifies ITuple returned by DictionaryMatcher
+     */
 
-       @Test
-       public void testTuple() throws Exception {
+    @Test
+    public void testTuple() throws Exception {
 
-           ArrayList<String> names = new ArrayList<String>(
-                   Arrays.asList("bruce","tom","lee", "brad","george","cena","rock","brande"));
-           IDictionary dict = new Dictionary(names);
-           ISourceOperator sourceOperator = new ScanBasedSourceOperator(dataReader);
+        ArrayList<String> names = new ArrayList<String>(
+                Arrays.asList("bruce", "tom", "lee", "brad", "george", "cena", "rock", "brande"));
+        IDictionary dict = new Dictionary(names);
+        ISourceOperator sourceOperator = new ScanBasedSourceOperator(dataReader);
 
-           dictionaryMatcher = new DictionaryMatcher(dict, sourceOperator);
-           dictionaryMatcher.open();
-           
-           ITuple iTuple;
-           int numTuples = 0;
-           while ( (iTuple=dictionaryMatcher.getNextTuple()) != null) {
-            
-            String returnedString = (String)iTuple.getField(6).getValue();
+        dictionaryMatcher = new DictionaryMatcher(dict, sourceOperator);
+        dictionaryMatcher.open();
+
+        ITuple iTuple;
+        int numTuples = 0;
+        while ((iTuple = dictionaryMatcher.getNextTuple()) != null) {
+
+            String returnedString = (String) iTuple.getField(6).getValue();
             boolean contains = TestUtils.contains(names, returnedString);
             Assert.assertTrue(contains);
             numTuples++;
-            
-           }
-           Assert.assertEquals(5, numTuples);
-           dictionaryMatcher.close();
-       }
 
+        }
+        Assert.assertEquals(5, numTuples);
+        dictionaryMatcher.close();
+    }
 
 }
--- textdb/textdb-dataflow/src/test/java/edu/uci/ics/textdb/dataflow/utils/TestUtils.java
+++ textdb/textdb-dataflow/src/test/java/edu/uci/ics/textdb/dataflow/utils/TestUtils.java
@@ -17,28 +17,27 @@ public class TestUtils {
         for (ITuple sampleTuple : sampleTuples) {
             contains = true;
             for (int i = 0; i < schemaSize; i++) {
-                if(!sampleTuple.getField(i).equals(actualTuple.getField(i))){
+                if (!sampleTuple.getField(i).equals(actualTuple.getField(i))) {
                     contains = false;
                 }
             }
-            if(contains){
+            if (contains) {
                 return contains;
             }
         }
         return contains;
     }
-    
+
     public static boolean contains(ArrayList<String> Dictionary, String returnedString) {
         boolean contains = false;
-        
+
         for (String dictItem : Dictionary) {
-        		if(dictItem.equals(returnedString))
-        		{
-        			contains = true;
-        			return contains;
-        		}
+            if (dictItem.equals(returnedString)) {
+                contains = true;
+                return contains;
             }
-            
+        }
+
         return contains;
     }
 }
