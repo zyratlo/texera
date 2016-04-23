51215c93211fa82bf91c863a76a2f87b9fe3c667
Akshay <akshaybetala@gmail.com>
2016-04-22 18:28:30 -0700

    1. updating test cases 2. Indentation fix

--- textdb/textdb-common/src/main/java/edu/uci/ics/textdb/common/field/TextField.java
+++ textdb/textdb-common/src/main/java/edu/uci/ics/textdb/common/field/TextField.java
@@ -1,42 +1,46 @@
 package edu.uci.ics.textdb.common.field;
 
-
 import edu.uci.ics.textdb.api.common.IField;
 
 /**
- * Created by chenli on 3/31/16.
+ * Created by chenli on 3/31/16. 
+ * A field that is indexed and tokenized, without
+ * term vectors. For example this would be used on a 'body' field, that contains
+ * the bulk of a document's text.
  */
 public class TextField implements IField {
 
-    private final String value;
+	private final String value;
 
-    public TextField(String value){
-        this.value = value;
-    }
+	public TextField(String value) {
+		this.value = value;
+	}
 
-    public String getValue(){
-        return value;
-    }
+	public String getValue() {
+		return value;
+	}
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
 
-        TextField that = (TextField) o;
+		TextField that = (TextField) o;
 
-        return !(value != null ? !value.equals(that.value) : that.value != null);
+		return !(value != null ? !value.equals(that.value) : that.value != null);
 
-    }
+	}
 
-    @Override
-    public int hashCode() {
-        return value != null ? value.hashCode() : 0;
-    }
+	@Override
+	public int hashCode() {
+		return value != null ? value.hashCode() : 0;
+	}
 
-    @Override
-    public String toString() {
-        return "TextField [value=" + value + "]";
-    }
-    
+	@Override
+	public String toString() {
+		return "TextField [value=" + value + "]";
+	}
+
 }
