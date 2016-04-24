0de425544b1434033b54fec04ca4238f9b21eedb
Sandeep Reddy Madugula <reddy602.sandeep@gmail.com>
2016-04-23 19:15:50 -0700

    Issue #63: Added more fields in the SchemaTest

--- textdb/textdb-api/src/test/java/edu/uci/ics/textdb/api/common/SchemaTest.java
+++ textdb/textdb-api/src/test/java/edu/uci/ics/textdb/api/common/SchemaTest.java
@@ -11,19 +11,24 @@ import org.junit.Test;
 public class SchemaTest {
     private Schema schema;
     private List<Attribute> attributes;
-    private String fieldName = "sampleField";
-    private FieldType type = FieldType.STRING;
+    private String fieldName_1 = "sampleField_1";
+    private FieldType type_1 = FieldType.STRING;
+    private String fieldName_2 = "sampleField_2";
+    private FieldType type_2 = FieldType.STRING;
     
     @Before
     public void setUp(){
         attributes = new ArrayList<Attribute>();
-        attributes.add(new Attribute(fieldName, type));
+        attributes.add(new Attribute(fieldName_1, type_1));
+        attributes.add(new Attribute(fieldName_2, type_2));
         schema = new Schema(attributes);
     }
     
     @Test
     public void testGetAttributes(){
         List<Attribute> attributes = schema.getAttributes();
+        //"assertSame" tests if the variables are pointing to the same object or not
+        //So no need to check for the internal fields.
         Assert.assertSame(this.attributes, attributes);
     }
 }
