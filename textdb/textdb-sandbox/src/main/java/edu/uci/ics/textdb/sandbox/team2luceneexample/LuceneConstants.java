868d3da42d33bd6f6b8593dd66a8366a70ae1e9f
Sandeep Reddy Madugula <reddy602.sandeep@gmail.com>
2016-04-23 20:17:42 -0700

    Creating the pokadex index at the root level

--- textdb/textdb-sandbox/src/main/java/edu/uci/ics/textdb/sandbox/team2luceneexample/LuceneConstants.java
+++ textdb/textdb-sandbox/src/main/java/edu/uci/ics/textdb/sandbox/team2luceneexample/LuceneConstants.java
@@ -2,7 +2,7 @@ package edu.uci.ics.textdb.sandbox.team2luceneexample;
 
 public class LuceneConstants
 {
-    public static final String INDEX = "src/main/java/edu/uci/ics/textdb/sandbox/team2luceneexample/pokadex";
+    public static final String INDEX = "pokadex";
     public static final String ID_FIELD = "id";
     public static final String NAME_FIELD = "name";
 
