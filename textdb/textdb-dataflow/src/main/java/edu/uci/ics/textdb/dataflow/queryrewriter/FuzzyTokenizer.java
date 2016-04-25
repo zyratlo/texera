4b348487c28b70bfbecb77638413a177f53085bf
Kishore Narendran <kishore.narendran09@gmail.com>
2016-04-25 13:18:05 -0700

    Added FuzzyTokenizer class'

---
+++ textdb/textdb-dataflow/src/main/java/edu/uci/ics/textdb/dataflow/queryrewriter/FuzzyTokenizer.java
@@ -0,0 +1,29 @@
+package edu.uci.ics.textdb.dataflow.queryrewriter;
+
+/**
+ * Created by kishorenarendran on 25/04/16.
+ */
+public class FuzzyTokenizer {
+
+    private String query;
+
+    /**
+     * Constructor that accepts the string version of the Lucene search query
+     *
+     * @param query
+     */
+    public FuzzyTokenizer(String query) {
+        this.query = query;
+    }
+
+    /**
+     * Function that returns the list of parsed/rewritten queries
+     *
+     * @return
+     */
+    public String[] getParsedQueries() {
+        //TODO - Stubbed function
+        return new String[1];
+    }
+}
+
