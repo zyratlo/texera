352ca56536ef81db254e26def0c7bdebdd7bd7ba
Kishore Narendran <kishore.narendran09@gmail.com>
2016-04-25 17:41:11 -0700

    Updated interface to QueryRewriter class

--- textdb/textdb-dataflow/src/main/java/edu/uci/ics/textdb/dataflow/queryrewriter/QueryRewriter.java
+++ textdb/textdb-dataflow/src/main/java/edu/uci/ics/textdb/dataflow/queryrewriter/QueryRewriter.java
@@ -1,7 +1,12 @@
 package edu.uci.ics.textdb.dataflow.queryrewriter;
 
-import edu.uci.ics.textdb.api.common.ITuple;
+import edu.uci.ics.textdb.api.common.*;
 import edu.uci.ics.textdb.api.dataflow.IOperator;
+import edu.uci.ics.textdb.common.field.DataTuple;
+import edu.uci.ics.textdb.common.field.StringListField;
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Created by kishorenarendran on 25/04/16.
@@ -10,6 +15,13 @@ public class QueryRewriter implements IOperator{
 
     private String searchQuery;
     private FuzzyTokenizer fuzzyTokenizer;
+
+    public static final String QUERYLIST = "querylist";
+    public static final Attribute QUERYLIST_ATTR = new Attribute(QUERYLIST, FieldType.STRING_LIST);
+    public static final List<Attribute> ATTRIBUTES_QUERY_LIST = Arrays.asList(QUERYLIST_ATTR);
+    public static final Schema SCHEMA_QUERY_LIST = new Schema(ATTRIBUTES_QUERY_LIST);
+
+    private ITuple itupleResult;
 
     /**
      * Parameterized constructor that requires a Search Query String that
@@ -28,17 +40,21 @@ public class QueryRewriter implements IOperator{
     @Override
     public void open() throws Exception {
         fuzzyTokenizer = new FuzzyTokenizer(searchQuery);
+
     }
 
     /**
      * Calling appropriate fuzzyTokenizer method to populate the list
-     * of rewritten search queries
+     * of rewritten search queries, and constructing a tuple from it. 
      * @return - Tuple with the rewritten queries as a comma separated string
      * @throws Exception
      */
     @Override
     public ITuple getNextTuple() throws Exception {
-        return null;
+        List<String> queryStrings = Arrays.asList(fuzzyTokenizer.getParsedQueries());
+        IField[] iFieldResult = {new StringListField(queryStrings)};
+        itupleResult = new DataTuple(SCHEMA_QUERY_LIST, iFieldResult);
+        return itupleResult;
     }
 
     /**
