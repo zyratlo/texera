edec7bd58b16bca3bdef76f2304fa8463922fbd4
Kishore Narendran <kishore.narendran09@gmail.com>
2016-05-01 01:26:08 -0700

    Updated QueryRewriter test cases

--- textdb/textdb-dataflow/src/test/java/edu/uci/ics/textdb/dataflow/queryrewriter/QueryRewriterTest.java
+++ textdb/textdb-dataflow/src/test/java/edu/uci/ics/textdb/dataflow/queryrewriter/QueryRewriterTest.java
@@ -1,11 +1,14 @@
 package edu.uci.ics.textdb.dataflow.queryrewriter;
 
+import edu.uci.ics.textdb.api.common.IField;
 import edu.uci.ics.textdb.api.common.ITuple;
+import edu.uci.ics.textdb.common.field.DataTuple;
+import edu.uci.ics.textdb.common.field.ListField;
+import edu.uci.ics.textdb.dataflow.utils.TestUtils;
 import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 /**
@@ -26,30 +29,20 @@ public class QueryRewriterTest {
      */
     @Test
     public void testHorseShoeString() throws Exception {
+
         String query = "horseshoe";
 
         QueryRewriter queryRewriter = new QueryRewriter(query);
         queryRewriter.open();
-        ITuple iTuple = queryRewriter.getNextTuple();
+        ITuple resultItuple = queryRewriter.getNextTuple();
         queryRewriter.close();
 
-        Assert.assertNotNull(iTuple);
+        List<String> expectedRewrittenStrings = Arrays.asList("hor se shoe", "hors es hoe", "horse shoe", "horses hoe","horseshoe");
+        IField[] expectedIfield = {new ListField(expectedRewrittenStrings)};
+        ITuple expectedItuple = new DataTuple(QueryRewriter.SCHEMA_QUERY_LIST, expectedIfield);
 
-        List<String> fuzzySet = (List<String>) iTuple.getField(0).getValue();
-        Assert.assertNotNull(fuzzySet);
-
-        List<String> correctSet = Arrays.asList("hor se shoe",
-                                                "hors es hoe",
-                                                "horse shoe",
-                                                "horses hoe",
-                                                "horseshoe");
-        Collections.sort(correctSet);
-        Collections.sort(fuzzySet);
-
-        Assert.assertEquals(1,1);
-
-        //Assert.assertEquals(correctSet.size(), fuzzySet.size());
-        //Assert.assertEquals(correctSet, fuzzySet);
+        boolean isSame = TestUtils.equalTo(resultItuple, expectedItuple);
+        Assert.assertTrue(isSame);
     }
 
     /**
@@ -58,28 +51,20 @@ public class QueryRewriterTest {
      */
     @Test
     public void testHorseSpaceShoeString() throws Exception {
+
         String query = "horse shoe";
 
         QueryRewriter queryRewriter = new QueryRewriter(query);
         queryRewriter.open();
-        ITuple iTuple = queryRewriter.getNextTuple();
+        ITuple resultItuple = queryRewriter.getNextTuple();
         queryRewriter.close();
 
-        Assert.assertNotNull(iTuple);
+        List<String> expectedRewrittenStrings = Arrays.asList("horse shoe","hor se shoe");
+        IField[] expectedIfield = {new ListField(expectedRewrittenStrings)};
+        ITuple expectedItuple = new DataTuple(QueryRewriter.SCHEMA_QUERY_LIST, expectedIfield);
 
-        List<String> fuzzySet = (List<String>) iTuple.getField(0).getValue();
-        Assert.assertNotNull(fuzzySet);
-
-        List<String> correctSet = Arrays.asList("horse shoe",
-                                                "hor se shoe");
-
-        Collections.sort(correctSet);
-        Collections.sort(fuzzySet);
-
-        Assert.assertEquals(1,1);
-
-        //Assert.assertEquals(correctSet.size(), fuzzySet.size());
-        //Assert.assertEquals(correctSet, fuzzySet);
+        boolean isSame = TestUtils.equalTo(resultItuple, expectedItuple);
+        Assert.assertTrue(isSame);
     }
 
     /**
@@ -88,26 +73,19 @@ public class QueryRewriterTest {
      */
     @Test
     public void testNewYorkCityString() throws Exception {
+
         String query = "newyork city";
 
         QueryRewriter queryRewriter = new QueryRewriter(query);
         queryRewriter.open();
-        ITuple iTuple = queryRewriter.getNextTuple();
+        ITuple resultItuple = queryRewriter.getNextTuple();
         queryRewriter.close();
 
-        Assert.assertNotNull(iTuple);
+        List<String> expectedRewrittenStrings = Arrays.asList("new york city","newyork city");
+        IField[] expectedIfield = {new ListField(expectedRewrittenStrings)};
+        ITuple expectedItuple = new DataTuple(QueryRewriter.SCHEMA_QUERY_LIST, expectedIfield);
 
-        List<String> fuzzySet = (List<String>) iTuple.getField(0).getValue();
-        Assert.assertNotNull(fuzzySet);
-
-        List<String> correctSet = Arrays.asList("new york city",
-                                                "newyork city");
-        Collections.sort(correctSet);
-        Collections.sort(fuzzySet);
-
-        Assert.assertEquals(1,1);
-
-        //Assert.assertEquals(correctSet.size(), fuzzySet.size());
-        //Assert.assertEquals(correctSet, fuzzySet);
+        boolean isSame = TestUtils.equalTo(resultItuple, expectedItuple);
+        Assert.assertTrue(isSame);
     }
 }
