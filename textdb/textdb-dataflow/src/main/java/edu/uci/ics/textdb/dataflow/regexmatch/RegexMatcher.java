f696156f0ae02d64b962b5dd6bb466e8b6b929ce
laisycs <stella.lai@foxmail.com>
2016-05-05 22:23:58 -0700

    Use createSpanSchema from common.utils package in RegexMatcher

--- textdb/textdb-dataflow/src/main/java/edu/uci/ics/textdb/dataflow/regexmatch/RegexMatcher.java
+++ textdb/textdb-dataflow/src/main/java/edu/uci/ics/textdb/dataflow/regexmatch/RegexMatcher.java
@@ -5,18 +5,17 @@ import java.util.List;
 
 import org.apache.lucene.search.Query;
 
-import edu.uci.ics.textdb.api.common.Attribute;
 import edu.uci.ics.textdb.api.common.IField;
 import edu.uci.ics.textdb.api.common.IPredicate;
 import edu.uci.ics.textdb.api.common.ITuple;
 import edu.uci.ics.textdb.api.common.Schema;
 import edu.uci.ics.textdb.api.dataflow.IOperator;
 import edu.uci.ics.textdb.api.dataflow.ISourceOperator;
-import edu.uci.ics.textdb.common.constants.SchemaConstants;
 import edu.uci.ics.textdb.common.exception.DataFlowException;
 import edu.uci.ics.textdb.common.field.DataTuple;
 import edu.uci.ics.textdb.common.field.ListField;
 import edu.uci.ics.textdb.common.field.Span;
+import edu.uci.ics.textdb.common.utils.Utils;
 import edu.uci.ics.textdb.dataflow.common.RegexPredicate;
 
 /**
@@ -63,9 +62,11 @@ public class RegexMatcher implements IOperator {
             spans = regexPredicate.computeMatches(sourceTuple);
             
             if (spans != null && spans.size() != 0) { // a list of matches found
-            	if (schema == null || spanSchema == null) {
+            	if (schema == null) {
             		schema = sourceTuple.getSchema();
-            		spanSchema = createSpanSchema();
+            	}
+            	if (spanSchema == null) {
+            		spanSchema = Utils.createSpanSchema(schema);
             	}
             	fields = sourceTuple.getFields();
             	return getSpanTuple(spans);
@@ -76,21 +77,6 @@ public class RegexMatcher implements IOperator {
             e.printStackTrace();
             throw new DataFlowException(e.getMessage(), e);
         }        
-    }
-    
-    /**
-     * This function converts the schema of original tuples to the schema of match results returned by RegexMatcher. 
-     * It adds SPAN_LIST_ATTRIBUTE in the end of the schema of tuples from lower level.
-     * Because we need to use span to specify the position of matched sequence in this tuple.
-     * For example, if a tuple ("george watson", "staff", 33, "(949)888-8888") matched "g[^\s]*",
-     * RegexMatcher returns ("george watson", "staff", 33, "(949)888-8888", [Span(name, 0, 6, "g[^\s]*", "george watson")])
-     * 
-     * @return a schema object = attributes in original tuples followed by SPAN_LIST_ATTRIBUTE
-     */
-    private Schema createSpanSchema() {
-    	List<Attribute> attributesCopy = new ArrayList<>(schema.getAttributes());
-    	attributesCopy.add(SchemaConstants.SPAN_LIST_ATTRIBUTE);
-    	return new Schema(attributesCopy.toArray(new Attribute[attributesCopy.size()]));
     }
     
     private ITuple getSpanTuple(List<Span> spans) {
