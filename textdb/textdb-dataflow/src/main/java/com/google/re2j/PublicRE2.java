c39abccb6a456fb6b55a2dbe618ff1516cbdd06d
zuozhiw <zuozhiw@uci.edu>
2016-05-10 13:15:58 -0700

    issue #30 add comments for re2j wrapper classes

--- textdb/textdb-dataflow/src/main/java/com/google/re2j/PublicRE2.java
+++ textdb/textdb-dataflow/src/main/java/com/google/re2j/PublicRE2.java
@@ -1,10 +1,66 @@
 package com.google.re2j;
 
+/**
+ * PublicRE2 includes flags from RE2 class
+ * comments about flags are copied directly from RE2.java
+ * 
+ * A commonly needed flag is PublicRE2.PERL, which tells RE2 to use Perl syntax
+ * 
+ * @author zuozhi
+ *
+ */
 public class PublicRE2 {
-	
-	// a flag about regex syntax
-	public static int getPERL() {
-		return RE2.PERL;
-	}
-	
+
+	// Fold case during matching (case-insensitive).
+	public static final int FOLD_CASE 		= RE2.FOLD_CASE;
+
+	// Treat pattern as a literal string instead of a regexp.
+	public static final int LITERAL 		= RE2.LITERAL;
+
+	// Allow character classes like [^a-z] and [[:space:]] to match newline.
+	public static final int CLASS_NL 		= RE2.CLASS_NL;
+
+	// Allow '.' to match newline.
+	public static final int DOT_NL 			= RE2.DOT_NL;
+
+	// Treat ^ and $ as only matching at beginning and end of text, not
+	// around embedded newlines. (Perl's default).
+	public static final int ONE_LINE 		= RE2.ONE_LINE;
+
+	// Make repetition operators default to non-greedy.
+	public static final int NON_GREEDY 		= RE2.NON_GREEDY;
+
+	// allow Perl extensions:
+	// non-capturing parens - (?: )
+	// non-greedy operators - *? +? ?? {}?
+	// flag edits - (?i) (?-i) (?i: )
+	// i - FoldCase
+	// m - !OneLine
+	// s - DotNL
+	// U - NonGreedy
+	// line ends: \A \z
+	// \Q and \E to disable/enable metacharacters
+	// (?P<name>expr) for named captures
+	// \C (any byte) is not supported.
+	public static final int PERL_X 			= RE2.PERL_X;
+
+	// Allow \p{Han}, \P{Han} for Unicode group and negation.
+	public static final int UNICODE_GROUPS 	= RE2.UNICODE_GROUPS;
+
+	// Regexp END_TEXT was $, not \z. Internal use only.
+	public static final int WAS_DOLLAR 		= RE2.WAS_DOLLAR;
+
+	public static final int MATCH_NL 		= RE2.CLASS_NL | RE2.DOT_NL;
+
+	// As close to Perl as possible.
+	public static final int PERL 			= RE2.CLASS_NL | RE2.ONE_LINE | RE2.PERL_X | RE2.UNICODE_GROUPS;
+
+	// POSIX syntax.
+	public static final int POSIX 			= RE2.POSIX;
+
+	// // Anchors
+	public static final int UNANCHORED 		= RE2.UNANCHORED;
+	public static final int ANCHOR_START 	= RE2.ANCHOR_START;
+	public static final int ANCHOR_BOTH 	= RE2.ANCHOR_BOTH;
+
 }
--- textdb/textdb-dataflow/src/main/java/com/google/re2j/PublicRegexp.java
+++ textdb/textdb-dataflow/src/main/java/com/google/re2j/PublicRegexp.java
@@ -4,16 +4,47 @@ import java.util.Arrays;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+
+/**
+ * Public Wrapper class for re2j.Regexp
+ * Regexp class represents the abstract syntax tree
+ * 
+ * @author Zuozhi Wang
+ *
+ */
 public class PublicRegexp extends Regexp {
+/*
+	// fields originally declared in Regexp:
+	// for detailed explanations please see getter methods
+  
+	Op op;                   // operator
+	int flags;               // bitmap of parse flags
+	Regexp[] subs;           // subexpressions, if any.  Never null.
+		                           // subs[0] is used as the freelist.
+	int[] runes;             // matched runes, for LITERAL, CHAR_CLASS
+	int min, max;            // min, max for REPEAT
+	int cap;                 // capturing index, for CAPTURE
+	String name;             // capturing name, for CAPTURE
+*/
 	
+	// publicSubs are an array of subexpressions with type PublicRegexp
 	private PublicRegexp[] publicSubs;
 
-	// shallow copy constructor
+	/**
+	 * private shallow copy constructor
+	 * shallow copy only copies reference to subexpression array
+	 */
 	private PublicRegexp(Regexp that) {
 		super(that);
 	}
 	  
-	// deep copy
+	/**
+	 * deep copy converts every Regexp Object in subexpressions to a PublicRegexp object
+	 * and put them in publicSubs array
+	 * @param Regexp re, A Regexp that needs to be converted to PublicRegexp
+	 * @return PublicRegex
+	 */
+	// 
 	public static PublicRegexp deepCopy(Regexp re) {
 		PublicRegexp publicRegexp = new PublicRegexp(re);
 		
@@ -26,35 +57,121 @@ public class PublicRegexp extends Regexp {
 		return publicRegexp;
 	}
 	
-
-	public String getOp() {
-		return this.op.toString();
+	
+	/**
+	 * Public enum of op types
+	 * This enum is identical to Regex.Op
+	 * @author zuozhi
+	 *
+	 */
+	public enum PublicOp {
+	    NO_MATCH,           // Matches no strings.
+	    EMPTY_MATCH,        // Matches empty string.
+	    LITERAL,            // Matches runes[] sequence
+	    CHAR_CLASS,         // Matches Runes interpreted as range pair list
+	    ANY_CHAR_NOT_NL,    // Matches any character except '\n'
+	    ANY_CHAR,           // Matches any character
+	    BEGIN_LINE,         // Matches empty string at end of line
+	    END_LINE,           // Matches empty string at end of line
+	    BEGIN_TEXT,         // Matches empty string at beginning of text
+	    END_TEXT,           // Matches empty string at end of text
+	    WORD_BOUNDARY,      // Matches word boundary `\b`
+	    NO_WORD_BOUNDARY,   // Matches word non-boundary `\B`
+	    CAPTURE,            // Capturing subexpr with index cap, optional name name
+	    STAR,               // Matches subs[0] zero or more times.
+	    PLUS,               // Matches subs[0] one or more times.
+	    QUEST,              // Matches subs[0] zero or one times.
+	    REPEAT,             // Matches subs[0] [min, max] times; max=-1 => no limit.
+	    CONCAT,             // Matches concatenation of subs[]
+	    ALTERNATE,          // Matches union of subs[]
+	    
+	    // Pseudo ops, used internally by Parser for parsing stack:
+	    // These shouldn't be in the final parse tree
+	    LEFT_PAREN,
+	    VERTICAL_BAR;
 	}
 	
+	/**
+	 * This function converts Regex.Op to PublicRegex.PublicOp and returns it
+	 * @return PublicRegex.PublicOp
+	 */
+	public PublicOp getOp() {
+		try {
+			return PublicOp.valueOf(this.op.toString());
+		} catch (IllegalArgumentException e) {
+			return PublicOp.NO_MATCH;
+		}
+		
+	}
+	
+	/**
+	 * @return int indicating flags
+	 */
 	public int getFlags() {
 		return this.flags;
 	}
 	
+	/**
+	 * @return an array of subexpressions with type PublicRegexp
+	 */
 	public PublicRegexp[] getSubs() {
 		return this.publicSubs;
 	}
 	
+	/**
+	 * runes represents character classes,
+	 * for example,
+	 * regex: [a-z], runes: [a,z]
+	 * regex: [a-cx-z], runes: [a,c,x,z]
+	 * 
+	 * @return an array of runes
+	 */
 	public int[] getRunes() {
 		return this.runes;
 	}
 	
+	/**
+	 * min and max are used for repetitions
+	 * for example,
+	 * regex: a{3,5}, min will be 3, max will be 5
+	 * @return int indicating minimum number of repetitions
+	 */
 	public int getMin() {
 		return this.min;
 	}
 	
+	/**
+	 * min and max are used for repetitions
+	 * for example,
+	 * regex: a{3,5}, min will be 3, max will be 5
+	 * @return int indicating maxinum number of repetitions
+	 */
 	public int getMax() {
 		return this.max;
 	}
 	
+	/**
+	 * cap is the capturing index
+	 * expressions in () makes it a capture group, 
+	 * the entire regex's capturing index is 0, other capturing groups' indexes start from 1
+	 * 
+	 * for example,
+	 * regex: (a)(b)
+	 * for "(a)", cap will be 1, for "(b)", cap will be 2
+	 * @return int indicating capture index
+	 */
 	public int getCap() {
 		return this.cap;
 	}
 	
+	
+	/**
+	 * name is capturing group's name (if any)
+	 * for example,
+	 * regex: (?<name1>a)(?<name2>b)
+	 * for "(?<name1>a)", cap name will be name1, for "(?<name2>b)", cap name will be name2
+	 * @return int indicating capture index
+	 */
 	public String getCapName() {
 		return this.name;
 	}
