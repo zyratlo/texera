0623fd45853f9132492912bcd3285ba9581fdeb9
zuozhiw <zuozhiw@uci.edu>
2016-05-10 14:00:35 -0700

    merge from re2j importing

--- textdb/textdb-dataflow/src/main/java/com/google/re2j/PublicParser.java
+++ textdb/textdb-dataflow/src/main/java/com/google/re2j/PublicParser.java
@@ -1,7 +1,21 @@
 package com.google.re2j;
 
+/**
+ * Wrapper class for re2j.Parser
+ * @author zuozhi
+ *
+ */
 public class PublicParser{
 
+	/**
+	 * parse will call re2j.Parser.parse
+	 * it parses a string to an Regexp object, which represents the abstract syntax tree
+	 * the Regexp object will be converted to a PublicRegex object before return
+	 * @param pattern
+	 * @param flags
+	 * @return PublicRegexp
+	 * @throws PatternSyntaxException
+	 */
 	public static PublicRegexp parse(String pattern, int flags) throws PatternSyntaxException {
 		return PublicRegexp.deepCopy(Parser.parse(pattern, flags));
 	}
--- textdb/textdb-dataflow/src/main/java/com/google/re2j/PublicSimplify.java
+++ textdb/textdb-dataflow/src/main/java/com/google/re2j/PublicSimplify.java
@@ -1,7 +1,19 @@
 package com.google.re2j;
 
+/**
+ * Wrapper class for re2j.Simplify
+ * @author zuozhi
+ *
+ */
 public class PublicSimplify{
 	
+	/**
+	 * this method simplifies a given Regexp to an equivalent one,
+	 * after applying various simplifications
+	 * for example, x{1,2} will be simplified to xx?
+	 * @param re
+	 * @return
+	 */
 	public static PublicRegexp simplify(Regexp re) {
 		return PublicRegexp.deepCopy(Simplify.simplify(re));
 	}
