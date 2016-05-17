2b408a3fd46b7b6a9c240f8bb522b23a005e47c7
zuozhiw <zuozhiw@uci.edu>
2016-05-17 14:36:29 -0700

    move regex matching logic to RegexMatcher

--- textdb/textdb-dataflow/src/main/java/edu/uci/ics/textdb/dataflow/common/RegexPredicate.java
+++ textdb/textdb-dataflow/src/main/java/edu/uci/ics/textdb/dataflow/common/RegexPredicate.java
@@ -1,28 +1,14 @@
 package edu.uci.ics.textdb.dataflow.common;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.apache.lucene.analysis.Analyzer;
-import org.apache.lucene.queryparser.classic.MultiFieldQueryParser;
-import org.apache.lucene.queryparser.classic.ParseException;
-import org.apache.lucene.queryparser.classic.QueryParser;
-import org.apache.lucene.search.Query;
 
 import edu.uci.ics.textdb.api.common.Attribute;
 import edu.uci.ics.textdb.api.common.FieldType;
-import edu.uci.ics.textdb.api.common.IField;
 import edu.uci.ics.textdb.api.common.IPredicate;
-import edu.uci.ics.textdb.api.common.ITuple;
-import edu.uci.ics.textdb.api.dataflow.ISourceOperator;
 import edu.uci.ics.textdb.api.storage.IDataStore;
-import edu.uci.ics.textdb.common.constants.DataConstants;
-import edu.uci.ics.textdb.common.exception.DataFlowException;
-import edu.uci.ics.textdb.common.field.Span;
-import edu.uci.ics.textdb.dataflow.regexmatch.RegexToGramQueryTranslator;
-import edu.uci.ics.textdb.dataflow.source.IndexBasedSourceOperator;
-import edu.uci.ics.textdb.storage.DataReaderPredicate;
 
 /**
  * This class is responsible for calculating regex match. <br>
@@ -36,117 +22,37 @@ import edu.uci.ics.textdb.storage.DataReaderPredicate;
 public class RegexPredicate implements IPredicate {
 
 	private String regex;
+	private List<String> fieldNameList;
+	
 	private Analyzer analyzer;
-	private List<String> fields;
-	private Query luceneQuery;
-	private ISourceOperator sourceOperator;
-	
-	private enum RegexEngine {
-		JavaRegex,
-		RE2J
-	}
-	private RegexEngine regexEngine;
-	
+	private IDataStore dataStore;	
 
-	public RegexPredicate(String regex, List<Attribute> attributeList, Analyzer analyzer, IDataStore dataStore)
-			throws DataFlowException{
-		try {
-			this.regex = regex;
-			this.analyzer = analyzer;		
-			this.fields = attributeList.stream()
-					.filter(attr -> (attr.getFieldType() == FieldType.TEXT || attr.getFieldType() == FieldType.STRING))
-					.map(attr -> attr.getFieldName())
-					.collect(Collectors.toList());
-			
-			// try to use RE2J
-			try {
-				com.google.re2j.Pattern.compile(regex);
-				regexEngine = RegexEngine.RE2J;
-				this.luceneQuery = generateQuery(this.regex, this.fields, 
-						RegexToGramQueryTranslator.translate(this.regex).getLuceneQueryString());
-			// if RE2J failes, try to use Java Regex
-			} catch (com.google.re2j.PatternSyntaxException re2jException) {
-				java.util.regex.Pattern.compile(regex);
-				regexEngine = RegexEngine.JavaRegex;
-				this.luceneQuery = generateQuery(this.regex, this.fields, DataConstants.SCAN_QUERY);
-			}
-			
-			this.sourceOperator = new IndexBasedSourceOperator(new DataReaderPredicate(dataStore, luceneQuery));
-		} catch (ParseException | java.util.regex.PatternSyntaxException e) {
-			throw new DataFlowException(e.getMessage(), e);
-		}
+
+	public RegexPredicate(String regex, List<Attribute> attributeList, Analyzer analyzer, IDataStore dataStore) {
+		this.regex = regex;
+		this.analyzer = analyzer;
+		this.dataStore = dataStore;
+		this.fieldNameList = attributeList.stream()
+				.filter(attr -> (attr.getFieldType() == FieldType.TEXT || attr.getFieldType() == FieldType.STRING))
+				.map(attr -> attr.getFieldName())
+				.collect(Collectors.toList());
 	}
 
-	private Query generateQuery(String regexStr, List<String> fields, String queryStr) throws ParseException {
-		String[] fieldsArray = new String[fields.size()];
-		QueryParser parser = new MultiFieldQueryParser(fields.toArray(fieldsArray), analyzer);
-		return parser.parse(queryStr);
-	}
 
-	/**
-	 * This function returns a list of spans in the given tuple that match the
-	 * regex For example, given tuple ("george watson", "graduate student", 23,
-	 * "(949)888-8888") and regex "g[^\s]*", this function will return
-	 * [Span(name, 0, 6, "g[^\s]*", "george
-	 * watson"), Span(position, 0, 8, "g[^\s]*", "graduate student")]
-	 * 
-	 * @param tuple
-	 *            document in which search is performed
-	 * @return a list of spans describing the occurrence of a matching sequence
-	 *         in the document
-	 */
-	public List<Span> computeMatches(ITuple tuple) {
-		List<Span> spanList = new ArrayList<>();
-		if (tuple == null) {
-			return spanList; // empty array
-		}
-		for (String fieldName : fields) {
-			IField field = tuple.getField(fieldName);
-			String fieldValue = field.getValue().toString();
-			if (fieldValue == null) {
-				return spanList;
-			} else {
-				switch (regexEngine) {
-				case JavaRegex:
-					javaRegexMatch(fieldValue, fieldName, spanList);
-					break;
-				case RE2J:
-					re2jRegexMatch(fieldValue, fieldName, spanList);
-					break;
-				}
-			}
-		}
-		return spanList;
-	}
-	
-	private void javaRegexMatch(String fieldValue, String fieldName, List<Span> spanList) {
-		java.util.regex.Matcher javaMatcher = 
-				java.util.regex.Pattern.compile(this.regex)
-				.matcher(fieldValue);
-		while (javaMatcher.find()) {
-			spanList.add(new Span(fieldName, javaMatcher.start(), javaMatcher.end(), regex, fieldValue));
-		}
-	}
-	
-	private void re2jRegexMatch(String fieldValue, String fieldName, List<Span> spanList) {
-		com.google.re2j.Matcher re2jMatcher = 
-		com.google.re2j.Pattern.compile(this.regex)
-		.matcher(fieldValue);
-		while (re2jMatcher.find()) {
-			spanList.add(new Span(fieldName, re2jMatcher.start(), re2jMatcher.end(), regex, fieldValue));
-		}
-	}
-	
 	public String getRegex() {
 		return regex;
 	}
-
-	public ISourceOperator getSourceOperator() {
-		return this.sourceOperator;
+	
+	public Analyzer getAnalyzer() {
+		return this.analyzer;
 	}
-
-	public List<String> getFields() {
-		return this.fields;
+	
+	public IDataStore getDataStore() {
+		return this.dataStore;
+	}
+	
+	public List<String> getFieldNameList() {
+		return this.fieldNameList;
 	}
 	
 }
--- textdb/textdb-dataflow/src/main/java/edu/uci/ics/textdb/dataflow/regexmatch/RegexMatcher.java
+++ textdb/textdb-dataflow/src/main/java/edu/uci/ics/textdb/dataflow/regexmatch/RegexMatcher.java
@@ -3,48 +3,88 @@ package edu.uci.ics.textdb.dataflow.regexmatch;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.queryparser.classic.MultiFieldQueryParser;
+import org.apache.lucene.queryparser.classic.ParseException;
+import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.search.Query;
+
 import edu.uci.ics.textdb.api.common.IField;
 import edu.uci.ics.textdb.api.common.IPredicate;
 import edu.uci.ics.textdb.api.common.ITuple;
 import edu.uci.ics.textdb.api.common.Schema;
 import edu.uci.ics.textdb.api.dataflow.IOperator;
 import edu.uci.ics.textdb.api.dataflow.ISourceOperator;
+import edu.uci.ics.textdb.api.storage.IDataStore;
+import edu.uci.ics.textdb.common.constants.DataConstants;
 import edu.uci.ics.textdb.common.exception.DataFlowException;
 import edu.uci.ics.textdb.common.field.DataTuple;
 import edu.uci.ics.textdb.common.field.ListField;
 import edu.uci.ics.textdb.common.field.Span;
 import edu.uci.ics.textdb.common.utils.Utils;
 import edu.uci.ics.textdb.dataflow.common.RegexPredicate;
+import edu.uci.ics.textdb.dataflow.source.IndexBasedSourceOperator;
+import edu.uci.ics.textdb.storage.DataReaderPredicate;
 
 /**
  * Created by chenli on 3/25/16.
  * @author laishuying
  */
 public class RegexMatcher implements IOperator {
-    private final RegexPredicate regexPredicate;
+    private RegexPredicate regexPredicate;
     
-    private ISourceOperator sourceOperator;
-    private List<IField> fields;
+    private String regex;
+    private List<String> fieldNameList;
+    
     private Schema sourceTupleSchema = null;
     private Schema spanSchema = null;
     
-    private List<Span> spans;
+	private Analyzer analyzer;
+	private IDataStore dataStore;
+	private Query luceneQuery;
+	private ISourceOperator sourceOperator;
+    
+    private List<Span> spanList;
+    
+	private enum RegexEngine {
+		JavaRegex,
+		RE2J
+	}
+	private RegexEngine regexEngine;
 
-    public RegexMatcher(IPredicate predicate) {
-    	this.regexPredicate = (RegexPredicate)predicate;
-    	sourceOperator = regexPredicate.getSourceOperator();
+    public RegexMatcher(IPredicate predicate) throws DataFlowException{
+    	this.regexPredicate = (RegexPredicate) predicate;
+    	this.regex = regexPredicate.getRegex();
+    	this.fieldNameList = regexPredicate.getFieldNameList();
+    	this.analyzer = regexPredicate.getAnalyzer();
+    	this.dataStore = regexPredicate.getDataStore();
+    	
+		try {			
+			// try to translate to LuceneQuery using RegexToGramQueryTranslator
+			try {
+				com.google.re2j.Pattern.compile(regexPredicate.getRegex());
+				regexEngine = RegexEngine.RE2J;
+				this.luceneQuery = generateLuceneQuery(regex, fieldNameList,
+						RegexToGramQueryTranslator.translate(regex).getLuceneQueryString());
+			// if RE2J failes, try to use Java Regex
+			} catch (com.google.re2j.PatternSyntaxException re2jException) {
+				java.util.regex.Pattern.compile(regex);
+				regexEngine = RegexEngine.JavaRegex;
+				this.luceneQuery = generateLuceneQuery(regex, fieldNameList,
+						DataConstants.SCAN_QUERY);
+			}
+			this.sourceOperator = new IndexBasedSourceOperator(new DataReaderPredicate(dataStore, luceneQuery));
+		} catch (ParseException | java.util.regex.PatternSyntaxException e) {
+			throw new DataFlowException(e.getMessage(), e);
+		}
     }
-
-    @Override
-    public void open() throws DataFlowException {
-        try {
-            sourceOperator.open();
-        } catch (Exception e) {
-            e.printStackTrace();
-            throw new DataFlowException(e.getMessage(), e);
-        }
-    }
-
+    
+	private Query generateLuceneQuery(String regexStr, List<String> fields, String queryStr) throws ParseException {
+		String[] fieldsArray = new String[fields.size()];
+		QueryParser parser = new MultiFieldQueryParser(fields.toArray(fieldsArray), analyzer);
+		return parser.parse(queryStr);
+	}
+	
     @Override
     public ITuple getNextTuple() throws DataFlowException {
 		try {
@@ -53,17 +93,17 @@ public class RegexMatcher implements IOperator {
                 return null;
             }  
             
-            spans = regexPredicate.computeMatches(sourceTuple);
+            this.spanList = computeMatches(sourceTuple);
             
-            if (spans != null && spans.size() != 0) { // a list of matches found
+            if (spanList != null && spanList.size() != 0) { // a list of matches found
             	if (sourceTupleSchema == null) {
             		sourceTupleSchema = sourceTuple.getSchema();
             	}
             	if (spanSchema == null) {
             		spanSchema = Utils.createSpanSchema(sourceTupleSchema);
             	}
-            	fields = sourceTuple.getFields();
-            	return constructSpanTuple(spans);
+            	List<IField> fields = sourceTuple.getFields();
+            	return constructSpanTuple(fields, this.spanList);
             } else { // no match found
             	return getNextTuple();
             }
@@ -73,17 +113,81 @@ public class RegexMatcher implements IOperator {
         }        
     }
     
-    private ITuple constructSpanTuple(List<Span> spans) {
+    private ITuple constructSpanTuple(List<IField> fields, List<Span> spans) {
     	List<IField> fieldListDuplicate = new ArrayList<>(fields);
     	IField spanListField = new ListField<Span>(spans);
     	fieldListDuplicate.add(spanListField);
-    	
     	IField[]  fieldsDuplicate = fieldListDuplicate.toArray(new IField[fieldListDuplicate.size()]);
     	return new DataTuple(spanSchema, fieldsDuplicate);
     }
-    
+	
+	/**
+	 * This function returns a list of spans in the given tuple that match the
+	 * regex For example, given tuple ("george watson", "graduate student", 23,
+	 * "(949)888-8888") and regex "g[^\s]*", this function will return
+	 * [Span(name, 0, 6, "g[^\s]*", "george
+	 * watson"), Span(position, 0, 8, "g[^\s]*", "graduate student")]
+	 * 
+	 * @param tuple
+	 *            document in which search is performed
+	 * @return a list of spans describing the occurrence of a matching sequence
+	 *         in the document
+	 */
+	public List<Span> computeMatches(ITuple tuple) {
+		List<Span> spanList = new ArrayList<>();
+		if (tuple == null) {
+			return spanList; // empty array
+		}
+		for (String fieldName : fieldNameList) {
+			IField field = tuple.getField(fieldName);
+			String fieldValue = field.getValue().toString();
+			if (fieldValue == null) {
+				return spanList;
+			} else {
+				switch (regexEngine) {
+				case JavaRegex:
+					javaRegexMatch(fieldValue, fieldName, spanList);
+					break;
+				case RE2J:
+					re2jRegexMatch(fieldValue, fieldName, spanList);
+					break;
+				}
+			}
+		}
+		return spanList;
+	}
+	
+	private void javaRegexMatch(String fieldValue, String fieldName, List<Span> spanList) {
+		java.util.regex.Matcher javaMatcher = 
+				java.util.regex.Pattern.compile(this.regexPredicate.getRegex())
+				.matcher(fieldValue);
+		while (javaMatcher.find()) {
+			spanList.add(new Span(fieldName, javaMatcher.start(), javaMatcher.end(), this.regexPredicate.getRegex(), fieldValue));
+		}
+	}
+	
+	private void re2jRegexMatch(String fieldValue, String fieldName, List<Span> spanList) {
+		com.google.re2j.Matcher re2jMatcher = 
+		com.google.re2j.Pattern.compile(this.regexPredicate.getRegex())
+		.matcher(fieldValue);
+		while (re2jMatcher.find()) {
+			spanList.add(new Span(fieldName, re2jMatcher.start(), re2jMatcher.end(), this.regexPredicate.getRegex(), fieldValue));
+		}
+	}
+
+
     public Schema getSpanSchema() {
     	return spanSchema;
+    }
+    
+    @Override
+    public void open() throws DataFlowException {
+        try {
+            sourceOperator.open();
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new DataFlowException(e.getMessage(), e);
+        }
     }
 
     @Override
