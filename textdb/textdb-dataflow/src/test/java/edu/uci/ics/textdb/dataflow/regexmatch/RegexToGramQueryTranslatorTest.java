d352772e6e751e38abefe1e46096be5e7f0dffe5
zuozhiw <zuozhiw@uci.edu>
2016-05-17 18:44:45 -0700

    add printQueryTree

--- textdb/textdb-dataflow/src/test/java/edu/uci/ics/textdb/dataflow/regexmatch/RegexToGramQueryTranslatorTest.java
+++ textdb/textdb-dataflow/src/test/java/edu/uci/ics/textdb/dataflow/regexmatch/RegexToGramQueryTranslatorTest.java
@@ -9,6 +9,31 @@ import org.junit.Test;
  */
 
 public class RegexToGramQueryTranslatorTest {
+	private int indentation = 0;
+	private String indentStr = "    ";
+	
+	private String printQueryTree(GramBooleanQuery query) {
+		String s = "";
+		for (int i = 0; i < indentation; i++) {
+			s += indentStr;
+		}
+		s += query.operator.toString();
+		s += "\n";
+		
+		indentation++;
+		for (String operand : query.operandList) {
+			for (int i = 0; i < indentation; i++) {
+				s += indentStr;
+			}
+			s += operand;
+			s += "\n";
+		}
+		for (GramBooleanQuery subQuery : query.subQueryList) {
+			s += printQueryTree(subQuery);
+		}
+		indentation--;
+		return s;
+	}
 	
 	@Test
 	public void testEmptyRegex() {
