d01891835bcb8b2ccc4bda0c5d32348783a0892e
prakul <discover.prakul@gmail.com>
2016-05-21 15:26:34 -0700

    Phrase Search

--- textdb/textdb-dataflow/src/main/java/edu/uci/ics/textdb/dataflow/keywordmatch/KeywordMatcher.java
+++ textdb/textdb-dataflow/src/main/java/edu/uci/ics/textdb/dataflow/keywordmatch/KeywordMatcher.java
@@ -1,6 +1,8 @@
 package edu.uci.ics.textdb.dataflow.keywordmatch;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import edu.uci.ics.textdb.api.common.Attribute;
 import edu.uci.ics.textdb.api.common.IField;
@@ -8,6 +10,7 @@ import edu.uci.ics.textdb.api.common.IPredicate;
 import edu.uci.ics.textdb.api.common.ITuple;
 import edu.uci.ics.textdb.api.dataflow.IOperator;
 import edu.uci.ics.textdb.api.dataflow.ISourceOperator;
+import edu.uci.ics.textdb.common.constants.DataConstants.KeywordOperatorType;
 import edu.uci.ics.textdb.common.constants.SchemaConstants;
 import edu.uci.ics.textdb.common.exception.DataFlowException;
 import edu.uci.ics.textdb.common.field.Span;
@@ -93,13 +96,13 @@ public class KeywordMatcher implements IOperator {
             int schemaIndex = sourceTuple.getSchema().getIndex(SchemaConstants.SPAN_LIST_ATTRIBUTE.getFieldName());
             List<Span> spanList =
                     (List<Span>)sourceTuple.getField(schemaIndex).getValue();
-
+            Collections.sort(spanList, tokenOffsetComp);
             for(int attributeIndex = 0; attributeIndex < attributeList.size(); attributeIndex++) {
                 String fieldName = attributeList.get(attributeIndex).getFieldName();
                 IField field = sourceTuple.getField(fieldName);
-                if (!(field instanceof TextField)) {
+                String fieldValue = (String) (field).getValue();
 
-                    String fieldValue = (String) (field).getValue();
+                if (!(field instanceof TextField)) {
 
                     //Keyword should match fieldValue entirely
                     if (fieldValue.equals(query)) {
@@ -124,10 +127,66 @@ public class KeywordMatcher implements IOperator {
                     }
 
                     boolean allTokenPresent = areAllTrue(tokensPresent);
+                    if(predicate.getOperatorType() == KeywordOperatorType.BASIC) {
 
-                    if (!allTokenPresent) {
-                        spanList.removeAll(spanForThisField);
+                        if (!allTokenPresent) {
+                            spanList.removeAll(spanForThisField);
+                        }
                     }
+
+                    else if(predicate.getOperatorType() == KeywordOperatorType.PHRASE){
+
+                        spanList.removeAll(spanForThisField);
+                        //relevantWordsInQuery has all the tokens (including duplicates but excluding stop words) in the query.
+                        List<String> relevantWordsInQuery = new ArrayList<>();
+                        //relevantWordsInQueryOffset maintains the offset position of tokens in query
+                        List<Integer> relevantWordsInQueryOffset = new ArrayList<>();
+                        String[] queryArray = query.split(" ");
+                        if (allTokenPresent) {
+                            for(int iter = 0; iter < queryArray.length; iter++){
+                                if(queryTokens.contains(queryArray[iter])){
+                                    relevantWordsInQuery.add(queryArray[iter]);
+                                    relevantWordsInQueryOffset.add(iter);
+                                }
+                            }
+
+                            int iter = 0; // maintains position of term being checked in spanForThisField list
+                            while(iter < spanForThisField.size()){
+
+                                /*Verify if span in the spanForThisField correspond to our phrase query, ie relative position offsets should be similar
+                                and the value should be same.
+                                *Ex:
+                                * if Query is "lin clooney and angry"
+                                * and document is "Lin Clooney is Short and lin clooney is Angry"
+                                * we want to return combined span "lin clooney is angry" and ignore "Lin Clooney"
+                                *
+                                 */
+                                int flag=0;// flag checks if a mismatch in spans occurs
+                                if(iter <= spanForThisField.size()-relevantWordsInQuery.size()){
+                                    int i; // To check all the terms in query are verified
+                                    for(i=0; i < relevantWordsInQuery.size()-1; i++) {
+                                        Span A = spanForThisField.get(iter+i);
+                                        Span B = spanForThisField.get(iter +i+ 1);
+                                        if (!(B.getTokenOffset() - A.getTokenOffset() == relevantWordsInQueryOffset.get(i+1) - relevantWordsInQueryOffset.get(i) &&
+                                                A.getValue().equalsIgnoreCase(relevantWordsInQuery.get(i)) && B.getValue().equalsIgnoreCase(relevantWordsInQuery.get(i+1)))) {
+                                            iter++;
+                                            flag=1;
+                                        }
+                                    }
+                                    if(flag==1)continue;
+                                }
+
+                                int combinedSpanStartIndex = spanForThisField.get(iter).getStart();
+                                int combinedSpanEndIndex = spanForThisField.get(iter+relevantWordsInQuery.size()-1).getEnd();
+
+                                Span combinedSpan = new Span(fieldName, combinedSpanStartIndex, combinedSpanEndIndex, query, fieldValue.substring(combinedSpanStartIndex, combinedSpanEndIndex));
+                                spanList.add(combinedSpan);
+                                iter = iter + relevantWordsInQuery.size();
+                            }
+                        }
+                    }
+
+
                 }
             }
 
@@ -149,6 +208,13 @@ public class KeywordMatcher implements IOperator {
             throw new DataFlowException(e.getMessage(), e);
         }
     }
+
+    public static Comparator<Span> tokenOffsetComp = new Comparator<Span>() {
+        @Override
+        public int compare(Span o1, Span o2) {
+            return o1.getTokenOffset()-o2.getTokenOffset();
+        }
+    };
 
     public static boolean areAllTrue(boolean[] array)
     {
