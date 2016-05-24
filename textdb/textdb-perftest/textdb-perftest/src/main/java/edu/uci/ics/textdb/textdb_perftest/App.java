a5b960bbd157b77857472ce93f3c1cf05a8c3f64
zuozhiw <zuozhiw@uci.edu>
2016-05-24 12:36:54 -0700

    Add textdb-perftest project

---
+++ textdb/textdb-perftest/textdb-perftest/src/main/java/edu/uci/ics/textdb/textdb_perftest/App.java
@@ -0,0 +1,13 @@
+package edu.uci.ics.textdb.textdb_perftest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
