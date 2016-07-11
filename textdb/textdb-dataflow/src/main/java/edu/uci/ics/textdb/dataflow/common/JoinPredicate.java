14135ebecf4c8522eaf4377425729a35989a0bd4
sripadks <sripadkowshik@gmail.com>
2016-07-11 16:14:30 -0700

    Added comments and examples and made changes to method types. Also removed unused code

--- textdb/textdb-dataflow/src/main/java/edu/uci/ics/textdb/dataflow/common/JoinPredicate.java
+++ textdb/textdb-dataflow/src/main/java/edu/uci/ics/textdb/dataflow/common/JoinPredicate.java
@@ -3,39 +3,46 @@ package edu.uci.ics.textdb.dataflow.common;
 import edu.uci.ics.textdb.api.common.Attribute;
 import edu.uci.ics.textdb.api.common.IPredicate;
 
+/**
+ * 
+ * @author sripadks
+ *
+ */
 public class JoinPredicate implements IPredicate {
 	
-	//Reserved for future
-//	private enum CompOp {
-//		NO_OP,	//No comparision
-//		EQ_OP,	//==
-//		NEQ_OP,	//!=
-//		LT_OP,	//<
-//		GT_OP,	//>
-//		LTEQ_OP,//<=
-//		GTEQ_OP	//>=
-//	}
-	
-	private Integer outerAttributeId;
+	private Attribute outerIdAttribute;
 	private Attribute outerJoinAttribute;
-	private Integer innerAttributeId;
+	private Attribute innerIdAttribute;
 	private Attribute innerJoinAttribute;
 	private Integer threshold;
 	
-	public JoinPredicate(Integer outerAttributeId, Attribute outerJoinAttribute, Integer innerAttributeId, Attribute innerJoinAttribute, Integer threshold) {
-		this.outerAttributeId = outerAttributeId;
+	/**
+	 * This constructor is used to set the parameters required for the Join Operator.
+	 * 
+	 * JoinPredicate joinPre = new JoinPredicate(ID_ATTR, DESCRIPTION_ATTR, ID_ATTR, DESCRIPTION_ATTR, 10)
+	 * will create a predicate that compares the spans of type DESCRIPTION_ATTR of outer and inner operator
+	 * output tuples which have the ID_ATTR id attribute to be within 10 characters of each other.
+	 * 
+	 * @param outerIdAttribute is the ID attribute of the outer operator
+	 * @param outerJoinAttribute is the attribute of the outer operator to be used for join
+	 * @param innerIdAttribute is the ID attribute of the inner operator
+	 * @param innerJoinAttribute is the ID of the inner operator to be used for join
+	 * @param threshold is the maximum distance (in characters) between any two spans
+	 */
+	public JoinPredicate(Attribute outerIdAttribute, Attribute outerJoinAttribute, Attribute innerIdAttribute, Attribute innerJoinAttribute, Integer threshold) {
+		this.outerIdAttribute = outerIdAttribute;
 		this.outerJoinAttribute = outerJoinAttribute;
-		this.innerAttributeId = innerAttributeId;
+		this.innerIdAttribute = innerIdAttribute;
 		this.innerJoinAttribute = innerJoinAttribute;
 		this.threshold = threshold;
 	}
 	
-	public Integer getOuterAttrId() {
-		return outerAttributeId;
+	public Attribute getOuterAttrId() {
+		return this.outerIdAttribute;
 	}
 	
-	public Integer getInnerAttrId() {
-		return innerAttributeId;
+	public Attribute getInnerAttrId() {
+		return this.innerIdAttribute;
 	}
 	
 	public Attribute getOuterAttr() {
@@ -43,10 +50,10 @@ public class JoinPredicate implements IPredicate {
 	}
 	
 	public Attribute getInnerAttr() {
-		return innerJoinAttribute;
+		return this.innerJoinAttribute;
 	}
 	
 	public Integer getThreshold() {
-		return threshold;
+		return this.threshold;
 	}
 }
--- textdb/textdb-dataflow/src/main/java/edu/uci/ics/textdb/dataflow/join/Join.java
+++ textdb/textdb-dataflow/src/main/java/edu/uci/ics/textdb/dataflow/join/Join.java
@@ -3,16 +3,28 @@ package edu.uci.ics.textdb.dataflow.join;
 import edu.uci.ics.textdb.api.common.IPredicate;
 import edu.uci.ics.textdb.api.common.ITuple;
 import edu.uci.ics.textdb.api.dataflow.IOperator;
-
+/**
+ * 
+ * @author sripadks
+ *
+ */
 public class Join implements IOperator{
 	
-	private ITuple outerTuple;
-	private ITuple innerTuple;
+	private IOperator outer;
+	private IOperator inner;
 	private IPredicate joinPredicate;
 	
-	public Join(ITuple outerTuple, ITuple innerTuple, IPredicate joinPredicate) {
-		this.outerTuple = outerTuple;
-		this.innerTuple = innerTuple;
+	/**
+	 * This constructor is used to set the operators whose output is to be compared and joined and the 
+	 * predicate which specifies the fields and constraints over which join happens.
+	 * 
+	 * @param outer is the outer operator producing the tuples
+	 * @param inner is the inner operator producing the tuples
+	 * @param joinPredicate is the predicate over which the join is made
+	 */
+	public Join(IOperator outer, IOperator inner, IPredicate joinPredicate) {
+		this.outer = outer;
+		this.inner = inner;
 		this.joinPredicate = joinPredicate;
 	}
 
@@ -22,20 +34,38 @@ public class Join implements IOperator{
 		
 	}
 
+	/**
+	 * Gets the next tuple which is a joint of two tuples which passed the criteria set in the JoinPredicate.
+	 * 
+	 * <p>
+	 * Example 1:
+	 * JoinPredicate joinPre = new JoinPredicate(ID_ATTR, DESCRIPTION_ATTR, ID_ATTR, DESCRIPTION_ATTR, 10);
+	 * Join join = new Join(regexMatcher, keywordMatcher, joinPre);
+	 * 
+	 * if the ID's of both the documents are same and if the spans are 10 characters apart, the spans are 
+	 * joined.
+	 * </p>
+	 * <p>
+	 * Example 2:
+	 * JoinPredicate joinPre = new JoinPredicate(ID_ATTR, DESCRIPTION_ATTR, ID_ATTR, AUTHOR_ATTR, 10);
+	 * Join join = new Join(regexMatcher, keywordMatcher, joinPre);
+	 * 
+	 * even if the ID's of both the documents are same, since the attributes to be joined upon are different,
+	 * join won't take place.
+	 * </p>
+	 * 
+	 * @return result the resulting joint tuple
+	 */
 	@Override
 	public ITuple getNextTuple() throws Exception {
 		// TODO Auto-generated method stub
-		return null;
+		ITuple result = null;
+		return result;
 	}
 
 	@Override
 	public void close() throws Exception {
 		// TODO Auto-generated method stub
 		
-	}
-	
-	public ITuple compareAndJoin() {
-		ITuple result = null;
-		return result;
 	}
 }
