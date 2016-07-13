ddfb9730f710a2ff4f39daa19f856c0dcbc871e8
zuozhiw <zuozhiw@uci.edu>
2016-07-13 16:33:51 -0700

    add stopwords

--- textdb/textdb-common/src/main/java/edu/uci/ics/textdb/common/constants/DataConstants.java
+++ textdb/textdb-common/src/main/java/edu/uci/ics/textdb/common/constants/DataConstants.java
@@ -51,6 +51,13 @@ public class DataConstants {
      * doesn't match "nice gentle person" <br>
      * <br>
      * 
+     * A list of stopwords:
+     * "a", "an", "and", "are", "as", "at", "be", "but", "by",
+     * "for", "if", "in", "into", "is", "it",
+     * "no", "not", "of", "on", "or", "such",
+     * "that", "the", "their", "then", "there", "these",
+     * "they", "this", "to", "was", "will", "with"
+     * 
      */
     public static enum KeywordMatchingType {
         SUBSTRING_SCANBASED, 
