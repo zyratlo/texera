e10440c2fd399b016ce695d5ebc957a48d9b775c
Xuxip <haileyxxpan@gmail.com>
2016-07-26 14:46:34 -0700

    add keyword perftest and perftest utils

---
+++ textdb/textdb-perftest/src/main/java/edu/uci/ics/textdb/perftest/keywordmatcher/KeywordMatcherPerformanceTest.java
@@ -0,0 +1,166 @@
+ 
+package edu.uci.ics.textdb.perftest.keywordmatcher;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+
+import edu.uci.ics.textdb.api.common.Attribute;
+import edu.uci.ics.textdb.api.common.IPredicate;
+import edu.uci.ics.textdb.api.common.ITuple;
+import edu.uci.ics.textdb.common.constants.DataConstants;
+import edu.uci.ics.textdb.common.constants.SchemaConstants;
+import edu.uci.ics.textdb.common.constants.DataConstants.KeywordMatchingType;
+import edu.uci.ics.textdb.common.exception.DataFlowException;
+import edu.uci.ics.textdb.common.exception.StorageException;
+import edu.uci.ics.textdb.common.field.ListField;
+import edu.uci.ics.textdb.common.field.Span;
+import edu.uci.ics.textdb.dataflow.common.KeywordPredicate;
+import edu.uci.ics.textdb.dataflow.keywordmatch.KeywordMatcher;
+import edu.uci.ics.textdb.perftest.medline.MedlineIndexWriter;
+import edu.uci.ics.textdb.perftest.utils.PerfTestUtils;
+import edu.uci.ics.textdb.storage.DataStore;
+
+
+/**
+ *@author Hailey Pan
+ * 
+ * This is the performance test of KeywordMatcher
+ * */
+
+public class KeywordMatcherPerformanceTest {
+	
+	private static String HEADER = "Record #,Operator , Min Time, Max Time, Average Time, Std, Average Results\n";
+	private static String basicHeader = "Conjunctive,";
+	private static String phraseHeader = "Phrase,";
+	private static String newLine = "\n";
+	
+ 
+	private static 	List<Double> timeResults = null;
+	private static int totalResultCount = 0;
+	private static String csvFileFolder = "keyword/";
+	 
+	/**
+	 * @param dictFile: this file contains line(s) of queries; the file must be placed in ./data-files/dictionaries/
+	 * @param testCycle: the number of times the test expected to be ran 
+	 * @return 
+	 * 
+	 * This function will match the queries against all indices in ./index/standard/
+	 * 
+	 *Test results includes minimum runtime, maximum runtime, average runtime, the standard deviation and
+	 * the average results for each index, each operator and each test cycle. 
+	 *They are recorded in a csv file that is named by current time and located at
+	 * ./data-files/results/keyword/.
+	 * 
+	 * */
+	public static void runTest(String dictFile, int testCycle) throws StorageException, DataFlowException, IOException{
+		
+		ArrayList<String> queries = PerfTestUtils.readDict(PerfTestUtils.getDictPath(dictFile));
+		
+		FileWriter fileWriter = null;
+		
+		if(!new File(PerfTestUtils.resultFolder, "keyword").exists()){
+			File resultFile = new File(PerfTestUtils.resultFolder+csvFileFolder);
+			resultFile.mkdir();
+		}
+		 
+		String currentTime = PerfTestUtils.formatTime(System.currentTimeMillis());
+		String csvFile = csvFileFolder+currentTime+".csv";
+		fileWriter = new FileWriter(PerfTestUtils.getResultPath(csvFile));
+ 
+		
+		File indexFiles = new File(PerfTestUtils.standardIndexFolder);
+		double avg = 0;
+		for(int i = 1; i <= testCycle; i++){
+			fileWriter.append("Cycle" +i);
+			fileWriter.append(newLine);
+			fileWriter.append(HEADER);
+			for(File file: indexFiles.listFiles()){
+				if (file.getName().startsWith(".")) {
+					continue;
+				}
+				DataStore dataStore = new DataStore(PerfTestUtils.getIndexPath(file.getName()), MedlineIndexWriter.SCHEMA_MEDLINE);
+			
+				fileWriter.append(file.getName()+",");
+				fileWriter.append(basicHeader);
+				clear();
+				match(queries, DataConstants.KeywordMatchingType.CONJUNCTION_INDEXBASED, new StandardAnalyzer(), dataStore);	
+				avg = PerfTestUtils.calculateAverage(timeResults);
+				fileWriter.append(Collections.min(timeResults)+","+Collections.max(timeResults)+","+avg+","+PerfTestUtils.calculateSTD(timeResults, avg)+","+String.format("%.2f", totalResultCount*1.0/queries.size()));
+				fileWriter.append(newLine);
+				
+				fileWriter.append(file.getName()+",");
+				fileWriter.append(phraseHeader);
+				clear();
+				match(queries, DataConstants.KeywordMatchingType.PHRASE_INDEXBASED, new StandardAnalyzer(), dataStore);
+				avg = PerfTestUtils.calculateAverage(timeResults);
+				fileWriter.append(Collections.min(timeResults)+","+Collections.max(timeResults)+","+avg+","+PerfTestUtils.calculateSTD(timeResults, avg)+","+String.format("%.2f", totalResultCount*1.0/queries.size()));
+				fileWriter.append(newLine);
+			 
+			}
+			fileWriter.append(newLine);
+		}
+		
+		 
+		fileWriter.flush();
+	    fileWriter.close();
+		 
+	}
+	
+	/**
+	 * reset timeResults and totalResultCount
+	 * */
+	public static void clear(){
+		timeResults = new ArrayList<Double>();
+		totalResultCount = 0;
+	}
+	
+ 
+	/**
+	 * @param dict:  queries
+	 * @param Optype: operator type
+	 * @param luceneAnalyzer
+	 * @param DataStore
+	 * @return
+	 * 
+	 * This function does match for a list of queries
+	 * */
+	public static void match(ArrayList<String> dict, KeywordMatchingType Optype,Analyzer luceneAnalyzer, DataStore dataStore ) 
+			throws DataFlowException, IOException {
+ 
+		Attribute[] attributeList = new Attribute[]{ MedlineIndexWriter.ABSTRACT_ATTR };
+			
+		for (String query:dict){
+			IPredicate predicate = new KeywordPredicate(query, Arrays.asList(attributeList),
+	        Optype, luceneAnalyzer, dataStore);
+	        KeywordMatcher keywordMatcher = new KeywordMatcher(predicate);
+	        
+	        long startMatchTime = System.currentTimeMillis();
+			keywordMatcher.open();
+			int counter = 0;
+			ITuple nextTuple = null;
+			while ((nextTuple = keywordMatcher.getNextTuple()) != null) {
+				List<Span> spanList = ((ListField<Span>) nextTuple.getField(SchemaConstants.SPAN_LIST)).getValue();
+				counter += spanList.size();
+			}
+			keywordMatcher.close();
+			long endMatchTime = System.currentTimeMillis();
+			double matchTime = (endMatchTime - startMatchTime)/1000.0;
+		
+			timeResults.add(Double.parseDouble(String.format("%.4f", matchTime)));
+			totalResultCount+= counter;
+ 
+			}
+		}
+	
+	 
+}
+ 
---
+++ textdb/textdb-perftest/src/main/java/edu/uci/ics/textdb/perftest/utils/PerfTestUtils.java
@@ -0,0 +1,188 @@
+package edu.uci.ics.textdb.perftest.utils;
+
+/**
+ *@author Hailey Pan
+ *
+ *performance test helper functions 
+ **/
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Scanner;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+
+import edu.uci.ics.textdb.common.constants.DataConstants;
+import edu.uci.ics.textdb.engine.Engine;
+import edu.uci.ics.textdb.perftest.medline.MedlineIndexWriter;
+import edu.uci.ics.textdb.storage.DataStore;
+
+public class PerfTestUtils {
+	
+	public static final String fileFolder = "./data-files/";
+	public static final String standardIndexFolder = "./index/standard/";
+	public static final String trigramIndexFolder = "./index/trigram/";
+	public static final String resultFolder = "./data-files/results/";
+	public static final String dictFolder = "./data-files/dictionaries/";
+	
+	/**
+	 * 
+	 * @param testResults, a list of doubles
+	 * @return the average of the values in testResults
+	 */
+	public static double calculateAverage(List<Double> testResults){
+		double totalTime = 0;
+		for(Double result: testResults){
+			totalTime += result;
+		}
+		
+		return Double.parseDouble(String.format("%.4f", totalTime/testResults.size()));
+	}
+	
+	/**
+	 * @param testResults, a list of doubles
+	 * @param average, the average of the values in testResults
+	 * @return standard deviation of the values in testResults
+	 */
+	public static double calculateSTD(List<Double> testResults, Double average){
+		double numerator =0;
+		for(Double result: testResults){
+			numerator += Math.pow(result-average,2);
+		}
+		
+		return Double.parseDouble(String.format("%.4f", Math.sqrt(numerator/testResults.size())));
+	}
+	
+	/**
+	 * Writes all files in ./data-files/ into indices
+	 * @throws Exception
+	 */
+	public static void writeIndices() throws Exception {
+		File files = new File(fileFolder);
+		for(File file: files.listFiles()){
+			if (file.getName().startsWith(".")) {
+				continue;
+			}
+			if (file.isDirectory()) {
+				continue;
+			}
+			writeIndex(file.getName() , new StandardAnalyzer(),"standard");
+		}
+	
+	}
+	
+	/**
+	 * Writes all files in ./data-files/ into trigram indices
+	 * @throws Exception
+	 */
+	public static void writeTrigramIndices() throws Exception {
+		File files = new File(fileFolder);
+		for(File file: files.listFiles()){
+			if (file.getName().startsWith(".")) {
+				continue;
+			}
+			if (file.isDirectory()) {
+				continue;
+			}
+			writeIndex(file.getName() ,  DataConstants.getTrigramAnalyzer(), "trigram");
+		}
+	
+	}
+
+	/**
+	 * Writes a data file into an index
+	 * 
+	 * @param fileName, data file
+	 * @param luceneAnalyzer
+	 * @param indexType, indicates the types of index, trigram or standard 
+	 * @throws Exception
+	 */
+	public static void writeIndex(String fileName,   Analyzer luceneAnalyzer, String indexType)
+			throws Exception {
+		DataStore dataStore = null;
+		if(indexType.equalsIgnoreCase("trigram")){
+			dataStore = new DataStore(getTrigramIndexPath(fileName.replace(".txt", "")), MedlineIndexWriter.SCHEMA_MEDLINE);
+		}
+		else {
+			dataStore = new DataStore(getIndexPath(fileName.replace(".txt", "")), MedlineIndexWriter.SCHEMA_MEDLINE);
+		}
+		
+		Engine writeIndexEngine = Engine.getEngine();
+
+		writeIndexEngine.evaluate(MedlineIndexWriter.getMedlineIndexPlan(fileFolder+fileName, dataStore, luceneAnalyzer));
+	
+	}
+	
+	/**
+	 * Reads each line in a file into a list
+	 * 
+	 * @param filePath
+	 * @return a list of string
+	 * @throws FileNotFoundException
+	 */
+	public static ArrayList<String> readDict(String filePath) throws FileNotFoundException{
+		ArrayList<String> dict = new ArrayList<String>();
+    	Scanner scanner = new Scanner(new File(filePath));
+    	while (scanner.hasNextLine()){
+    		dict.add(scanner.nextLine().trim());
+    	}
+    	scanner.close();
+    	return dict;
+	}
+	
+	/**
+	 * 
+	 * @param indexName
+	 * @return a path of an index in ./index/standard/
+	 */
+	public static String getIndexPath(String indexName) {
+		 return  standardIndexFolder+indexName;	
+	}
+	
+	/**
+	 * 
+	 * @param indexName
+	 * @return a path of a trigram index in ./index/trigram/
+	 */
+	public static String getTrigramIndexPath(String indexName) {
+		return trigramIndexFolder+indexName; 
+	}
+	
+	/**
+	 * 
+	 * @param resultFileName
+	 * @return a path of a result file in ./data-files/results/
+	 */
+	public static String getResultPath(String resultFileName) {
+			return resultFolder+resultFileName;	 
+	}
+	
+	/**
+	 * 
+	 * @param dictFileName
+	 * @return a path of a data file in ./data-files/dictionaries/
+	 */
+	public static String getDictPath(String dictFileName) {
+			return dictFolder+dictFileName ;	
+		 
+	}
+	
+	/**
+	 * Formats a time to string
+	 * @param time (the milliseconds since January 1, 1970, 00:00:00 GMT)
+	 * @return string representation of the time
+	 */
+	public static String formatTime(long time) {
+        Date date = new Date(time);
+
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd-HHmmss");
+        
+        return sdf.format(date).toString();
+	}
+	
+	
+}
