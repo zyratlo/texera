02425163449c130a649c5e895b2c82d27f8bc4b1
Xuxip <haileyxxpan@gmail.com>
2016-08-04 15:14:24 -0700

    testing:

--- textdb/textdb-perftest/src/main/java/edu/uci/ics/textdb/perftest/runme/WriteIndex.java
+++ textdb/textdb-perftest/src/main/java/edu/uci/ics/textdb/perftest/runme/WriteIndex.java
@@ -6,7 +6,9 @@ package edu.uci.ics.textdb.perftest.runme;
 
 import edu.uci.ics.textdb.perftest.utils.PerfTestUtils;
 
- 
+/*
+ * Run this class to write all necessary index for performance tests!
+ * */
 public class WriteIndex {
 	public static void main(String[] args) {
 		if(args.length !=0){
