ea75a461cf25ef6e7330c86d7d4f881367d15811
zuozhiw <zuozhiw@uci.edu>
2016-09-02 15:19:35 -0700

    add regex builder"

---
+++ textdb/textdb-dataflow/src/main/java/edu/uci/ics/textdb/plangen/operatorbuilder/RegexMatcherBuilder.java
@@ -0,0 +1,20 @@
+package edu.uci.ics.textdb.plangen.operatorbuilder;
+
+/**
+ * RegexMatcherBuilder provides a static function that builds a RegexMatcher.
+ * 
+ * Besides some commonly used properties (properties for attribute list, limit, offset), 
+ * KeywordMatcherBuilder currently needs the following properties:
+ * 
+ *   regex (required)
+ * 
+ * @author Zuozhi Wang
+ *
+ */
+public class RegexMatcherBuilder {
+    
+    public static final String REGEX = "regex";
+    
+    
+
+}
