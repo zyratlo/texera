7d3ac2edb87ae56112dccd7076bd655ee867bac6
zuozhiw <zuozhiw@uci.edu>
2016-09-22 22:50:46 -0700

    add more comments for join builder

--- textdb/textdb-dataflow/src/main/java/edu/uci/ics/textdb/plangen/operatorbuilder/JoinBuilder.java
+++ textdb/textdb-dataflow/src/main/java/edu/uci/ics/textdb/plangen/operatorbuilder/JoinBuilder.java
@@ -17,9 +17,28 @@ import edu.uci.ics.textdb.plangen.PlanGenUtils;
  * 
  * JoinBuilder currently needs the following properties:
  * 
- *   predicate (required)
+ *   predicateType (required)
  *   
  *   properties required for constructing attributeList, see OperatorBuilderUtils.constructAttributeList
+ *   (As of now, join must have one attribute in the attribute list.)
+ *   
+ *   idAttributeName and idAttributeType (required): the name and type of the "ID" attribute
+ *   
+ *   requirements for differnt join predicates:
+ *   
+ *   predicatType CharacterDistance:
+ *      distance (required), the character distance threshold of this join predicate.
+ *   
+ *   Sample JSON representation:
+ *   {
+ *      "predicateType" : "CharacterDistance",
+ *      "distance" : "100",
+ *      
+ *      "attributeName" : "content",
+ *      "attributeType" : "Text",
+ *      "idAttributeName" : "id",
+ *      "idAttributeType" : "String",   
+ *   }
  * 
  * @author Zuozhi Wang
  *
@@ -46,22 +65,34 @@ public class JoinBuilder {
     }
     
     
+    /*
+     * This is an interface to builds a Join Predicate from the given operator properties
+     */
     @FunctionalInterface
     private interface GetJoinPredicate {
         IJoinPredicate getJoinPredicate(Map<String, String> operatorProperties) throws PlanGenException;
     }
     
+    /*
+     * This is a map of join predicates' names to functions that build the corresponding join predicate.
+     */
     private static HashMap<String, GetJoinPredicate> joinPredicateHandlerMap = new HashMap<>();
     static {
         joinPredicateHandlerMap.put(JOIN_CHARACTER_DISTANCE.toLowerCase(), JoinBuilder::getJoinCharDistancePredicate);
     }
     
+    /*
+     * This function returns a Join Predicate from the given join predicate type.
+     */
     private static IJoinPredicate generateJoinPredicate(String joinPredicateType, Map<String, String> operatorProperties) throws PlanGenException {
         PlanGenUtils.planGenAssert(joinPredicateHandlerMap.containsKey(joinPredicateType.toLowerCase()), 
                 "Join predicate type is invalid, it must be one of " + joinPredicateHandlerMap.keySet());
         return joinPredicateHandlerMap.get(joinPredicateType.toLowerCase()).getJoinPredicate(operatorProperties);
     }
     
+    /*
+     * This functions builds a JoinDistancePredicate, which is a join predicate of character distance.
+     */
     private static JoinDistancePredicate getJoinCharDistancePredicate(
             Map<String, String> operatorProperties) throws PlanGenException{
         String distanceStr = OperatorBuilderUtils.getRequiredProperty(JOIN_DISTANCE, operatorProperties);
