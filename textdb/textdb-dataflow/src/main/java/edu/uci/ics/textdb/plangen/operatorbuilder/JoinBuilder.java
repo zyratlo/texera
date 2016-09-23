8ccc12b3f336291543e77ca832a9e869a9da1747
zuozhiw <zuozhiw@uci.edu>
2016-09-22 22:39:48 -0700

    Revert "add join builder"
    This reverts commit 1cd09c7d84f7341cff748e1ee9e170b1c947da84.

--- textdb/textdb-dataflow/src/main/java/edu/uci/ics/textdb/plangen/operatorbuilder/JoinBuilder.java
+++ textdb/textdb-dataflow/src/main/java/edu/uci/ics/textdb/plangen/operatorbuilder/JoinBuilder.java
@@ -1,15 +1,13 @@
 package edu.uci.ics.textdb.plangen.operatorbuilder;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+import org.json.JSONObject;
 
 import edu.uci.ics.textdb.api.common.Attribute;
 import edu.uci.ics.textdb.api.dataflow.IOperator;
 import edu.uci.ics.textdb.common.exception.PlanGenException;
-import edu.uci.ics.textdb.dataflow.common.IJoinPredicate;
-import edu.uci.ics.textdb.dataflow.common.JoinDistancePredicate;
-import edu.uci.ics.textdb.dataflow.join.Join;
 import edu.uci.ics.textdb.plangen.PlanGenUtils;
 
 
@@ -27,71 +25,23 @@ import edu.uci.ics.textdb.plangen.PlanGenUtils;
  */
 public class JoinBuilder {
     
-    public static final String JOIN_PREDICATE = "predicateType";
+    public static final String JOIN_PREDICATE = "predicate";
     
-    public static final String JOIN_ID_ATTRIBUTE_NAME = "idAttributeName";
-    public static final String JOIN_ID_ATTRIBUTE_TYPE = "idAttributeType";
-    
-    public static final String JOIN_CHARACTER_DISTANCE = "characterDistance";
-    
+    public static IOperator buildOperator(Map<String, String> operatorProperties) throws PlanGenException {
+        String joinPredicateString = OperatorBuilderUtils.getRequiredProperty(JOIN_PREDICATE, operatorProperties);
         
-    public static IOperator buildOperator(Map<String, String> operatorProperties) throws PlanGenException {        
-        String joinPredicateType = OperatorBuilderUtils.getRequiredProperty(JOIN_PREDICATE, operatorProperties);
-        PlanGenUtils.planGenAssert(! joinPredicateType.trim().isEmpty(), "Join predicate type is empty");
+        // generate attribute list
+        List<Attribute> attributeList = OperatorBuilderUtils.constructAttributeList(operatorProperties);
         
-        IJoinPredicate joinPredicate = generateJoinPredicate(joinPredicateType, operatorProperties);
-        Join joinOperator = new Join(null, null, joinPredicate);
-        
-        return joinOperator;
-    }
-    
-    
-    @FunctionalInterface
-    private interface GetJoinPredicate {
-        IJoinPredicate getJoinPredicate(Map<String, String> operatorProperties) throws PlanGenException;
-    }
-    
-    
-    private static JoinDistancePredicate getJoinCharDistancePredicate(
-            Map<String, String> operatorProperties) throws PlanGenException{
-        String distanceStr = OperatorBuilderUtils.getRequiredProperty(JOIN_ID_ATTRIBUTE_TYPE, operatorProperties);
-        String joinIDAttributeName = OperatorBuilderUtils.getRequiredProperty(JOIN_ID_ATTRIBUTE_NAME, operatorProperties);
-        String joinIDAttributeType = OperatorBuilderUtils.getRequiredProperty(JOIN_ID_ATTRIBUTE_TYPE, operatorProperties);
-        
-        PlanGenUtils.planGenAssert(! joinIDAttributeName.trim().isEmpty(), 
-                "Join character distance predicate: ID attribute name is empty.");
-        PlanGenUtils.planGenAssert(PlanGenUtils.isValidAttributeType(joinIDAttributeType), 
-                "Join character distance predicate: ID attribute type is invalid.");
-        Attribute joinIDAttribute = new Attribute(joinIDAttributeName, PlanGenUtils.convertAttributeType(joinIDAttributeType));
-        
-        
-        List<Attribute> attributeList = OperatorBuilderUtils.constructAttributeList(operatorProperties);       
         PlanGenUtils.planGenAssert(attributeList.size() == 1, 
-                "Join character distance predicate allows only 1 attribute, got " + attributeList.size()+  " attributes.");
-        Attribute joinAttribute = attributeList.get(0);
+                "Join operator allows only 1 attribute, got " + attributeList.size()+  " attributes.");
+
+        JSONObject joinPredicateJsonObject = new JSONObject(joinPredicateString);
+        String joinPredicateType = joinPredicateJsonObject.getString(key);
         
-        int distance;
-        try {
-            distance = Integer.parseInt(distanceStr);
-        } catch (NumberFormatException e) {
-            throw new PlanGenException(e.getMessage(), e);
-        }
-        if (distance <= 0) {
-            throw new PlanGenException("Join character distance predicate: distance must be greater than 0.");
-        }
         
-        return new JoinDistancePredicate(joinIDAttribute, joinAttribute, distance);
-    }
-    
-    private static HashMap<String, GetJoinPredicate> joinPredicateHandlerMap = new HashMap<>();
-    static {
-        joinPredicateHandlerMap.put("CharacterDistance".toLowerCase(), JoinBuilder::getJoinCharDistancePredicate);
-    }
-    
-    private static IJoinPredicate generateJoinPredicate(String joinPredicateType, Map<String, String> operatorProperties) throws PlanGenException {
-        PlanGenUtils.planGenAssert(joinPredicateHandlerMap.containsKey(joinPredicateType.toLowerCase()), 
-                "Join predicate type is invalid, it must be one of " + joinPredicateHandlerMap.keySet());
-        return joinPredicateHandlerMap.get(joinPredicateType).getJoinPredicate(operatorProperties);
+        
+        return null;
     }
 
 }
