83928e70b4855db13c590b741634f357f72c1b19
zuozhiw <zuozhiw@uci.edu>
2016-10-28 13:42:20 -0700

    remove open and close in data writer interface

--- textdb/textdb-api/src/main/java/edu/uci/ics/textdb/api/storage/IDataWriter.java
+++ textdb/textdb-api/src/main/java/edu/uci/ics/textdb/api/storage/IDataWriter.java
@@ -4,10 +4,6 @@ import edu.uci.ics.textdb.api.common.ITuple;
 
 public interface IDataWriter {
     
-    public void open() throws Exception;
-    
-    public void close() throws Exception;
-    
     public void insertTuple(ITuple tuple) throws Exception;
 
     public void clearData() throws Exception;
