4fcc36b60654fd9578fb1a0b7f8f649dcbf00130
Hailey Pan <haileypan@dhcp-v100-013.mobile.uci.edu>
2016-11-28 12:47:09 -0800

    added OpenNLP examples

---
+++ textdb/textdb-sandbox/src/main/java/edu/uci/ics/textdb/sandbox/OpenNLPexample/NameFinderExample.java
@@ -0,0 +1,89 @@
+package edu.uci.ics.textdb.sandbox.OpenNLPexample;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.StringReader;
+import java.util.Scanner;
+
+import opennlp.tools.cmdline.PerformanceMonitor;
+import opennlp.tools.cmdline.postag.POSModelLoader;
+import opennlp.tools.namefind.NameFinderME;
+import opennlp.tools.namefind.TokenNameFinderModel;
+import opennlp.tools.postag.POSModel;
+import opennlp.tools.postag.POSSample;
+import opennlp.tools.postag.POSTaggerME;
+import opennlp.tools.tokenize.Tokenizer;
+import opennlp.tools.tokenize.TokenizerME;
+import opennlp.tools.tokenize.TokenizerModel;
+import opennlp.tools.tokenize.WhitespaceTokenizer;
+import opennlp.tools.util.InvalidFormatException;
+import opennlp.tools.util.ObjectStream;
+import opennlp.tools.util.PlainTextByLineStream;
+import opennlp.tools.util.Span;
+
+public class NameFinderExample {
+   
+    public static String[] Tokenize(String sentence) throws InvalidFormatException, IOException {
+    	InputStream is = new FileInputStream("./src/main/resources/en-token.bin");
+     
+    	TokenizerModel model = new TokenizerModel(is);
+     
+    	Tokenizer tokenizer = new TokenizerME(model);
+     
+    	String tokens[] = tokenizer.tokenize(sentence);
+     
+    	is.close();
+    	
+    	return tokens;
+    }
+
+    
+    public static void main(String[] args) throws IOException {
+    	String dataFile = "./src/main/resources/abstract_100.txt";
+    	Scanner scan = new Scanner(new File(dataFile));
+    	
+    	InputStream is = new FileInputStream("./src/main/resources/en-ner-location.bin");
+     
+    	TokenNameFinderModel model = new TokenNameFinderModel(is);
+    	is.close();
+     
+    	NameFinderME nameFinder = new NameFinderME(model);
+    	
+    	PerformanceMonitor perfMon = new PerformanceMonitor(System.err, "sent");
+    	perfMon.start();
+    	while(scan.hasNextLine()){ 
+    		
+    		String[] sentence = Tokenize(scan.nextLine());
+    		Span Spans[] = nameFinder.find(sentence);
+    		perfMon.incrementCounter();
+    		
+    		if(Spans.length != 0) {
+    			System.out.print("[");
+    			for(String s: sentence) {
+    				System.out.print(s + ", ");
+    			}
+    			System.out.println("]");
+    		}
+    		
+    		for(Span s: Spans) {
+    			System.out.println(s.toString());
+    			for(int i = s.getStart(); i < s.getEnd(); i++)
+    				System.out.println(sentence[i]);	
+    		}	
+    		
+    		if(Spans.length != 0)
+    			System.out.println();
+    		
+    	}
+    	perfMon.stopAndPrintFinalResult();
+    	scan.close();
+    
+    }
+    
+
+    
+     
+    
+}
---
+++ textdb/textdb-sandbox/src/main/java/edu/uci/ics/textdb/sandbox/OpenNLPexample/POSTagexample.java
@@ -0,0 +1,51 @@
+package edu.uci.ics.textdb.sandbox.OpenNLPexample;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.Scanner;
+
+import opennlp.tools.cmdline.PerformanceMonitor;
+import opennlp.tools.cmdline.postag.POSModelLoader;
+import opennlp.tools.postag.POSModel;
+import opennlp.tools.postag.POSSample;
+import opennlp.tools.postag.POSTaggerME;
+import opennlp.tools.tokenize.WhitespaceTokenizer;
+import opennlp.tools.util.ObjectStream;
+import opennlp.tools.util.PlainTextByLineStream;
+
+public class POSTagexample {
+	
+    public static void main(String[] args) throws IOException {
+    	
+    	POSModel model = new POSModelLoader()	
+    		.load(new File("./src/main/resources/en-pos-maxent.bin"));
+    	PerformanceMonitor perfMon = new PerformanceMonitor(System.err, "sent");
+    	POSTaggerME tagger = new POSTaggerME(model);
+     
+    	String dataFile = "./src/main/resources/abstract_100.txt";
+    	Scanner scan = new Scanner(new File(dataFile));
+    	
+    	perfMon.start();
+    	while(scan.hasNextLine()) {
+    		String input = scan.nextLine();
+    		ObjectStream<String> lineStream = new PlainTextByLineStream(
+        			new StringReader(input));
+    		String line;
+        	while ((line = lineStream.read()) != null) {
+         
+        		String whitespaceTokenizerLine[] = WhitespaceTokenizer.INSTANCE
+        				.tokenize(line);
+        		String[] tags = tagger.tag(whitespaceTokenizerLine);
+         
+        		POSSample sample = new POSSample(whitespaceTokenizerLine, tags);
+        		System.out.println(sample.toString());
+         
+        		perfMon.incrementCounter();
+        	}
+    	}
+    	
+    	perfMon.stopAndPrintFinalResult();
+    	scan.close();
+    }
+}
