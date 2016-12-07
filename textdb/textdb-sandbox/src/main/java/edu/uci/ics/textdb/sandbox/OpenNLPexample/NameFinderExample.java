58b7dc293f2c826abf3f65c0f06954adc31e49fe
Hailey Pan <haileypan@Haileys-MacBook-Pro.local>
2016-12-07 10:45:48 -0800

    change print out format

--- textdb/textdb-sandbox/src/main/java/edu/uci/ics/textdb/sandbox/OpenNLPexample/NameFinderExample.java
+++ textdb/textdb-sandbox/src/main/java/edu/uci/ics/textdb/sandbox/OpenNLPexample/NameFinderExample.java
@@ -4,23 +4,15 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.StringReader;
 import java.util.Scanner;
 
 import opennlp.tools.cmdline.PerformanceMonitor;
-import opennlp.tools.cmdline.postag.POSModelLoader;
 import opennlp.tools.namefind.NameFinderME;
 import opennlp.tools.namefind.TokenNameFinderModel;
-import opennlp.tools.postag.POSModel;
-import opennlp.tools.postag.POSSample;
-import opennlp.tools.postag.POSTaggerME;
 import opennlp.tools.tokenize.Tokenizer;
 import opennlp.tools.tokenize.TokenizerME;
 import opennlp.tools.tokenize.TokenizerModel;
-import opennlp.tools.tokenize.WhitespaceTokenizer;
 import opennlp.tools.util.InvalidFormatException;
-import opennlp.tools.util.ObjectStream;
-import opennlp.tools.util.PlainTextByLineStream;
 import opennlp.tools.util.Span;
 
 public class NameFinderExample {
@@ -60,11 +52,12 @@ public class NameFinderExample {
     		perfMon.incrementCounter();
     		
     		if(Spans.length != 0) {
-    			System.out.print("[");
+    			
     			for(String s: sentence) {
-    				System.out.print(s + ", ");
+    				 
+    				System.out.print("["+ s + "] ");
     			}
-    			System.out.println("]");
+    			System.out.println("/n");
     		}
     		
     		for(Span s: Spans) {
