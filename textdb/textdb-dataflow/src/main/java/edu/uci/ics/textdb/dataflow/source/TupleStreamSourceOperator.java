1189ba1fc7a49beaaea5612f3ff04afe50543564
zuozhiw <zuozhiw@uci.edu>
2016-12-08 15:36:58 -0800

    fix typo

--- textdb/textdb-dataflow/src/main/java/edu/uci/ics/textdb/dataflow/source/TupleStreamSourceOperator.java
+++ textdb/textdb-dataflow/src/main/java/edu/uci/ics/textdb/dataflow/source/TupleStreamSourceOperator.java
@@ -9,7 +9,7 @@ import edu.uci.ics.textdb.api.dataflow.ISourceOperator;
 import edu.uci.ics.textdb.api.exception.TextDBException;
 
 /*
- * This operator takes a in-memory list of tuples and serves them as a source operator.
+ * This operator takes an in-memory list of tuples and serves them as a source operator.
  * 
  */
 public class TupleStreamSourceOperator implements ISourceOperator {
