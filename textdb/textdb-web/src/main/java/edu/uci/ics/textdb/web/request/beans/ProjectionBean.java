39de0305b2bd9073ef1929fb0b3fe01c5a278685
Flavio Bayer <flaviobayer@hotmail.com>
2016-12-08 13:53:10 -0800

    Added equals method to ProjectionBean

--- textdb/textdb-web/src/main/java/edu/uci/ics/textdb/web/request/beans/ProjectionBean.java
+++ textdb/textdb-web/src/main/java/edu/uci/ics/textdb/web/request/beans/ProjectionBean.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.annotation.JsonTypeName;
 
 import java.util.HashMap;
 
+import org.apache.commons.lang3.builder.EqualsBuilder;
+
 /**
  * This class defines the properties/data members specific to the Projection operator
  * and extends the OperatorBean class which defines the data members general to all operators
@@ -26,5 +28,13 @@ public class ProjectionBean extends OperatorBean {
         if(operatorProperties == null)
             return null;
         return operatorProperties;
+    }
+    @Override
+    public boolean equals(Object other) {
+        if (other == null) return false;
+        if (other == this) return true;
+        if (!(other instanceof ProjectionBean)) return false;
+        ProjectionBean projectionBean = (ProjectionBean) other;
+        return super.equals(projectionBean);
     }
 }
