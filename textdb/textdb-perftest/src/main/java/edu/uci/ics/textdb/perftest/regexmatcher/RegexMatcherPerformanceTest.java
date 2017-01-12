812f924c056001e218ff4b34e8d9e8f7b9db7edb
zuozhiw <zuozhiw@uci.edu>
2017-01-11 22:53:53 -0800

    modify regex matcher tests, add regex matcher source op

--- textdb/textdb-perftest/src/main/java/edu/uci/ics/textdb/perftest/regexmatcher/RegexMatcherPerformanceTest.java
+++ textdb/textdb-perftest/src/main/java/edu/uci/ics/textdb/perftest/regexmatcher/RegexMatcherPerformanceTest.java
@@ -15,9 +15,11 @@ import edu.uci.ics.textdb.common.field.ListField;
 import edu.uci.ics.textdb.common.field.Span;
 import edu.uci.ics.textdb.dataflow.common.RegexPredicate;
 import edu.uci.ics.textdb.dataflow.regexmatch.RegexMatcher;
+import edu.uci.ics.textdb.dataflow.regexmatch.RegexMatcherSourceOperator;
 import edu.uci.ics.textdb.dataflow.source.IndexBasedSourceOperator;
 import edu.uci.ics.textdb.perftest.medline.MedlineIndexWriter;
 import edu.uci.ics.textdb.perftest.utils.PerfTestUtils;
+import edu.uci.ics.textdb.storage.DataReaderPredicate;
 import edu.uci.ics.textdb.storage.DataStore;
 
 /*
@@ -100,7 +102,7 @@ public class RegexMatcherPerformanceTest {
 	        Analyzer luceneAnalyzer = LuceneAnalyzerConstants.getNGramAnalyzer(3);
 	        RegexPredicate regexPredicate = new RegexPredicate(regex, attributeNames, luceneAnalyzer);
 	        IndexBasedSourceOperator indexInputOperator = new IndexBasedSourceOperator(
-	                regexPredicate.generateDataReaderPredicate(dataStore));
+	                new DataReaderPredicate(RegexMatcherSourceOperator.createLuceneQuery(regexPredicate), dataStore));
 	
 	        RegexMatcher regexMatcher = new RegexMatcher(regexPredicate);
 	        regexMatcher.setInputOperator(indexInputOperator);
