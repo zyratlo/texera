6f0af82833d748da926e25a091555c9351677f86
zuozhiw <zuozhiw@uci.edu>
2017-01-19 22:20:41 -0800

    add limit and offset on keyword source

--- textdb/textdb-gui/js/onstart.js
+++ textdb/textdb-gui/js/onstart.js
@@ -31,19 +31,21 @@ var setup = function(){
 	var DEFAULT_DISTANCE = 10;
 	var DEFAULT_ATTRIBUTES = "first name, last name";
 	var DEFAULT_LIMIT = 10;
-	var DEFAULT_OFFSET = 5;
+	var DEFAULT_OFFSET = 0;
 	
 	/*
 		Helper Functions
 	*/
 	//Helper Function for Process Queries that displays the results after hitting "Process Query"
 	function createResultFrame(message){
+		var resultJSON = JSON.parse(message['text']);
+
 		var resultFrame = $('<div class="result-frame"><div class="result-box"><div class="result-box-band">Return Result<div class="result-frame-close"><img src="img/close-icon.png"></div></div><div class="return-result"></div></div></div>');
 		$('body').append(resultFrame);
 		
 		var node = new PrettyJSON.view.Node({
 			el:$('.return-result'),
-			data:message
+			data:resultJSON
 		});
 	}
 	
@@ -312,6 +314,8 @@ var setup = function(){
 		}
 		TEXTDBJSON.operators = operators;
 		TEXTDBJSON.links = links;
+
+		console.log(TEXTDBJSON);
 		
 		$.ajax({
 			url: "http://localhost:8080/queryplan/execute",
@@ -321,8 +325,6 @@ var setup = function(){
 			contentType: "application/json",
 			success: function(returnedData){
 				console.log("SUCCESS\n");
-
-				console.log(returnedData);
 
 				createResultFrame(JSON.parse(returnedData));
 			},
