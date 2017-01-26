a5227047e9259c785fbe4eb84cf666233123e7e3
Flavio Bayer <flaviobayer@hotmail.com>
2017-01-25 16:34:36 -0800

    Transformed Predicates from abstract class to interface.

--- textdb/textdb-textql/src/main/java/edu/uci/ics/textdb/textql/statements/predicates/SelectPredicate.java
+++ textdb/textdb-textql/src/main/java/edu/uci/ics/textdb/textql/statements/predicates/SelectPredicate.java
@@ -3,7 +3,7 @@ package edu.uci.ics.textdb.textql.statements.predicates;
 import edu.uci.ics.textdb.web.request.beans.OperatorBean;
 
 /**
- * Object representation of a "SELECT (...)" predicate inside a { @code SelectExtractStatement }.
+ * Interface for representation of a "SELECT (...)" predicate inside a { @code SelectExtractStatement }.
  * Subclasses have specific fields related to its projection functionalities.
  * SelectPredicate --+ SelectAllPredicate
  *                   + SelectFieldsPredicate
@@ -11,7 +11,7 @@ import edu.uci.ics.textdb.web.request.beans.OperatorBean;
  * @author Flavio Bayer
  *
  */
-public abstract class SelectPredicate {
+public interface SelectPredicate {
 
     /**
      * Return this operator converted to an { @code OperatorBean }.
@@ -19,12 +19,4 @@ public abstract class SelectPredicate {
      */
     public abstract OperatorBean getOperatorBean(String selectOperatorId);
     
-
-    @Override
-    public boolean equals(Object other) {
-        if (other == null) { return false; }
-        if (other.getClass() != getClass()) { return false; }
-        SelectPredicate selectPredicate = (SelectPredicate) other;
-        return true;
-    }
 }
