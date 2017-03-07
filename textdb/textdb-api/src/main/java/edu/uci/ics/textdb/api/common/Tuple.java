ea262ed9db2fa685cab3010a6e4ceec10a3b5524
zuozhiw <zuozhiw@uci.edu>
2017-03-07 14:21:49 -0800

    make getField(int) returns T

--- textdb/textdb-api/src/main/java/edu/uci/ics/textdb/api/common/Tuple.java
+++ textdb/textdb-api/src/main/java/edu/uci/ics/textdb/api/common/Tuple.java
@@ -23,8 +23,14 @@ public class Tuple {
         this.fields = Arrays.asList(fields);
     }
 
-    public IField getField(int index) {
-        return fields.get(index);
+    public <T extends IField> T getField(int index) {
+        @SuppressWarnings("unchecked")
+        T field = (T) fields.get(index);
+        return field;
+    }
+    
+    public <T extends IField> T getField(int index, Class<T> fieldClass) {
+        return getField(index);
     }
 
     public IField getField(String fieldName) {
