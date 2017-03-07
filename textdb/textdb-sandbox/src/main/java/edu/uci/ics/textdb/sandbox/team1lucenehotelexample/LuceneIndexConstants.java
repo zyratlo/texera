252fab58c7d6ad7c9e0bef0f0910670ac31e89f1
Kishore Narendran <kishore.narendran09@gmail.com>
2017-03-06 16:30:17 -0800

    Removes old sandbox sample packages and files

--- textdb/textdb-sandbox/src/main/java/edu/uci/ics/textdb/sandbox/team1lucenehotelexample/LuceneIndexConstants.java
+++
@@ -1,9 +0,0 @@
-package edu.uci.ics.textdb.sandbox.team1lucenehotelexample;
-
-public class LuceneIndexConstants {
-    public static final String INDEX_DIR = "index";
-    public static final String ID_FIELD = "id";
-    public static final String NAME_FIELD = "name";
-    public static final String CITY_FIELD = "city";
-    public static final String CONTENT_FIELD = "content";
-}
--- textdb/textdb-sandbox/src/main/java/edu/uci/ics/textdb/sandbox/team5lucenemovieexample/LuceneIndexConstants.java
+++
@@ -1,8 +0,0 @@
-package edu.uci.ics.textdb.sandbox.team5lucenemovieexample;
-
-public class LuceneIndexConstants {
-    public static final String INDEX_DIR = "index";
-    public static final String ID_FIELD = "id";
-    public static final String NAME_FIELD = "name";
-    public static final String CONTENT_FIELD = "content";
-}
