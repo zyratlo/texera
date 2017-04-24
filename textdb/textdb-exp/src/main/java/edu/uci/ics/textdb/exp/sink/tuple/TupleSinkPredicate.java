76f3f0b0ebecd789721a831c26a1d0e3eaf52e14
zuozhiw <zuozhiw@uci.edu>
2017-04-23 22:44:44 -0700

    add new plangen files

--- textdb/textdb-exp/src/main/java/edu/uci/ics/textdb/exp/sink/tuple/TupleSinkPredicate.java
+++ textdb/textdb-exp/src/main/java/edu/uci/ics/textdb/exp/sink/tuple/TupleSinkPredicate.java
@@ -1,14 +1,9 @@
 package edu.uci.ics.textdb.exp.sink.tuple;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-
 import edu.uci.ics.textdb.api.dataflow.IOperator;
 import edu.uci.ics.textdb.exp.common.PredicateBase;
 
 public class TupleSinkPredicate extends PredicateBase {
-    
-    @JsonCreator
-    public TupleSinkPredicate() {}
 
     @Override
     public IOperator newOperator() {
