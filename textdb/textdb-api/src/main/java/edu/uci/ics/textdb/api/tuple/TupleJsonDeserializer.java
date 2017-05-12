e5bb3f0f084de7e479302ed610aad35a18c7c1b4
zuozhiw <zuozhiw@uci.edu>
2017-05-12 00:30:17 -0700

    add comments in custom deserializer

--- textdb/textdb-api/src/main/java/edu/uci/ics/textdb/api/tuple/TupleJsonDeserializer.java
+++ textdb/textdb-api/src/main/java/edu/uci/ics/textdb/api/tuple/TupleJsonDeserializer.java
@@ -15,6 +15,30 @@ import edu.uci.ics.textdb.api.field.IField;
 import edu.uci.ics.textdb.api.schema.AttributeType;
 import edu.uci.ics.textdb.api.schema.Schema;
 
+/**
+ * This class is a custom Json Deserializer for the Tuple class.
+ * 
+ * The Tuple class cannot use the default Deserializer because there are many
+ *   kinds of fields, and the field doesn't have information of the FieldType.
+ * This custom deserializer can use the schema information to process the JSON.
+ *   
+ * For example, a tuple has an IDField and a TextField, the JSON representation is:
+ * (IDField):   {"value": "id of the tuple"}  ("_id" is also a string)
+ * (TextField): {"value": "some text in the field"}
+ * There's no difference between the representation of IDField and TextField.
+ * 
+ * However, a tuple also contains its schema: 
+ * {"attributes": [
+ *     {"attributeName": "_id", "attributeType": "_id"},
+ *     {"attributeName": "textAttr", "attributeType": " text"}
+ * ]}
+ * Therefore, this custom deserializer can use the information:
+ *   the first field is an IDField, the second field is TextField,
+ *   to map each field to the right class.
+ * 
+ * @author Zuozhi Wang
+ *
+ */
 public class TupleJsonDeserializer extends StdDeserializer<Tuple> {
 
     private static final long serialVersionUID = 6244351898113632246L;
