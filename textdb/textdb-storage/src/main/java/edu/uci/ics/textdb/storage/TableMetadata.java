3ddf044ae1439643fc2091feb75d5383b78a1f6e
Seungmok Lee <seungmol@uci.edu>
2017-05-15 20:28:31 -0700

    Recreate PR

---
+++ textdb/textdb-storage/src/main/java/edu/uci/ics/textdb/storage/TableMetadata.java
@@ -0,0 +1,7 @@
+package edu.uci.ics.textdb.storage;
+
+/**
+ * Created by DebugYourBrain on 5/15/17.
+ */
+public class TableMetadata {
+}
---
+++ textdb/textdb-web/src/main/java/edu/uci/ics/textdb/web/resource/SystemResource.java
@@ -0,0 +1,7 @@
+package edu.uci.ics.textdb.web.resource;
+
+/**
+ * Created by DebugYourBrain on 5/15/17.
+ */
+public class SystemResource {
+}
