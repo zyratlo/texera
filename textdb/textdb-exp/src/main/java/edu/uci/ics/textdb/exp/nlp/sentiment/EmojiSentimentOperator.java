9d9003710cc1b929952518c49aca2cce3b188a14
zuozhiw <zuozhiw@uci.edu>
2017-06-12 19:34:38 -0400

    adjust sentiment analysis score

--- textdb/textdb-exp/src/main/java/edu/uci/ics/textdb/exp/nlp/sentiment/EmojiSentimentOperator.java
+++ textdb/textdb-exp/src/main/java/edu/uci/ics/textdb/exp/nlp/sentiment/EmojiSentimentOperator.java
@@ -22,9 +22,9 @@ import java.util.regex.Pattern;
  *
  * The result is an integer indicating the sentiment score, which represents:
  *
- * 3 - positive
- * 2 - neutral
- * 1 - negative
+ * 1 - positive
+ * 0 - neutral
+ * -1 - negative
  *
  *
  * The result will be put into an attribute with resultAttributeName specified in predicate, and type Integer.
@@ -37,6 +37,8 @@ public class EmojiSentimentOperator implements IOperator {
     private IOperator inputOperator;
     private Schema outputSchema;
     private int cursor = CLOSED;
+    // set neutral score to 0
+    private static final int NEUTRAL_SCORE = 0;
     //SMILEY_REGEX_PATTERN identifies all happiness related emoticons like :) :-) <3 etc in the given text. 
     //The regex is given below. 
     public static final Pattern SMILEY_REGEX_PATTERN = Pattern.compile(".*(:[)DdpP]|:[ -]\\)|<3)+.*");
@@ -172,7 +174,7 @@ public class EmojiSentimentOperator implements IOperator {
     private Integer computeSentimentScore(Tuple inputTuple) {
         String inputText = inputTuple.<IField>getField(predicate.getInputAttributeName()).getValue().toString();
         Matcher matcher = null;
-        Integer matchedStringScore = 2;
+        Integer matchedStringScore = NEUTRAL_SCORE;
         if(SMILEY_REGEX_PATTERN!= null){
             matcher = SMILEY_REGEX_PATTERN.matcher(inputText);
             if(matcher.matches()){
@@ -207,11 +209,11 @@ public class EmojiSentimentOperator implements IOperator {
                 }
             }
         }
-        if(matchedStringScore<1){
-            matchedStringScore = 1;
+        if(matchedStringScore < NEUTRAL_SCORE){
+            matchedStringScore = -1;
         }
-        if(matchedStringScore>3){
-            matchedStringScore = 3;
+        if(matchedStringScore > NEUTRAL_SCORE){
+            matchedStringScore = 1;
         }
         return matchedStringScore;
     }
