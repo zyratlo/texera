5c857628c22e0af7d09e36cce140d89977d4e46b
Qinhua Huang <qinhuah@gmail.com>
2017-07-01 16:03:51 -0700

    installed nltk

--- textdb/textdb-exp/src/main/resources/Python/classifier-loader.py
+++ textdb/textdb-exp/src/main/resources/Python/classifier-loader.py
@@ -1,7 +1,7 @@
 import sys
 import pickle
 import warnings
-import nltk
+#import nltk
 
 def debugLine(strLine):
 	f = open("python_classifier_loader.log","a+")
@@ -9,26 +9,12 @@ def debugLine(strLine):
 	f.close()
 
 def main():
-#	print("neg")
-	#fs = open('Senti.pickle', 'rb')
-	warnings.filterwarnings("ignore")
 	picklePath = sys.argv[1]
-#	debugLine(path)
-#	print(picklePath)
-	pyversion = format(nltk.__version__);
-	#print("BeforeClasify")
 	pickleFile = open(picklePath, 'rb')
-#	print("After open pickeleFile: %s"% picklePath)
 	Sentimm = pickle.load(pickleFile)
-#	print(pyversion)
-	#print('The nltk version is {}.'.format(nltk.__version__))
-#	print("1700"+picklePath)
-#	text = sys.argv[2]
-#	classLabel = Sentimm.classify(text)
-#	print("Afterclassify %s" %classLabel)
-#	print(classLabel)
 	for text in sys.argv[2:]:
 		print("%s"%Sentimm.classify(text))
 	pickleFile.close()
 
+warnings.filterwarnings("ignore")
 main()
