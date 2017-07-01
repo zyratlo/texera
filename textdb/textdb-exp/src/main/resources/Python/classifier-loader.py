218d9e17642f416cee0a831d963afb5be4426dd3
Qinhua Huang <qinhuah@gmail.com>
2017-06-30 17:36:07 -0700

    sdfadfjl

--- textdb/textdb-exp/src/main/resources/Python/classifier-loader.py
+++ textdb/textdb-exp/src/main/resources/Python/classifier-loader.py
@@ -11,12 +11,12 @@ def main():
 	#fs = open('Senti.pickle', 'rb')
 	picklePath = sys.argv[1]
 #	debugLine(path)
-	print(picklePath)
+#	print(picklePath)
 	pickleFile = open(picklePath, 'rb')
 	Sentimm = pickle.load(pickleFile)
 #	print("1700"+picklePath)
-	pickleFile.close()
 	for text in sys.argv[2:]:
 		print(Sentimm.classify(text))
+	pickleFile.close()
 
 main()
