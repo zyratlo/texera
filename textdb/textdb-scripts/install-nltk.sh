4960f33833dd82f51ebc2a08df0eaebf3fc82ddb
Qinhua Huang <qinhuah@gmail.com>
2017-07-20 19:35:54 -0700

    add install script

--- textdb/textdb-scripts/install-nltk.sh
+++ textdb/textdb-scripts/install-nltk.sh
@@ -1,6 +1,25 @@
-sudo apt-get install python3
-sudo apt-get install python3-setuptools
-sudo easy_install3 pip
-export LC_ALL=C
-pip3 install -U nltk
+unameOut="$(uname -s)"
+case "${unameOut}" in
+	Linux*) 
+		machine=Linux
+		OS=$(lsb_release -si)
+		VER=$(lsb_release -sr)
+		if [ $OS = "Ubuntu" ]; then
+			machine="UBUNTU"
+			sudo apt-get install python3
+			sudo pip3 install nltk
+		fi
+		;;
+	Darwin*)
+		machine=Mac
+		sudo pip3 install nltk
+		;;
+esac
+echo ${machine}
+
+#sudo apt-get install python3
+#sudo apt-get install python3-setuptools
+#sudo easy_install3 pip
+#export LC_ALL=C
+#pip3 install -U nltk
 
