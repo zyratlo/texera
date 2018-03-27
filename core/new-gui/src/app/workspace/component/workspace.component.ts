de27d2f6e9def0d0aed100383097d4c3ee669803
zuozhiw <zuozhiw@gmail.com>
2018-03-27 15:51:37 -0700

    add basic grid layout for each component

--- core/new-gui/src/app/workspace/component/workspace.component.ts
+++ core/new-gui/src/app/workspace/component/workspace.component.ts
@@ -1,5 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
+import { NavigationComponent } from './navigation/navigation.component';
+import { OperatorPanelComponent } from './operator-panel/operator-panel.component';
+import { PropertyEditorComponent } from './property-editor/property-editor.component';
+import { WorkflowEditorComponent } from './workflow-editor/workflow-editor.component';
+import { ResultPanelComponent } from './result-panel/result-panel.component';
+
+
+
 @Component({
   selector: 'texera-workspace',
   templateUrl: './workspace.component.html',
