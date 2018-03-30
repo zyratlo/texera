41601bec0d361c17077900eebe16bde2a07fa680
zuozhiw <zuozhiw@gmail.com>
2018-03-29 21:01:04 -0700

    add comments for ng material import and rxjs import

--- core/new-gui/src/app/common/app-setting.ts
+++ core/new-gui/src/app/common/app-setting.ts
@@ -1,21 +1,19 @@
 import { isDevMode } from '@angular/core';
 
-
-/* tslint:disable:no-inferrable-types */
 export class AppSettings {
-    private static readonly SERVER_ADDRESS: string = 'http://localhost';
-    private static readonly API_PORT: number = 8080;
-    private static readonly API_ENDPOINT: string = 'api';
+  private static readonly SERVER_ADDRESS = 'http://localhost';
+  private static readonly API_PORT = 8080;
+  private static readonly API_ENDPOINT = 'api';
 
-    public static getApiEndpoint(): string {
+  public static getApiEndpoint(): string {
+    if (isDevMode()) {
       // if we are in development mode (we use angular CLI development server at localhost:4200)
       // we need to send request to localhost:8080 api endpoint
-      if (isDevMode()) {
-        return `${AppSettings.SERVER_ADDRESS}:${AppSettings.API_PORT}/${AppSettings.API_ENDPOINT}`;
-      } else {
-        // if we are in production mode (the frontend page is served by backend at a server)
-        // we use relative path to resolve the URL
-        return `/${AppSettings.API_ENDPOINT}`;
-      }
+      return `${AppSettings.SERVER_ADDRESS}:${AppSettings.API_PORT}/${AppSettings.API_ENDPOINT}`;
+    } else {
+      // if we are in production mode (the frontend page is served by backend at a server)
+      // we use relative path to resolve the URL
+      return AppSettings.API_ENDPOINT;
     }
+  }
 }
