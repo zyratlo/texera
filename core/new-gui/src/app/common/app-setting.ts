5c648b2a4ab52d6946b8df5023dcc8de3e7950f2
zuozhiw <zuozhiw@gmail.com>
2018-03-29 18:55:43 -0700

    use isDevMode to judge which URL to use

--- core/new-gui/src/app/common/app-setting.ts
+++ core/new-gui/src/app/common/app-setting.ts
@@ -1,6 +1,21 @@
+import { isDevMode } from '@angular/core';
+
+
 /* tslint:disable:no-inferrable-types */
 export class AppSettings {
     private static readonly SERVER_ADDRESS: string = 'http://localhost';
     private static readonly API_PORT: number = 8080;
-    public static API_ENDPOINT: string = `${AppSettings.SERVER_ADDRESS}:${AppSettings.API_PORT}/api`;
+    private static readonly API_ENDPOINT: string = 'api';
+
+    public static getApiEndpoint(): string {
+      // if we are in development mode (we use angular CLI development server at localhost:4200)
+      // we need to send request to localhost:8080 api endpoint
+      if (isDevMode()) {
+        return `${AppSettings.SERVER_ADDRESS}:${AppSettings.API_PORT}/${AppSettings.API_ENDPOINT}`;
+      } else {
+        // if we are in production mode (the frontend page is served by backend at a server)
+        // we use relative path to resolve the URL
+        return `/${AppSettings.API_ENDPOINT}`;
+      }
+    }
 }
