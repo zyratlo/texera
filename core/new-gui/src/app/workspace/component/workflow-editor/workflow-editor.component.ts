4da0f1bf25b825e28fd2e0f3548779ce4a391a51
zuozhiw <zuozhiw@gmail.com>
2018-04-02 13:36:26 -0700

    add more comments for jointjs

--- core/new-gui/src/app/workspace/component/workflow-editor/workflow-editor.component.ts
+++ core/new-gui/src/app/workspace/component/workflow-editor/workflow-editor.component.ts
@@ -3,6 +3,19 @@ import { Component, AfterViewInit } from '@angular/core';
 import * as joint from 'jointjs';
 import { OperatorViewElementService } from '../../service/operator-view-element/operator-view-element.service';
 
+/**
+ * WorkflowEditorComponent is the componenet for the main workflow editor part of the UI.
+ *
+ * This componenet is binded with the JointJS paper. JointJS handles the operations of the main workflow.
+ * The JointJS UI events are wrapped into observables and exposed to other components / services.
+ *
+ * See JointJS documentation for the list of events that can be captured on the JointJS paper view.
+ * https://resources.jointjs.com/docs/jointjs/v2.0/joint.html#dia.Paper.events
+ *
+ * @author Zuozhi Wang
+ * @author Henry Chen
+ *
+*/
 @Component({
   selector: 'texera-workflow-editor',
   templateUrl: './workflow-editor.component.html',
@@ -10,6 +23,8 @@ import { OperatorViewElementService } from '../../service/operator-view-element/
 })
 export class WorkflowEditorComponent implements AfterViewInit {
 
+  // the DOM element ID of the main editor. It can be used by jQuery and jointJS to find the DOM element
+  // in the HTML template, the div element ID is set using this variable
   public readonly WORKFLOW_EDITOR_ELEMENT_ID = 'texera-workflow-editor-body-id';
 
   private paper: joint.dia.Paper = null;
@@ -22,6 +37,7 @@ export class WorkflowEditorComponent implements AfterViewInit {
   ngAfterViewInit() {
     this.createJointjsPaper();
 
+    // add some dummy operators and links to show that JointJS works
     this.graph.addCell(
       this.operatorViewElementService.getJointjsOperatorElement(
         'ScanSource',
@@ -49,25 +65,36 @@ export class WorkflowEditorComponent implements AfterViewInit {
   }
 
   /**
- * Creates a JointJS Paper object, which is the JointJS view object responsible for
- *  rendering the workflow cells and handle UI events.
- *
- * JointJS documentation about paper: https://resources.jointjs.com/docs/jointjs/v2.0/joint.html#dia.Paper
- */
+   * Creates a JointJS Paper object, which is the JointJS view object responsible for
+   *  rendering the workflow cells and handle UI events.
+   *
+   * JointJS documentation about paper: https://resources.jointjs.com/docs/jointjs/v2.0/joint.html#dia.Paper
+   */
   private createJointjsPaper(): joint.dia.Paper {
 
     const paper = new joint.dia.Paper({
+      // bind the DOM element
       el: $('#' + this.WORKFLOW_EDITOR_ELEMENT_ID),
+      // bind the jointjs graph model
       model: this.graph,
+      // set the height and width of the paper to be the height and width of the parent element
       height: $('#' + this.WORKFLOW_EDITOR_ELEMENT_ID).height(),
       width: $('#' + this.WORKFLOW_EDITOR_ELEMENT_ID).width(),
+      // set grid size to 1px (smallest grid)
       gridSize: 1,
+      // enable jointjs feature that automatically snaps a link to the closest port when user drops a link
       snapLinks: true,
+      // disable jointjs default action that can make a link not connect to an operator
       linkPinning: false,
+      // provide a validation to determine if two ports could be connected (only output connect to input is allowed)
       validateConnection: validateOperatorConnection,
+      // disable jointjs default action of adding vertexes to the link
       interactive: { vertexAdd: false },
+      // set a default link element used by jointjs when user creates a link on UI
       defaultLink: this.operatorViewElementService.getDefaultLinkElement(),
+      // disable jointjs default action that stops propagate click events on jointjs paper
       preventDefaultBlankAction: false,
+      // disable jointjs default action that prevents normal right click menu showing up on jointjs paper
       preventContextMenu: false,
     });
 
