2fdd94b52dd54b3e0e95a05a84eb07e114759f9f
Henry <whchen1@uci.edu>
2018-04-03 15:55:28 -0700

    add stub operator view element

--- core/new-gui/src/app/workspace/component/workflow-editor/workflow-editor.component.spec.ts
+++ core/new-gui/src/app/workspace/component/workflow-editor/workflow-editor.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { WorkflowEditorComponent } from './workflow-editor.component';
+import { OperatorViewElementService } from '../../service/operator-view-element/operator-view-element.service';
+import { StubOperatorViewElementService } from '../../service/operator-view-element/stub-operator-view-element.service';
 
 describe('WorkflowEditorComponent', () => {
   let component: WorkflowEditorComponent;
@@ -8,7 +10,10 @@ describe('WorkflowEditorComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ WorkflowEditorComponent ]
+      declarations: [ WorkflowEditorComponent ],
+      providers: [
+        {provide: OperatorViewElementService, useClass: StubOperatorViewElementService }
+      ]
     })
     .compileComponents();
   }));
