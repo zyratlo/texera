403db76be305fa9f50fd106efd2f36caac18f60a
Ming Huang <mingh2@uci.edu>
2018-04-09 19:17:27 -0700

    Remove Unused Constants

--- core/dataflow/src/main/java/edu/uci/ics/texera/dataflow/common/PropertyNameConstants.java
+++ core/dataflow/src/main/java/edu/uci/ics/texera/dataflow/common/PropertyNameConstants.java
@@ -122,18 +122,6 @@ public class PropertyNameConstants {
     public static final String TWEET_TOKEN = "token";
     public static final String TWEET_TOKEN_SECRET = "tokenSecret";
 
-
-    public static final String QUERY = "query";
-
-    public static final Double DEFAULT_THRESHOLD = 0.8;
-    public static final String DEFAULT_QUERY = "keyword";
-    public static final String DEFAULT_ATTRIBUTE_NAME = "attr1";
-    public static final String DEFAULT_REGEX = "regex";
-
-    public static final Integer DEFAULT_LIMIT = 10;
-    public static final Integer DEFAULT_OFFSET = 0;
-    public static final Integer DEFAULT_SAMPLE_SIZE = 10;
-
     public static final String EMPTY_NAME_EXCEPTION = "Table Name Cannot be Empty";
     public static final String EMPTY_REGEX_EXCEPTION = "regex should not be empty";
     public static final String EMPTY_QUERY_EXCEPTION = "query should not be empty";
