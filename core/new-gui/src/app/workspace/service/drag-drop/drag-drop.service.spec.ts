a82ef6142fa790d898fab0bdea26a7309f4e77ee
Henry <whchen1@uci.edu>
2018-05-08 12:42:47 -0700

    add some testing to check if an element can successfully register to become draggable or droppable

--- core/new-gui/src/app/workspace/service/drag-drop/drag-drop.service.spec.ts
+++ core/new-gui/src/app/workspace/service/drag-drop/drag-drop.service.spec.ts
@@ -7,8 +7,14 @@ import { WorkflowActionService } from '../workflow-graph/model/workflow-action.s
 import { WorkflowUtilService } from '../workflow-graph/util/workflow-util.service';
 import { OperatorMetadataService } from '../operator-metadata/operator-metadata.service';
 import { StubOperatorMetadataService } from '../operator-metadata/stub-operator-metadata.service';
+import { getMockOperatorMetaData } from '../operator-metadata/mock-operator-metadata.data';
+
+import { marbles, Context } from 'rxjs-marbles';
 
 describe('DragDropService', () => {
+
+  let service: DragDropService;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [
@@ -20,9 +26,39 @@ describe('DragDropService', () => {
         { provide: OperatorMetadataService, useClass: StubOperatorMetadataService },
       ]
     });
+
+    service = TestBed.get(DragDropService);
   });
 
-  it('should be created', inject([DragDropService], (service: DragDropService) => {
-    expect(service).toBeTruthy();
+  it('should be created', inject([DragDropService], (injectedService: DragDropService) => {
+    expect(injectedService).toBeTruthy();
   }));
+
+
+  it('should successfully register the element as draggable', marbles((m) => {
+
+    const dragElementID = 'testing-draggable-1';
+    jQuery('body').append(`<div id="${dragElementID}"></div>`);
+
+
+    const operatorType = getMockOperatorMetaData().operators[0].operatorType;
+    service.registerOperatorLabelDrag(dragElementID, operatorType);
+
+    expect(jQuery('#' + dragElementID).is('.ui-draggable')).toBeTruthy();
+
+  }));
+
+
+  it('should successfully register the element as droppable', marbles((m) => {
+
+    const dropElement = 'testing-droppable-1';
+    jQuery('body').append(`<div id="${dropElement}"></div>`);
+
+    const operatorType = getMockOperatorMetaData().operators[0].operatorType;
+    service.registerWorkflowEditorDrop(dropElement);
+
+    expect(jQuery('#' + dropElement).is('.ui-droppable')).toBeTruthy();
+
+  }));
+
 });
