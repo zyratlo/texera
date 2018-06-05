562e4046dc9b3da1ef9125b82c4b97307e633a81
Henry <whchen1@uci.edu>
2018-06-04 21:46:51 -0700

    Add paginator to the result panel

--- core/new-gui/src/app/workspace/component/result-panel/result-panel.component.ts
+++ core/new-gui/src/app/workspace/component/result-panel/result-panel.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { DataSource } from '@angular/cdk/table';
+import { MatPaginator, MatTableDataSource } from '@angular/material';
+
 
 import { ExecuteWorkflowService } from "./../../service/execute-workflow/execute-workflow.service";
 
@@ -19,19 +21,23 @@ export class ResultPanelComponent implements OnInit {
 
   public currentColumns: TableColumn[] | undefined;
   public currentDisplayColumns: string[] | undefined;
-  public currentDataSource: ResultDataSource | undefined;
-  public currentDisplayRows: string = '';
+  public currentDataSource: MatTableDataSource<object> | undefined;
+  public currentDisplayRow: string = '';
 
-  /** Column definitions in order */
-  // displayedColumns = this.columns.map(x => x.columnDef);
-  constructor(private executeWorkflowService: ExecuteWorkflowService, private changeDetectorRef: ChangeDetectorRef,
-    private modalService: NgbModal) {
+  @ViewChild(MatPaginator) paginator: MatPaginator | null = null;
+
+  constructor(private executeWorkflowService: ExecuteWorkflowService, private modalService: NgbModal) {
+    // once the execution ended, update the result panel
     this.executeWorkflowService.getExecuteEndedStream().subscribe(
       executionResult => this.handleResultData(executionResult),
     );
   }
 
-  open(content: any): void {
+  /**
+   * open the ng-bootstrap model to display row delay when clicked
+   * @param content model content passed from the html
+   */
+  public open(content: any): void {
     this.modalService.open(content).result.then((result) => {
       console.log(result);
     }, (reason) => {
@@ -39,24 +45,48 @@ export class ResultPanelComponent implements OnInit {
     });
   }
 
+  /**
+   * Changes the result table to use the new workflow execution result.
+   * Set all the necessary for angular table and display the data table
+   * and paginator.
+   */
   private changeResultTableProperty(response: ExecutionResult) {
     const resultData: object[] | undefined = response.result;
     if (resultData !== undefined){
+      // generate columnDef from first row, column definition is in order
       this.currentDisplayColumns = Object.keys(resultData[0]).filter(x => x !== '_id');
       this.currentColumns = this.generateColumns(this.currentDisplayColumns);
-      this.currentDataSource = new ResultDataSource(resultData);
+
+      // create a new DataSource object based on the new result data
+      this.currentDataSource = new MatTableDataSource<object>(resultData);
+
+      // set the paginator to be the new DataSource's paginator
+      this.currentDataSource.paginator = this.paginator;
+
       console.log(this.currentDisplayColumns);
     }
   }
 
+  /**
+   * Handler for the execution result.
+   * Response code == 0:
+   * - Execution had run correctly
+   * - Don't show any error message
+   * - Update data table's property to display new result
+   * Response code != 0:
+   * - Execution had encountered an error
+   * - Update and show the error message on the panel
+   *
+   * @param response execution response from the backend
+   */
   private handleResultData(response: ExecutionResult): void {
     console.log('view result compoenent, ');
 
     console.log(response);
     if (response.code === 0) {
       console.log('show success data');
+      console.log(response.result);
       this.showMessage = false;
-      // generate columnDef from first row
       this.changeResultTableProperty(response);
     } else {
       console.log('show fail message');
@@ -65,45 +95,47 @@ export class ResultPanelComponent implements OnInit {
     }
   }
 
+  /**
+   * generate all the column information for displaying
+   * @param columnNames list of column names that will be displayed on the data table
+   */
   private generateColumns(columnNames: string[]): TableColumn[] {
+    console.log('generateColumns: ');
+
     const columns: TableColumn[] = [];
+    // generate a TableColumn object for each column
     columnNames.forEach(col => columns.push(new TableColumn(col, col, (row) => `${row[col]}`)));
     console.log(columns);
     return columns;
   }
 
+  /**
+   * get the information from the selected row
+   * and display it on the modal once it is opened
+   *
+   * @param row row data
+   * @param content modal content
+   */
   public getRowDetails(row: any, content: any): void {
-    console.log('ROw clicked');
+    console.log('getRowDetails: ');
     console.log(row);
-    this.currentDisplayRows = JSON.stringify(row, undefined, 2);
+    this.currentDisplayRow = JSON.stringify(row, undefined, 2);
     this.open(content);
   }
 
   ngOnInit() {
+
   }
 
 }
 
+/**
+ * Class for holding the information regarding to each column
+ */
 export class TableColumn {
   constructor(
     public columnDef: string,
     public header: string,
     public cell: (row: any) => any
   ) { }
-}
-
-
-export class ResultDataSource extends DataSource<object> {
-
-  constructor(private resultData: object[]) {
-    super();
-  }
-
-  connect(): Observable<object[]> {
-    return Observable.of(this.resultData);
-  }
-
-  disconnect() {
-  }
-
 }
