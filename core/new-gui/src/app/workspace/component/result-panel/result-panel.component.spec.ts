91c255782125af69f6da95040fe5549a718df8ea
Henry <whchen1@uci.edu>
2018-06-04 17:32:13 -0700

    add necessary dependencies to result panel component test

--- core/new-gui/src/app/workspace/component/result-panel/result-panel.component.spec.ts
+++ core/new-gui/src/app/workspace/component/result-panel/result-panel.component.spec.ts
@@ -1,6 +1,16 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ResultPanelComponent } from './result-panel.component';
+import { ExecuteWorkflowService } from "./../../service/execute-workflow/execute-workflow.service";
+import { StubExecuteWorkflowService } from "./../../service/execute-workflow/stub-execute-workflow.service";
+import { CustomNgMaterialModule } from "./../../../common/custom-ng-material.module";
+
+import { WorkflowActionService } from "./../../service/workflow-graph/model/workflow-action.service";
+import { JointUIService } from "./../../service/joint-ui/joint-ui.service";
+import { OperatorMetadataService } from "./../../service/operator-metadata/operator-metadata.service";
+import { StubOperatorMetadataService } from "./../../service/operator-metadata/stub-operator-metadata.service";
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 
 describe('ResultPanelComponent', () => {
   let component: ResultPanelComponent;
@@ -8,7 +18,17 @@ describe('ResultPanelComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ResultPanelComponent ]
+      declarations: [ ResultPanelComponent ],
+      imports: [
+        NgbModule.forRoot(),
+        CustomNgMaterialModule
+      ],
+      providers: [
+        WorkflowActionService,
+        JointUIService,
+        { provide: ExecuteWorkflowService, useClass: StubExecuteWorkflowService },
+        { provide: OperatorMetadataService, useClass: StubOperatorMetadataService }
+      ]
     })
     .compileComponents();
   }));
