c31bf5f43eaf1e74804a40565d820dc6ace4e9d6
Zhaomin Li <zhaominl@uci.edu>
2018-07-10 21:30:24 +0800

    add all testcases to test the SavedProject/Resource modifications

--- core/new-gui/src/app/dashboard/component/feature-container/saved-project-section/saved-project-section.component.spec.ts
+++ core/new-gui/src/app/dashboard/component/feature-container/saved-project-section/saved-project-section.component.spec.ts
@@ -1,13 +1,17 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { SavedProjectSectionComponent } from './saved-project-section.component';
+import { SavedProjectSectionComponent,
+  NgbdModalAddProjectComponent,
+  NgbdModalDeleteProjectComponent } from './saved-project-section.component';
 import { SavedProjectService } from '../../../service/saved-project/saved-project.service';
 import { StubSavedProjectService } from '../../../service/saved-project/stub-saved-project.service';
 import {MatDividerModule} from '@angular/material/divider';
 import {MatListModule} from '@angular/material/list';
 import {MatCardModule} from '@angular/material/card';
+import {MatDialogModule} from '@angular/material/dialog';
 
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbModule, NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
 
 import { SavedProject } from '../../../type/saved-project';
 
@@ -17,6 +21,13 @@ import { HttpModule } from '@angular/http';
 describe('SavedProjectSectionComponent', () => {
   let component: SavedProjectSectionComponent;
   let fixture: ComponentFixture<SavedProjectSectionComponent>;
+
+  let addcomponent: NgbdModalAddProjectComponent;
+  let addfixture: ComponentFixture<NgbdModalAddProjectComponent>;
+
+  let deletecomponent: NgbdModalDeleteProjectComponent;
+  let deletefixture: ComponentFixture<NgbdModalDeleteProjectComponent>;
+
   const TestCase: SavedProject[] = [
     {
       id: '1',
@@ -52,14 +63,19 @@ describe('SavedProjectSectionComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SavedProjectSectionComponent ],
+      declarations: [ SavedProjectSectionComponent,
+        NgbdModalAddProjectComponent,
+        NgbdModalDeleteProjectComponent ],
       providers: [
-        { provide: SavedProjectService, useClass: StubSavedProjectService }
+        { provide: SavedProjectService, useClass: StubSavedProjectService },
+        NgbActiveModal
       ],
       imports: [MatDividerModule,
         MatListModule,
         MatCardModule,
+        MatDialogModule,
         NgbModule.forRoot(),
+        FormsModule,
         HttpModule]
     })
     .compileComponents();
@@ -109,6 +125,37 @@ describe('SavedProjectSectionComponent', () => {
     const SortedCase = component.projects.map(item => item.lastModifiedTime);
     expect(SortedCase)
       .toEqual(['2018-01-22T17:26:50Z', '2018-01-17T06:26:50Z', '2018-01-17T06:26:50Z', '2018-01-14T22:56:50Z', '2018-01-14T22:56:50Z']);
+  });
+
+  it('addProjectComponent addProject should add new project', () => {
+    addfixture = TestBed.createComponent(NgbdModalAddProjectComponent);
+    addcomponent = addfixture.componentInstance;
+    addfixture.detectChanges();
+
+    let getResult: String = null;
+    addcomponent.name = 'test';
+    addcomponent.newProject.subscribe(out => getResult = out);
+    addcomponent.addProject();
+
+    expect(getResult).toEqual('test');
+  });
+
+  it('deleteProjectComponent deleteSavedProject should delete project in list', () => {
+    deletefixture = TestBed.createComponent(NgbdModalDeleteProjectComponent);
+    deletecomponent = deletefixture.componentInstance;
+    // deletefixture.detectChanges();
+
+    let getBool: boolean;
+    deletecomponent.project = {
+      id: '4',
+      name: 'project 1',
+      creationTime: '2017-10-25T12:34:50Z',
+      lastModifiedTime: '2018-01-17T06:26:50Z',
+    };
+    deletecomponent.deleteProject.subscribe(outb => getBool = outb);
+    deletecomponent.deleteSavedProject();
+
+    expect(getBool).toEqual(true);
   });
 
 });
--- core/new-gui/src/app/dashboard/component/feature-container/user-dictionary-section/user-dictionary-section.component.spec.ts
+++ core/new-gui/src/app/dashboard/component/feature-container/user-dictionary-section/user-dictionary-section.component.spec.ts
@@ -1,22 +1,41 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { UserDictionarySectionComponent } from './user-dictionary-section.component';
+import { UserDictionarySectionComponent,
+  NgbdModalResourceAddComponent,
+  NgbdModalResourceDeleteComponent,
+  NgbdModalResourceViewComponent } from './user-dictionary-section.component';
 import { UserDictionaryService } from '../../../service/user-dictionary/user-dictionary.service';
 import { StubUserDictionaryService } from '../../../service/user-dictionary/stub-user-dictionary.service';
 
 import {MatDividerModule} from '@angular/material/divider';
 import {MatListModule} from '@angular/material/list';
 import {MatCardModule} from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatChipsModule} from '@angular/material/chips';
+import {MatIconModule} from '@angular/material/icon';
 
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbModule, NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
 
 import { UserDictionary } from '../../../type/user-dictionary';
 
 import { HttpModule } from '@angular/http';
+import { NgbdModalAddProjectComponent } from '../saved-project-section/saved-project-section.component';
 
 describe('UserDictionarySectionComponent', () => {
   let component: UserDictionarySectionComponent;
   let fixture: ComponentFixture<UserDictionarySectionComponent>;
+
+  let addcomponent: NgbdModalResourceAddComponent;
+  let addfixture: ComponentFixture<NgbdModalResourceAddComponent>;
+
+  let viewcomponent: NgbdModalResourceViewComponent;
+  let viewfixture: ComponentFixture<NgbdModalResourceViewComponent>;
+
+  let deletecomponent: NgbdModalResourceDeleteComponent;
+  let deletefixture: ComponentFixture<NgbdModalResourceDeleteComponent>;
+
   const TestCase: UserDictionary[] = [
     {
       id: '1',
@@ -38,14 +57,24 @@ describe('UserDictionarySectionComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ UserDictionarySectionComponent ],
+      declarations: [ UserDictionarySectionComponent,
+        NgbdModalResourceAddComponent,
+        NgbdModalResourceDeleteComponent,
+        NgbdModalResourceViewComponent ],
       providers: [
-        { provide: UserDictionaryService, useClass: StubUserDictionaryService }
+        { provide: UserDictionaryService, useClass: StubUserDictionaryService },
+        NgbActiveModal
       ],
       imports: [MatCardModule,
         MatDividerModule,
         MatListModule,
+        MatFormFieldModule,
+        MatDialogModule,
+        MatChipsModule,
+        MatIconModule,
+
         NgbModule.forRoot(),
+        FormsModule,
         HttpModule]
     })
     .compileComponents();
@@ -87,4 +116,77 @@ describe('UserDictionarySectionComponent', () => {
     expect(SortedCase)
       .toEqual(['immigration policy', 'gun control', 'police violence']);
   });
+
+  it('resourceViewComponent addKey should generate new key', () => {
+    viewfixture = TestBed.createComponent(NgbdModalResourceViewComponent);
+    viewcomponent = viewfixture.componentInstance;
+    // viewfixture.detectChanges();
+
+    let getResult: String = null;
+    viewcomponent.dictionary = {
+      id: '1',
+      name: 'police violence',
+      items: ['BLM']
+    };
+    viewcomponent.name = 'test';
+    viewcomponent.ifAdd = true;
+    viewcomponent.addedName.subscribe(out => getResult = out);
+    viewcomponent.addKey();
+
+    expect(getResult).toEqual('test');
+  });
+
+  it('resourceViewComponent remove should indicate the key to be removed', () => {
+    viewfixture = TestBed.createComponent(NgbdModalResourceViewComponent);
+    viewcomponent = viewfixture.componentInstance;
+
+    let getRemove: String = null;
+    viewcomponent.dictionary = {
+      id: '1',
+      name: 'police violence',
+      items: ['BLM']
+    };
+    let item: String;
+    item  = 'deleted keyword';
+    viewcomponent.deleteName.subscribe(outr => getRemove = outr);
+    viewcomponent.remove(item);
+
+    expect(getRemove).toEqual('deleted keyword');
+  });
+
+  it('resourceAddComponent addKey should add a new dictionary', () => {
+    addfixture = TestBed.createComponent(NgbdModalResourceAddComponent);
+    addcomponent = addfixture.componentInstance;
+    // addfixture.detectChanges();
+
+    let getResultDict: UserDictionary = null;
+
+    addcomponent.dictContent = 'key1,key2,key3';
+    addcomponent.name = 'test';
+    addcomponent.separator = ',';
+    addcomponent.addedDictionary.subscribe(outd => getResultDict = outd);
+    addcomponent.addKey();
+
+    expect(getResultDict.id).toEqual('1');
+    expect(getResultDict.name).toEqual('test');
+    expect(getResultDict.items).toEqual(['key1', 'key2', 'key3']);
+  });
+
+  it('resourceDeleteComponent deleteDictionary should delete a certain dictionary', () => {
+    deletefixture = TestBed.createComponent(NgbdModalResourceDeleteComponent);
+    deletecomponent = deletefixture.componentInstance;
+    // deletefixture.detectChanges();
+
+    deletecomponent.dictionary = {
+      id: '1',
+      name: 'police violence',
+      items: ['BLM']
+    };
+    let deleteSignal: Boolean = false;
+    deletecomponent.deleteDict.subscribe(outb => deleteSignal = outb);
+    deletecomponent.deleteDictionary();
+
+    expect(deleteSignal).toEqual(true);
+  });
+
 });
