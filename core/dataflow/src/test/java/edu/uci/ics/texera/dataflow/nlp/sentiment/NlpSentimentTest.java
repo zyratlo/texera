6e3ac13d28d9863ebadde1ee9e62b4abbdc20965
ZhihuiYangCS <zhyangcs@gmail.com>
2019-02-06 16:59:04 -0800

    Format: change tab to space

--- core/dataflow/src/test/java/edu/uci/ics/texera/dataflow/nlp/sentiment/NlpSentimentTest.java
+++ core/dataflow/src/test/java/edu/uci/ics/texera/dataflow/nlp/sentiment/NlpSentimentTest.java
@@ -12,7 +12,7 @@ import edu.uci.ics.texera.dataflow.sink.tuple.TupleSink;
 import edu.uci.ics.texera.dataflow.source.tuple.TupleSourceOperator;
 
 public class NlpSentimentTest {
-    
+
     /*
      * Test sentiment with a positive sentence, result should be 3 (positive)
      */
@@ -23,18 +23,18 @@ public class NlpSentimentTest {
         NlpSentimentOperator sentiment = new NlpSentimentOperator(
                 new NlpSentimentPredicate(NlpSentimentTestConstants.TEXT, "sentiment"));
         TupleSink tupleSink = new TupleSink();
-        
+
         sentiment.setInputOperator(tupleSource);
         tupleSink.setInputOperator(sentiment);
-        
+
         tupleSink.open();
         List<Tuple> results = tupleSink.collectAllTuples();
         tupleSink.close();
-        
+
         Tuple tuple = results.get(0);
         Assert.assertEquals(tuple.getField("sentiment").getValue(), SentimentConstants.POSITIVE);
     }
-    
+
     /*
      * Test sentiment with a neutral sentence, result should be 2 (neutral)
      */
@@ -45,18 +45,18 @@ public class NlpSentimentTest {
         NlpSentimentOperator sentiment = new NlpSentimentOperator(
                 new NlpSentimentPredicate(NlpSentimentTestConstants.TEXT, "sentiment"));
         TupleSink tupleSink = new TupleSink();
-        
+
         sentiment.setInputOperator(tupleSource);
         tupleSink.setInputOperator(sentiment);
-        
+
         tupleSink.open();
         List<Tuple> results = tupleSink.collectAllTuples();
         tupleSink.close();
-        
+
         Tuple tuple = results.get(0);
-        Assert.assertEquals(tuple.getField("sentiment").getValue(), SentimentConstants.NEUTRAL);     
+        Assert.assertEquals(tuple.getField("sentiment").getValue(), SentimentConstants.NEUTRAL);
     }
-    
+
     /*
      * Test sentiment with a negative sentence, result should be 1 (negative)
      */
@@ -67,37 +67,40 @@ public class NlpSentimentTest {
         NlpSentimentOperator sentiment = new NlpSentimentOperator(
                 new NlpSentimentPredicate(NlpSentimentTestConstants.TEXT, "sentiment"));
         TupleSink tupleSink = new TupleSink();
-        
+
         sentiment.setInputOperator(tupleSource);
         tupleSink.setInputOperator(sentiment);
-        
+
         tupleSink.open();
         List<Tuple> results = tupleSink.collectAllTuples();
         tupleSink.close();
-        
+
         Tuple tuple = results.get(0);
-        Assert.assertEquals(tuple.getField("sentiment").getValue(), SentimentConstants.NEGATIVE);        
-    }  
+        Assert.assertEquals(tuple.getField("sentiment").getValue(), SentimentConstants.NEGATIVE);
+    }
+
     /*
-     * Test sentiment with multiple sentences in a tweet, 
-     * the sentiment result should equal to the sentiment of the longest sentence.
-     * For this input "Blabla. Bugs are always annoying. But programming is so super awesome. Blabla.", the result should be 3 (positive)
+     * Test sentiment with multiple sentences in a tweet, the sentiment result
+     * should equal to the sentiment of the longest sentence. For this input
+     * "Blabla. Bugs are always annoying. But programming is so super awesome. Blabla."
+     * , the result should be 3 (positive)
      */
     @Test
-    public void test4() throws TexeraException{
-    	TupleSourceOperator tupleSource = new TupleSourceOperator(
-                Arrays.asList(NlpSentimentTestConstants.MULTIPLE_SENTENCES_TUPLE), NlpSentimentTestConstants.SENTIMENT_SCHEMA);
-    	NlpSentimentOperator sentiment = new NlpSentimentOperator(
+    public void test4() throws TexeraException {
+        TupleSourceOperator tupleSource = new TupleSourceOperator(
+                Arrays.asList(NlpSentimentTestConstants.MULTIPLE_SENTENCES_TUPLE),
+                NlpSentimentTestConstants.SENTIMENT_SCHEMA);
+        NlpSentimentOperator sentiment = new NlpSentimentOperator(
                 new NlpSentimentPredicate(NlpSentimentTestConstants.TEXT, "sentiment"));
-    	TupleSink tupleSink = new TupleSink();
-    	
-    	sentiment.setInputOperator(tupleSource);
+        TupleSink tupleSink = new TupleSink();
+
+        sentiment.setInputOperator(tupleSource);
         tupleSink.setInputOperator(sentiment);
-        
+
         tupleSink.open();
         List<Tuple> results = tupleSink.collectAllTuples();
         tupleSink.close();
-        
+
         Tuple tuple = results.get(0);
         Assert.assertEquals(tuple.getField("sentiment").getValue(), SentimentConstants.POSITIVE);
     }
