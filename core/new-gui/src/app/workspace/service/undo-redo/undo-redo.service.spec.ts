5f539cc2e5c5cb643c536e75a5abe719957cb495
kerryqliu <kerryqliu@gmail.com>
2019-10-28 14:13:06 -0700

    Fixed provider for tests

--- core/new-gui/src/app/workspace/service/undo-redo/undo-redo.service.spec.ts
+++ core/new-gui/src/app/workspace/service/undo-redo/undo-redo.service.spec.ts
@@ -1,64 +1,59 @@
 import { StubOperatorMetadataService } from './../operator-metadata/stub-operator-metadata.service';
 import { OperatorMetadataService } from './../operator-metadata/operator-metadata.service';
-import { Command, WorkflowActionService } from './../workflow-graph/model/workflow-action.service';
+import { WorkflowActionService } from './../workflow-graph/model/workflow-action.service';
 import {
   mockScanPredicate, mockResultPredicate, mockPoint
 } from './../workflow-graph/model/mock-workflow-data';
 import { TestBed, inject } from '@angular/core/testing';
 
 import { UndoRedoService } from './undo-redo.service';
-// make some mock data maybe
 
-// TODO FOR TESTING
-// Write test functions for navigation component, property editor, here, and workflow action
-// Let's first write a function in nav component, one for undo and one for redo
-describe('UndoRedoService', () => {
-  let undoRedoService: UndoRedoService;
+describe('service', () => {
+  let service: UndoRedoService;
   let workflowActionService: WorkflowActionService;
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [
         UndoRedoService,
-        WorkflowActionService,
         { provide: OperatorMetadataService, useClass: StubOperatorMetadataService }
       ]
     });
-    undoRedoService = TestBed.get(UndoRedoService);
-    workflowActionService = TestBed.get(WorkflowActionService);
+    service = TestBed.get(UndoRedoService);
+    workflowActionService = (service as any).WorkflowActionService;
   });
 
-  it('should be created', inject([UndoRedoService], (service: UndoRedoService) => {
-    expect(service).toBeTruthy();
+  it('should be created', inject([UndoRedoService], (injectedService: UndoRedoService) => {
+    expect(injectedService).toBeTruthy();
   }));
 
   it('executing command should append to stack', () => {
     workflowActionService.addOperator(mockScanPredicate, mockPoint);
 
-    expect(undoRedoService.getUndoLength).toEqual(1);
-    expect(undoRedoService.getRedoLength).toEqual(0);
+    expect(service.getUndoLength).toEqual(1);
+    expect(service.getRedoLength).toEqual(0);
   });
 
   it('redoing command should move from undo to redo stack and vice versa', () => {
     workflowActionService.addOperator(mockScanPredicate, mockPoint);
 
-    undoRedoService.undoAction();
-    expect(undoRedoService.getUndoLength).toEqual(0);
-    expect(undoRedoService.getRedoLength).toEqual(1);
+    service.undoAction();
+    expect(service.getUndoLength).toEqual(0);
+    expect(service.getRedoLength).toEqual(1);
 
-    undoRedoService.redoAction();
-    expect(undoRedoService.getUndoLength).toEqual(1);
-    expect(undoRedoService.getRedoLength).toEqual(0);
+    service.redoAction();
+    expect(service.getUndoLength).toEqual(1);
+    expect(service.getRedoLength).toEqual(0);
   });
 
   it('executing new action clears redo stack', () => {
     workflowActionService.addOperator(mockScanPredicate, mockPoint);
 
-    undoRedoService.undoAction();
-    expect(undoRedoService.getUndoLength).toEqual(0);
-    expect(undoRedoService.getRedoLength).toEqual(1);
+    service.undoAction();
+    expect(service.getUndoLength).toEqual(0);
+    expect(service.getRedoLength).toEqual(1);
 
     workflowActionService.addOperator(mockResultPredicate, mockPoint);
-    expect(undoRedoService.getUndoLength).toEqual(1);
-    expect(undoRedoService.getRedoLength).toEqual(0);
+    expect(service.getUndoLength).toEqual(1);
+    expect(service.getRedoLength).toEqual(0);
   });
 });
