7f286b5e5328cd2d9f34ec4cc3b59f660df650fb
Albert Liu <ayliu5@uci.edu>
2020-04-11 15:23:58 -0700

    Refactored code. Added comments
    refactored code to be more organized and easier to comment

--- core/new-gui/multi-target/src/cmd-target.ts
+++ core/new-gui/multi-target/src/cmd-target.ts
@@ -3,9 +3,21 @@ import { ChildProcess, spawn, execSync } from 'child_process';
 import { platform, kill } from 'process';
 import { JsonObject } from '@angular-devkit/core';
 
+// cmd-target logs cmd's output with a header that gets truncated to CMD_LENGTH characters
+//eg: "echo foo" -> "(echo foo): foo", "echo supercalifragilisticexpialidocious" -> "(ech...ous): supercalifragilisticexpialidocious"
 const CMD_LENGTH = 20;
+
+//static array of CmdTask. gets added to when creating a task. used to lookup tasks to kill.
 let liveTasks: CmdTask[] = [];
 
+/** 
+ * properties:  
+ *  - **cmd**: *str* command as you would copy-paste into a shell  
+ *  - **daemon**: *bool* true to run without logging cmd's output  
+ *  - **detached**: *bool* true to detach cmd's process. builder will resolve successfully immediately and cmd process will continue even if nodejs stops  
+ *  - **kill**: *bool* true to kill process with matching cmd that was run previously.
+ *  - **killChildren**: *bool* true to also kill process descendants (processes launched by cmd) 
+*/
 interface Options extends JsonObject {
   cmd: string;
   daemon: boolean;
@@ -14,24 +26,35 @@ interface Options extends JsonObject {
   killChildren: boolean;
 }
 
+/**
+ * properties:
+ *  - **process**: *ChildProcess* process object
+ *  - **cmd**: *str* command as you would copy-paste into a shell
+ */
 interface CmdTask {
   process: ChildProcess,
   cmd: string
 }
 
+
 type PromiseFunc = (value?: BuilderOutput | PromiseLike<BuilderOutput> | undefined) => void
 
-export default createBuilder<Options>((options, context) => {
-  return new Promise<BuilderOutput>(async (resolve: PromiseFunc, reject: PromiseFunc) => {
-    if (options.kill) {
-      killTarget(options, context, resolve, reject);
-    } else {
-      execTarget(options, context, resolve, reject);
-    }
-  });
-});
+export default createBuilder<Options>(cmdBuilder);
 
-function clampCmd(cmd: string, length: number) {
+function cmdBuilder(options: Options, context: BuilderContext): Promise<BuilderOutput> {
+  return new Promise<BuilderOutput>(async (resolve: PromiseFunc, reject: PromiseFunc) => {
+    let builderOutput: BuilderOutput = options.kill ? killTarget(options, context) : await execTarget(options, context);
+    builderOutput.success ? resolve(builderOutput) : reject(builderOutput);
+  });
+}
+
+/**
+ * 
+ * @param cmd string to clamp the length of
+ * @param length number of characters to clamp to
+ * @returns clamped cmd string
+ */
+function clampCmd(cmd: string, length: number): string {
   if (cmd.length <= length) {
     return cmd
   } else {
@@ -39,8 +62,14 @@ function clampCmd(cmd: string, length: number) {
   }
 }
 
-function killDescendants(parentPID: number) {
+/**
+ * 
+ * @param parentPID pid of parent process
+ */
+function killDescendants(parentPID: number): void {
   if (platform == "win32") {
+    // On windows, use wmic to lookup processes by PPID
+    // and using a Stack, "recursively" discover all descendants, and descendants of descendants, etc
     var descendantPids: Set<number> = new Set();
     var pidStack: number[] = [parentPID];
     while (pidStack.length > 0) {
@@ -48,9 +77,9 @@ function killDescendants(parentPID: number) {
       var currentPid: number = pidStack.pop();
       if (!descendantPids.has(currentPid)) {
         descendantPids.add(currentPid);
-        var wmicOutput: string = execSync(`wmic process where (ParentProcessId=${parentPID}) get ProcessId`).toString();
-        var lines: string[] = (wmicOutput.match(/\S+/g) || []);
-        lines.shift();
+        var wmicOutput: string = execSync(`wmic process where (ParentProcessId=${parentPID}) get ProcessId`).toString(); // executes wmic and returns str of output
+        var lines: string[] = (wmicOutput.match(/\S+/g) || []); // matches non-whitespace, forming array of "words"
+        lines.shift(); // remove 0th item, which would be the column label "ProcessId" if we were in a shell
         var childPids: number[] = lines.map(x => parseInt(x));
         childPids.forEach((pid: number) => {
           pidStack.push(pid);
@@ -61,14 +90,20 @@ function killDescendants(parentPID: number) {
     descendantPids.forEach((pid: number) => {
       kill(pid);
     });
-  } else if (platform == "linux"){
-    execSync(`kill -TERM -${parentPID}`);
+  } else if (platform == "linux") {
+    execSync(`kill -TERM -${parentPID}`); //kills parentPID assuming it's a linux process group. <3 linux so ez
   } else {
     // other possibilities are AIX|Darwin|FreeBSD|OpenBSD|SunOS
-    console.warn(`Warning: killing process descendants currently only supported on windows and linux. Please check manually if it worked.`)
+    console.warn(`Warning: killing process descendants currently only supported on windows and linux. Please check manually if it worked.`) // narrator: it didn't
   }
 }
 
+/**
+ * 
+ * @param cmd command as you would copy-paste into a shell
+ * @param killChildren true to also kill process descendants
+ * @returns true if successful, false if couldn't find CmdTask
+ */
 function killCmdTask(cmd: string, killChildren: boolean): boolean {
   let index = liveTasks.findIndex(element => element.cmd == cmd);
   if (index != -1) {
@@ -76,13 +111,21 @@ function killCmdTask(cmd: string, killChildren: boolean): boolean {
       killDescendants(liveTasks[index].process.pid);
     }
     liveTasks[index].process.kill('SIGINT');
-    liveTasks.splice(index, 1);
+    liveTasks.splice(index, 1); // remove livetask (since it's been killed)
     return true
   } else {
     return false
   }
 }
 
+/**
+ * 
+ * @param cmd command as you would copy-paste into a shell
+ * @param loggerPrefix prepended to cmd output (to label it)
+ * @param options instance of Options
+ * @param context instance of BuilderContext
+ * @returns created CmdTask, with process already running
+ */
 function execCmdTask(cmd: string, loggerPrefix: string, options: Options, context: BuilderContext): CmdTask {
   const child: ChildProcess = spawn(cmd, [], { stdio: options.daemon ? 'ignore' : 'pipe', detached: options.detached, shell: true });
 
@@ -99,15 +142,27 @@ function execCmdTask(cmd: string, loggerPrefix: string, options: Options, contex
   return { process: child, cmd: cmd };
 }
 
-function killTarget(options: Options, context: BuilderContext, resolve: PromiseFunc, reject: PromiseFunc) {
+/**
+ * 
+ * @param options instance of Options
+ * @param context instance of BuilderContext
+ * @returns BuilderOutput. BuilderOutput.success == true if successful
+ */
+function killTarget(options: Options, context: BuilderContext): BuilderOutput {
   context.logger.info(`Killing ${options.daemon ? "daemon " : ""} '${options.cmd}'`);
-  if (!killCmdTask(options.cmd, options.killChildren)) {  
+  if (!killCmdTask(options.cmd, options.killChildren)) {
     context.logger.warn(`Couldn't find/kill cmd '${options.cmd}'. it may not have been executed or already terminated`);
+    return { success: false, target: context.target as Target }
   }
-  resolve({ success: true, target: context.target as Target });
+  return { success: true, target: context.target as Target }
 }
 
-function execTarget(options: Options, context: BuilderContext, resolve: PromiseFunc, reject: PromiseFunc) {
+/**
+ * 
+ * @param options instance of Options
+ * @param context instance of BuilderContext
+ */
+function execTarget(options: Options, context: BuilderContext): Promise<BuilderOutput> {
   context.reportStatus(options.cmd);
   context.logger.info(`Executing ${options.daemon ? "as daemon " : ""} '${options.cmd}'`);
 
@@ -116,19 +171,24 @@ function execTarget(options: Options, context: BuilderContext, resolve: PromiseF
   liveTasks.push(task);
 
   if (options.detached) {
-    resolve({ success: true, target: context.target as Target });
+    return Promise.resolve({ success: true, target: context.target as Target });
   } else {
-    task.process.on('close', code => {
-      killCmdTask(options.cmd, options.killChildren);
-      if (code === 0) {
-        context.reportStatus(`${prefix} successfully terminated`);
-        context.logger.info(`${prefix} successfully terminated)`);
-        resolve({ success: true, target: context.target as Target });
-      } else {
-        context.reportStatus(`${prefix} terminated (code ${code})`);
-        context.logger.info(`${prefix} terminated (code ${code})`);
-        reject({ success: false, target: context.target as Target });
-      }
-    });
+    return new Promise<BuilderOutput>((resolve, reject) => {
+      task.process.on('close', code => {
+        let index = liveTasks.findIndex(element => element == task);
+        if (index != -1) {
+          liveTasks.splice(index, 1); // remove livetask (since it terminated)
+        }
+        if (code === 0) {
+          context.reportStatus(`${prefix} successfully terminated`);
+          context.logger.info(`${prefix} successfully terminated)`);
+          resolve({ success: true, target: context.target as Target });
+        } else {
+          context.reportStatus(`${prefix} terminated (code ${code})`);
+          context.logger.fatal(`${prefix} terminated (code ${code})`);
+          resolve({ success: false, target: context.target as Target });
+        }
+      });
+    })
   }
 }
--- core/new-gui/multi-target/src/multi-target.ts
+++ core/new-gui/multi-target/src/multi-target.ts
@@ -1,101 +1,187 @@
 import { BuilderContext, BuilderOutput, BuilderRun, Target, ScheduleOptions, createBuilder, targetStringFromTarget, targetFromTargetString, scheduleTargetAndForget } from '@angular-devkit/architect';
-import { JsonObject, logging} from '@angular-devkit/core';
-import * as childProcess from 'child_process';
-import { Observable } from '@angular-devkit/core/node_modules/rxjs';
+import { JsonObject, logging } from '@angular-devkit/core';
 
+/**
+ * properties:  
+ *  - **targets**: *str[]* list of build targets
+ *  - **sequential**: *bool* run targets in order
+ *  - **race**: *bool* run targets in parallel until one of them finishes first (then stop others)
+ */
 interface Options extends JsonObject {
   targets: string[];
   sequential: boolean;
   race: boolean;
 }
 
-interface JobData extends Object {
-  buildFails: number;
+/**
+ * properties:  
+ *  - **totalTargets** *number*
+ * [//]: #
+ * methods:  
+ *  - **incrementTargetFails**()
+ *  - **incrementTargetSuccesses**()
+ *  - **getTargetFails**()
+ *  - **getTargetSuccesses**()
+ *  - **subscribeToFails**(fn: *(targetFails: number) => void*)
+ *  - **subscribeToSuccesses**(fn: *(targetSuccesses: number) => void*)
+ */
+class TargetData {
+  totalTargets: number;
+  private targetFails: number;
+  private targetSuccesses: number;
+
+  failSubscribers: { (targetFails: number): void }[] = [];
+  successSubscribers: { (targetFails: number): void }[] = [];
+
+  failedTargets: Target[];
+  successfulTargets: Target[];
+
+  constructor(totalTargets: number, targetFails: number, targetSuccesses: number) {
+    this.totalTargets = totalTargets;
+    this.targetFails = targetFails;
+    this.targetSuccesses = targetSuccesses;
+    this.failedTargets = [];
+    this.successfulTargets = [];
+  }
+
+  /**
+   * increments targetFails (+1)
+   * @event Calls all failSubscribers as a side effect
+   */
+  incrementTargetFails() {
+    this.targetFails += 1;
+    this.failSubscribers.forEach(fn => fn(this.targetFails))
+  }
+
+  /**
+   * increments targetSuccesses (+1)
+   * @event Calls all successSubscribers as a side effect
+   */
+  incrementTargetSuccesses() {
+    this.targetSuccesses += 1;
+    this.successSubscribers.forEach(fn => fn(this.targetSuccesses))
+  }
+
+  getTargetFails(): number {
+    return this.targetFails;
+  }
+
+  getTargetSuccesses(): number {
+    return this.targetSuccesses;
+  }
+
+  /**
+   * 
+   * @param fn gets called every incrementTargetFails()
+   */
+  subscribeToFails(fn: { (targetFails: number): void }) {
+    this.failSubscribers.push(fn);
+  }
+
+  /**
+   * 
+   * @param fn gets called every incrementTargetSuccesses()
+   */
+  subscribeToSuccesses(fn: { (targetSuccesses: number): void }) {
+    this.successSubscribers.push(fn);
+  }
 }
 
 export default createBuilder(multiBuilder);
 
-function multiBuilder( options: Options, context: BuilderContext): Promise<BuilderOutput> {
-  return new Promise<BuilderOutput>(async (resolve, reject) => {
-    let multiTargetStr: string = (context.target === undefined) ? "Multi-Target Builder" : context.target.project + ":" + context.target.target + ":" + context.target.configuration;
-    
-    if(options.sequential && options.race){
-      context.logger.fatal(`(${multiTargetStr}) invalid config: options 'race:true' and 'sequential:true' are incompatible`);
-      reject({success: false, target: context.target as Target})
-    }
-    
-    let builderOutputs: Promise<BuilderOutput>[] = [];
-    let builderRuns: Promise<BuilderRun>[] = [];
-    let jobData: JobData = {buildFails: 0};
-    
-    for (var i: number = 0; i < options.targets.length; i++){
-      builderRuns.push(scheduleTarget(options.targets[i],context));
-      
-      builderRuns[i].catch((error) => {
-        context.logger.fatal(`(${multiTargetStr})-->(${options.targets[i]})failed!`)
-        reject({success: false, target: context.target as Target, error: error.toString()})
-      })
-      
-      builderRuns[i].then((builderRun: BuilderRun) => {
-        builderOutputs.push(builderRun.result);
-        builderRun.result.catch((builderOutput: BuilderOutput) => {
-          let subTargetStr: string = (builderOutput.target === undefined) ? "Anonymous " + builderRun.info.builderName : builderOutput.target.project + ":" + builderOutput.target.target + ":" + builderOutput.target.configuration
+/**
+ * 
+ * @param options Options
+ * @param context BuilderContext
+ * @returns promise of builderOutput, resolves if build successful, rejects if not, always produces a BuilderOutput either way.
+ */
+function multiBuilder(options: Options, context: BuilderContext): Promise<BuilderOutput> {
+  // For help understanding builders/Architect, visit https://angular.io/guide/cli-builder.
 
-          if(options.race){
-            context.logger.warn(`(${multiTargetStr})-->(${subTargetStr}) failed! Ignoring due to config(race:true). `)
-            jobData.buildFails += 1;
-            if(jobData.buildFails >= options.targets.length){
-              reject({success: false, target: context.target as Target});
-            }
-          } else {
-            context.logger.fatal(`(${multiTargetStr})-->(${subTargetStr}) failed!`)
-            reject(builderOutput);
-          }
-          
-        });
-      });
-      if (options.sequential){
-        await (await builderRuns[i]).result;
-      } else if (options.race){
-        builderRuns[i].then((builderRun: BuilderRun) =>{
-          builderRun.result.then((builderOutput: BuilderOutput) => {
-            context.logger.warn("resolving race")
-            context.logger.warn(`${i} of ${options.targets.length} targets dispatched`);
-            let stopPromises: Promise<void>[] = [];
-            for(var j: number = 0; j < i; j++){
-              builderRuns[j].then((runToStop: BuilderRun) => {
-                if (runToStop != builderRun || true){
-                  context.logger.warn(`${builderRun.info.builderName} stopped`)
-                  stopPromises.push(runToStop.stop());
-                }
-              });
-            }
-            i = options.targets.length;
-            Promise.all(builderRuns).then(() => {
-              Promise.all(stopPromises).then(() => {
-                context.logger.warn("resolved race");
-                resolve({success: true, target: context.target as Target});
-              });
-            });
-          });
-        });
-      }
-      await builderRuns[i];
+  // const used to label/prefix logs
+  const multiTargetStr: string = (context.target === undefined) ? "Anonymous Multi-Target Builder" : targetStringFromTarget(context.target);
+
+  return new Promise<BuilderOutput>(async (resolve, reject) => {
+
+    if (options.sequential && options.race) {
+      let errorMsg = `(${multiTargetStr}) invalid config: options 'race: true' and 'sequential: true' are incompatible`
+      context.logger.fatal(errorMsg);
+      reject({ success: false, target: context.target as Target, error: errorMsg })
     }
-    if (!options.race){
-      Promise.all(builderRuns).then(() => {
-        Promise.all(builderOutputs).then(() => {
-          resolve({success: true, target: context.target as Target})
+
+    // Each BuilderRun represents a builder successfully scheduled and running.
+    // Just as this builder function returns a Promise<BuilderOutput>, each BuilderRun.result is the Promise<BuilderOutput> of that builder
+    let builderRuns: Promise<BuilderRun>[] = [];
+    // As each BuilderRun.result resolves/rejects, we increment targetData.
+    let targetData = new TargetData(options.targets.length, 0, 0);
+
+    // Subscribing to changes in targetData.targetFails lets us determine when to reject (this builder has failed)
+    targetData.subscribeToFails((targetFails: number) => {
+      if (!options.race) {
+        let subTargetStr = targetStringFromTarget(targetData.failedTargets[targetData.failedTargets.length - 1])
+        let errorMsg = `(${multiTargetStr})-->(${subTargetStr}) failed!`
+        context.logger.fatal(errorMsg)
+        reject({ success: false, target: context.target as Target, error: errorMsg })
+      }
+      else if (options.race && targetFails == targetData.totalTargets) {
+        let errorMsg = `(${multiTargetStr}) failed: all sub-targets failed! (race: true).`
+        context.logger.fatal(errorMsg)
+        reject({ success: false, target: context.target as Target, error: errorMsg })
+      }
+      else if (options.race && targetFails < targetData.totalTargets) {
+        let subTargetStr = targetStringFromTarget(targetData.failedTargets[targetData.failedTargets.length - 1])
+        let errorMsg = `(${multiTargetStr})-->(${subTargetStr}) failed. Ignoring due to config (race: true) `
+        context.logger.warn(errorMsg);
+      }
+    });
+
+    // Subscribing to changes in targetData.targetSuccesses lets us determine when to resolve (this builder has succeeded)
+    targetData.subscribeToSuccesses((targetSuccesses: number) => {
+      if (options.race || targetSuccesses == targetData.totalTargets) {
+        resolve({ success: true, target: context.target as Target });
+      }
+    });
+
+    // For each target, schedule it, and add hooks that increment targetData with each build fail/success.
+    for (var i: number = 0; i < options.targets.length; i++) {
+      let target = targetFromTargetString(options.targets[i]);
+
+      builderRuns.push(scheduleTarget(target, context));
+      // a target can fail to be scheduled if target doesn't exist or couldn't find module dependency.
+      builderRuns[i].catch((error: any) => {
+        targetData.failedTargets.push(target);
+        targetData.incrementTargetFails();
+      });
+      // when a target is scheduled successfully, the builder that is scheduled can fail too:
+      builderRuns[i].then((builderRun: BuilderRun) => {
+        //... for any reason, e.g. Cmd-target with cmd = "asdf" --(console)-> "'asdf' is not recognized as an internal or external command"
+        builderRun.result.catch((error: any) => {
+          targetData.failedTargets.push(target);
+          targetData.incrementTargetFails();
+        });
+        //... but if it succeeds, increment jobData
+        builderRun.result.then((builderOutput: BuilderOutput) => {
+          targetData.successfulTargets.push(target);
+          targetData.incrementTargetSuccesses();
         });
       });
+      // if sequential, await scheduleTarget scheduling a BuilderRun and said BuilderRun.result resolving into a BuilderOutput
+      if (options.sequential) { await (await builderRuns[i]).result }
     }
   });
 }
 
-function scheduleTarget(targetStr: string, context: BuilderContext): Promise<BuilderRun>{
-  let target = targetFromTargetString(targetStr);
+/**
+ * 
+ * @param target Build Target to schedule
+ * @param context Builder Context to schedule with
+ * @returns Promise of a **BuilderRun** (resolves if scheduled successfully).  
+ * Each **BuilderRun** represents a builder successfully scheduled and running.  
+ * Each **BuilderRun.result** is the **Promise<BuilderOutput>** of that builder (resolves if builder runs successfully). 
+ */
+function scheduleTarget(target: Target, context: BuilderContext): Promise<BuilderRun> {
   //@ts-ignore logger is actually Logger but was interfaced (as part of BuilderContext) into a LoggerApi.
-  let opt: ScheduleOptions = {logger: <logging.Logger> context.logger};
-  let overrides: JsonObject|undefined = undefined;
-  return context.scheduleTarget(target,overrides,opt);
+  let opt: ScheduleOptions = { logger: <logging.Logger>context.logger };
+  let overrides: JsonObject | undefined = undefined;
+  return context.scheduleTarget(target, overrides, opt);
 }
