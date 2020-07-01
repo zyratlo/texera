448f071df7efa3f9d2c9d7ab80af457b5c7f792f
Murphy Han <11711416@mail.sustech.edu.cn>
2020-07-01 21:32:21 +0800

    Add optimization in PieChartSink

--- core/dataflow/src/main/java/edu/uci/ics/texera/dataflow/sink/piechart/PieChartSink.java
+++ core/dataflow/src/main/java/edu/uci/ics/texera/dataflow/sink/piechart/PieChartSink.java
@@ -5,6 +5,11 @@ import edu.uci.ics.texera.api.dataflow.IOperator;
 import edu.uci.ics.texera.api.dataflow.ISink;
 import edu.uci.ics.texera.api.exception.DataflowException;
 import edu.uci.ics.texera.api.exception.TexeraException;
+import edu.uci.ics.texera.api.field.DoubleField;
+import edu.uci.ics.texera.api.field.IField;
+import edu.uci.ics.texera.api.field.IntegerField;
+import edu.uci.ics.texera.api.field.StringField;
+import edu.uci.ics.texera.api.field.TextField;
 import edu.uci.ics.texera.api.schema.Attribute;
 import edu.uci.ics.texera.api.schema.AttributeType;
 import edu.uci.ics.texera.api.schema.Schema;
@@ -67,14 +72,74 @@ public class PieChartSink extends VisualizationOperator {
         while ( (tuple = inputOperator.getNextTuple()) != null) {
             list.add(tuple);
         }
-        result = list.stream()
+        list = list.stream()
             .map(e -> new Tuple(outputSchema, e.getField(predicate.getNameColumn()), e.getField(predicate.getDataColumn())))
             .collect(Collectors.toList());
 
-        String a = "102";
+        list.sort((left, right) -> {
+            double leftValue =  extractNumber(left.getField(predicate.getDataColumn()));
+            double rightValue = extractNumber(right.getField(predicate.getDataColumn())) ;
+            if ( leftValue < rightValue ) {
+                return 1;
+            } else if (leftValue == rightValue) {
+                return 0;
+            } else {
+                return -1;
+            }
+        });
+
+        double sum = 0.0;
+        for (Tuple t: list) {
+            sum += extractNumber(t.getField(predicate.getDataColumn()));
+        }
+        double total = 0.0;
+        for (Tuple t: list) {
+            total += extractNumber(t.getField(predicate.getDataColumn()));
+            result.add(t);
+            if (total / sum > predicate.getPruneRatio()) {
+
+                IField nameField =  buildOtherNameField();
+                IField dataField =  buildOtherDataField(sum - total);
+                result.add(new Tuple(outputSchema, nameField, dataField));
+                return;
+            }
+        }
+
+
+
+    }
+    private IField buildOtherNameField() {
+
+
+        Attribute nameColumn =   inputOperator.getOutputSchema().getAttribute(predicate.getNameColumn());
+        AttributeType nameColumnType = nameColumn.getType();
+        if (nameColumnType.equals(AttributeType.STRING)) {
+            return new StringField("Other");
+        }
+        return new TextField("Other");
     }
 
+    private IField buildOtherDataField(double value) {
+        Attribute dataColumn =   inputOperator.getOutputSchema().getAttribute(predicate.getDataColumn());
+        AttributeType  dataColumnType = dataColumn.getType();
 
+        if (dataColumnType.equals(AttributeType.INTEGER)) {
+            return new IntegerField((int)value);
+        }
+        return new DoubleField(value);
 
+    }
+
+    private double extractNumber(IField field) {
+        if (field instanceof DoubleField) {
+            DoubleField doubleField = (DoubleField)field;
+            return doubleField.getValue();
+        }
+        else if (field instanceof IntegerField) {
+            IntegerField integerField = (IntegerField)field;
+            return integerField.getValue().doubleValue();
+        }
+        return 0.0;
+    }
 
 }
