9cd9a0179924a3441c9a45fe510b37b63442e215
Mingji Han <11711416@mail.sustech.edu.cn>
2020-07-10 15:36:43 +0800

    clean code

--- core/new-gui/src/app/workspace/component/result-panel/result-panel.component.ts
+++ core/new-gui/src/app/workspace/component/result-panel/result-panel.component.ts
@@ -9,8 +9,8 @@ import { ExecutionResult, SuccessExecutionResult } from './../../types/execute-w
 import { TableColumn, IndexableObject } from './../../types/result-table.interface';
 import { ResultPanelToggleService } from './../../service/result-panel-toggle/result-panel-toggle.service';
 import deepMap from 'deep-map';
-import { isEqual, repeat } from 'lodash';
-import { ResultObject } from '../../types/execute-workflow.interface'
+import { isEqual } from 'lodash';
+import { ResultObject } from '../../types/execute-workflow.interface';
 import { WorkflowActionService } from '../../service/workflow-graph/model/workflow-action.service';
 /**
  * ResultPanelCompoent is the bottom level area that displays the
--- core/new-gui/src/app/workspace/component/visualization-panel-content/visualization-panel-content.component.spec.ts
+++ core/new-gui/src/app/workspace/component/visualization-panel-content/visualization-panel-content.component.spec.ts
@@ -3,23 +3,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { VisualizationPanelContentComponent } from './visualization-panel-content.component';
 
 describe('VisualizationPanelContentComponent', () => {
-  let component: VisualizationPanelContentComponent;
-  let fixture: ComponentFixture<VisualizationPanelContentComponent>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ VisualizationPanelContentComponent ]
-    })
-    .compileComponents();
-  }));
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(VisualizationPanelContentComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
 
   it('should create', () => {
-    expect(component).toBeTruthy();
   });
 });
