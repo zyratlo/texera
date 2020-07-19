cdd2f33e5a4ac47a3c28a98c17ad4cd80f0ec2d2
Mingji Han <11711416@mail.sustech.edu.cn>
2020-07-19 11:27:06 +0800

    remove unnecessary interfaces

--- core/dataflow/src/main/java/edu/uci/ics/texera/dataflow/sink/ITupleSink.java
+++ core/dataflow/src/main/java/edu/uci/ics/texera/dataflow/sink/ITupleSink.java
@@ -1,15 +1,12 @@
 package edu.uci.ics.texera.dataflow.sink;
 
-import edu.uci.ics.texera.api.dataflow.IOperator;
 import edu.uci.ics.texera.api.dataflow.ISink;
-import edu.uci.ics.texera.api.schema.Schema;
 import edu.uci.ics.texera.api.tuple.Tuple;
 import java.util.List;
 
 public interface ITupleSink extends ISink {
-    void setInputOperator(IOperator inputOperator);
 
     List<Tuple> collectAllTuples();
 
-    Schema transformToOutputSchema(Schema... inputSchema);
+
 }
