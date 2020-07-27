07c69263a6d16d07e7c35780d16a47abde32fd8f
Yinan Zhou <zhouyinan49@gmail.com>
2020-07-26 17:16:18 -0700

    added some comments

--- core/scripts/sql/texera_ddl.sql
+++ core/scripts/sql/texera_ddl.sql
@@ -53,10 +53,3 @@ CREATE TABLE IF NOT EXISTS `UserWorkflow` (
     -- uncomment below to give workflows the concept of ownership
     -- FOREIGN KEY (`userID`) REFERENCES `UserAccount`(`userID`) ON DELETE CASCADE
 )ENGINE=INNODB;
-
--- the below code is a fixed workflow
-INSERT INTO `UserWorkflow` VALUES (
-    'tobacco-analysis-workflow',
-    'tobacco-analysis-workflow',
-    '{"operators":[{"limit":100,"offset":0,"operatorID":"operator-bb71eaad-615f-4298-843b-bbcca9d5b694","operatorType":"ViewResults"},{"batchSize":10,"arrowBatchSize":10,"resultAttribute":"prediction","inputAttributeModel":"NltkSentiment.pickle","attribute":"text","operatorID":"operator-71536cb7-1925-422b-abd8-8de984e6bc12","operatorType":"ArrowNltkSentiment"},{"host":"54.215.247.135","port":3306,"database":"tweets_readonly","table":"tobacco_superset","username":"team_member","password":"Summer2020@UCI","column":"text","boolean expression":"(+\"hemp wrap\")","operatorID":"operator-8d351d58-8926-4a0e-bf54-46462b2c8443","operatorType":"MysqlSource"}],"links":[{"origin":"operator-71536cb7-1925-422b-abd8-8de984e6bc12","destination":"operator-bb71eaad-615f-4298-843b-bbcca9d5b694"},{"origin":"operator-8d351d58-8926-4a0e-bf54-46462b2c8443","destination":"operator-71536cb7-1925-422b-abd8-8de984e6bc12"}]}'
-);
--- core/web/src/main/java/edu/uci/ics/texera/web/resource/UserWorkflowResource.java
+++ core/web/src/main/java/edu/uci/ics/texera/web/resource/UserWorkflowResource.java
@@ -1,19 +1,27 @@
 package edu.uci.ics.texera.web.resource;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import edu.uci.ics.texera.dataflow.sqlServerInfo.UserSqlServer;
 import edu.uci.ics.texera.web.TexeraWebException;
+import edu.uci.ics.texera.web.response.GenericWebResponse;
 import io.dropwizard.jersey.sessions.Session;
-import org.jooq.Record1;
+import org.glassfish.jersey.media.multipart.FormDataParam;
 import org.jooq.Record3;
 
 import javax.servlet.http.HttpSession;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 
+import java.io.IOException;
+
 import static edu.uci.ics.texera.dataflow.jooq.generated.Tables.USERWORKFLOW;
+
+/**
+ * This file handles various request related to saved-workflows.
+ * It sends mysql queries to the MysqlDB regarding the UserWorkflow Table
+ * The details of UserWorkflowTable can be found in /core/scripts/sql/texera_ddl.sql
+ */
 
 // uncomment and use below to give workflows the concept of ownership
 // @Path("/user/workflow")
@@ -22,37 +30,59 @@ import static edu.uci.ics.texera.dataflow.jooq.generated.Tables.USERWORKFLOW;
 public class UserWorkflowResource {
 
     /**
-     * Corresponds to `src/app/common/type/user-file.ts`
+     * This method handles the frontend's request to get a specific workflow to be displayed
+     * at current design, it only takes the workflowID and searches within the database for the matching workflow
+     * for future design, it should also take userID as an parameter.
+     * @param workflowID
+     * @param session
+     * @return
      */
-    public static class UserWorkflow {
-        public String id; // the ID in MySQL database is unsigned int
-        public String name;
-        public String body;
-
-        public UserWorkflow(String id, String name, String body) {
-            this.id = id;
-            this.name = name;
-            this.body = body;
-        }
-    }
-
     @GET
     @Path("/get/{workflowID}")
-    public UserWorkflow getUserWorkflow(@PathParam("workflowID") String workflowID, @Session HttpSession session) {
-        System.out.println("with in getUserWorkflow for " + workflowID);
-//        UInteger userID = UserResource.getUser(session).getUserID();
+    public ObjectNode getUserWorkflow(@PathParam("workflowID") String workflowID, @Session HttpSession session) {
+        // uncomment below to link user with workflow
+        // UInteger userID = UserResource.getUser(session).getUserID();
         Record3<String, String, String> result = getWorkflowFromDatabase(workflowID);
 
         if (result == null) {
             throw new TexeraWebException("Workflow with id: " + workflowID + " does not exit.");
         }
 
-        return new UserWorkflow(
-                result.get(USERWORKFLOW.WORKFLOWID),
-                result.get(USERWORKFLOW.NAME),
-                result.get(USERWORKFLOW.WORKFLOWBODY)
-        );
+        try {
+            // the json string stored in USERWORKFLOW.WORKFLOWBODY correspond to the interface savedWorkflow
+            // in new-gui/src/app/workspace/service/save-workflow/save-workflow.service.ts
+            ObjectNode savedWorkflow = new ObjectMapper().readValue(result.get(USERWORKFLOW.WORKFLOWBODY), ObjectNode.class);
+            return savedWorkflow;
+        } catch (IOException e) {
+            throw new TexeraWebException(e.getMessage());
+        }
     }
+
+    /**
+     * this method handles the frontend's request to save a specific workflow
+     * at current design, it only takes the workflowID
+     * for future design, it should also take userID as an parameter.
+     * @param session
+     * @param workflowID
+     * @param workflowBody
+     * @return
+     */
+    @POST
+    @Path("/set-workflow")
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    public GenericWebResponse setUserWorkflow(
+            @Session HttpSession session,
+            @FormDataParam("workflowID") String workflowID,
+            @FormDataParam("workflowBody") String workflowBody
+    ) {
+
+        int count = updateWorkflowInDataBase(workflowID,workflowBody);
+        throwErrorWhenNotOne("Error occurred while inserting workflow to database",count);
+
+        return GenericWebResponse.generateSuccessResponse();
+    }
+
+
 
     private Record3<String, String, String> getWorkflowFromDatabase(String workflowID) {
         return UserSqlServer.createDSLContext()
@@ -62,13 +92,33 @@ public class UserWorkflowResource {
                 .fetchOne();
     }
 
+    private int updateWorkflowInDataBase(String workflowID, String workflowBody) {
+        return UserSqlServer.createDSLContext().update(USERWORKFLOW)
+                .set(USERWORKFLOW.WORKFLOWBODY, workflowBody)
+                .where(USERWORKFLOW.WORKFLOWID.eq(workflowID))
+                .execute();
+    }
+
     private int insertWorkflowToDataBase(String userID, String workflowID, String workflowName, String workflowBody) {
         return UserSqlServer.createDSLContext().insertInto(USERWORKFLOW)
-                 // uncomment below to give workflows the concept of ownership
+                // uncomment below to give workflows the concept of ownership
 //                .set(USERWORKFLOW.USERID,userID)
                 .set(USERWORKFLOW.WORKFLOWID, workflowID)
                 .set(USERWORKFLOW.NAME, workflowName)
                 .set(USERWORKFLOW.WORKFLOWBODY, workflowBody)
                 .execute();
+    }
+
+    /**
+     * Most the sql operation should only be executed once. eg. insertion, deletion.
+     * this method will raise TexeraWebException when the input number is not one
+     * @param errorMessage
+     * @param count
+     * @throws TexeraWebException
+     */
+    private void throwErrorWhenNotOne(String errorMessage, int count) throws TexeraWebException {
+        if (count != 1) {
+            throw new TexeraWebException(errorMessage);
+        }
     }
 }
