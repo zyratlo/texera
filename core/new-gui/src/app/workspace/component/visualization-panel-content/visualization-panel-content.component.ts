b5bdd925799c32ba3f2fc62698d0189d1f48cbb6
JamieLXZWoolf <213170728@seu.edu.cn>
2020-09-16 18:37:05 +0800

    Fix another viz bug

--- core/new-gui/src/app/workspace/component/visualization-panel-content/visualization-panel-content.component.ts
+++ core/new-gui/src/app/workspace/component/visualization-panel-content/visualization-panel-content.component.ts
@@ -41,11 +41,14 @@ export class VisualizationPanelContentComponent implements OnInit, AfterViewInit
       // correspond to WordCloudSink.java
       case ChartType.WORD_CLOUD: this.onClickGenerateWordCloud(); break;
       // correspond to TexeraBarChart.java
-      case ChartType.BAR || ChartType.STACKED_BAR: this.onClickGenerateChart(); break;
+      case ChartType.BAR:
+      case ChartType.STACKED_BAR:
       // correspond to PieChartSink.java
-      case ChartType.PIE || ChartType.DOUNT: this.onClickGenerateChart(); break;
+      case ChartType.PIE:
+      case ChartType.DOUNT:
       // correspond to TexeraLineChart.java
-      case ChartType.LINE || ChartType.SPLINE: this.onClickGenerateChart(); break;
+      case ChartType.LINE:
+      case ChartType.SPLINE: this.onClickGenerateChart(); break;
     }
   }
 
