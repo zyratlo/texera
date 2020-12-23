8b3a1e0e23371500783bae44d52c4e98de42133d
Yicong Huang <hyc541978023@gmail.com>
2020-12-23 01:12:01 -0800

    Fix websocket base URL (#943)
    This PR fixes the issue where the WebSocket is mistakenly connecting to the current relative URL, instead of connecting to the base URL defined by config.

--- core/new-gui/src/app/workspace/service/workflow-websocket/workflow-websocket.service.ts
+++ core/new-gui/src/app/workspace/service/workflow-websocket/workflow-websocket.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
-import {
-  TexeraWebsocketRequest, TexeraWebsocketEvent, TexeraWebsocketRequestTypeMap, TexeraWebsocketRequestTypes
-} from '../../types/workflow-websocket.interface';
-import { webSocket, WebSocketSubject } from 'rxjs/webSocket';
 import { Observable } from 'rxjs';
+import { webSocket, WebSocketSubject } from 'rxjs/webSocket';
+import {
+  TexeraWebsocketEvent, TexeraWebsocketRequest, TexeraWebsocketRequestTypeMap, TexeraWebsocketRequestTypes
+} from '../../types/workflow-websocket.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -12,18 +12,18 @@ export class WorkflowWebsocketService {
 
   private static readonly TEXERA_WEBSOCKET_ENDPOINT = 'wsapi/workflow-websocket';
 
-  private readonly websocket: WebSocketSubject<TexeraWebsocketEvent | TexeraWebsocketRequest>;
+  private readonly websocket: WebSocketSubject<TexeraWebsocketEvent|TexeraWebsocketRequest>;
   private readonly webSocketObservable: Observable<TexeraWebsocketEvent>;
 
   constructor() {
-    this.websocket = webSocket<TexeraWebsocketEvent | TexeraWebsocketRequest>(WorkflowWebsocketService.getWorkflowWebsocketUrl());
+    this.websocket = webSocket<TexeraWebsocketEvent|TexeraWebsocketRequest>(WorkflowWebsocketService.getWorkflowWebsocketUrl());
     this.webSocketObservable = this.websocket.share() as Observable<TexeraWebsocketEvent>;
     this.webSocketObservable.subscribe(data => {
       if (data.type === 'HelloWorldResponse') {
         console.log('hello world works: ' + data.message);
       }
     });
-    this.send('HelloWorldRequest', { message: 'Texera on Amber' });
+    this.send('HelloWorldRequest', {message: 'Texera on Amber'});
   }
 
   public websocketEvent(): Observable<TexeraWebsocketEvent> {
@@ -39,7 +39,7 @@ export class WorkflowWebsocketService {
   }
 
   public static getWorkflowWebsocketUrl(): string {
-    const websocketUrl = new URL(WorkflowWebsocketService.TEXERA_WEBSOCKET_ENDPOINT, window.location.origin);
+    const websocketUrl = new URL(WorkflowWebsocketService.TEXERA_WEBSOCKET_ENDPOINT, document.baseURI);
     // replace protocol, so that http -> ws, https -> wss
     websocketUrl.protocol = websocketUrl.protocol.replace('http', 'ws');
     return websocketUrl.toString();
