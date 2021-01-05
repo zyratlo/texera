7d384d400b44682b390b0fed23409b84060b527f
Shengquan Ni <13672781+shengquan-ni@users.noreply.github.com>
2021-01-05 13:00:01 -0800

    Set default batch size to 400 (#953)
    This PR sets `defaultBatchSize` to 400 instead of 1 so that we won't wrap every tuple as a message in the actor system.

--- core/amber/src/main/scala/edu/uci/ics/amber/engine/common/Constants.scala
+++ core/amber/src/main/scala/edu/uci/ics/amber/engine/common/Constants.scala
@@ -3,7 +3,7 @@ package edu.uci.ics.amber.engine.common
 import scala.concurrent.duration._
 
 object Constants {
-  val defaultBatchSize = 1
+  val defaultBatchSize = 400
   val remoteHDFSPath = "hdfs://10.138.0.2:8020"
   val remoteHDFSIP = "10.138.0.2"
   var defaultNumWorkers = 0
