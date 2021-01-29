c6fe6fecf8473064f9641f8b8b464cdfb1d0972b
Yicong Huang <hyc541978023@gmail.com>
2021-01-29 14:05:41 -0800

    Update tweets.sql DDL to align with twitter's Tweet object (#1001)
    We take data from Twitter API, which returns the Tweet object. However, we have some renaming and type changes in the upstream data collection. We will align the source table DDL to the correct name and types from Tweet object.

--- core/scripts/sql/tweets.sql
+++ core/scripts/sql/tweets.sql
@@ -1,31 +1,30 @@
 CREATE TABLE TABLE_NAME
 (
-    id                   BIGINT                       NOT NULL PRIMARY KEY,
-    create_at            DATETIME                     NULL,
-    text                 VARCHAR(500) charset utf8mb4 NULL,
-    in_reply_to_status   VARCHAR(25)                  NULL,
-    in_reply_to_user     VARCHAR(25)                  NULL,
-    favourites_count     INT                          NULL,
-    retweet_count        INT                          NULL,
-    lang                 VARCHAR(10)                  NULL,
-    is_retweet           TINYINT(1)                   NULL,
-    hashtags             VARCHAR(500) charset utf8mb4 NULL,
-    user_mentions        VARCHAR(500) charset utf8mb4 NULL,
-    user_id              VARCHAR(25)                  NULL,
-    user_name            VARCHAR(500) charset utf8mb4 NULL,
-    user_screen_name     VARCHAR(500) charset utf8mb4 NULL,
-    user_location        VARCHAR(500)                 NULL,
-    user_description     VARCHAR(500) charset utf8mb4 NULL,
-    user_followers_count INT                          NULL,
-    user_friends_count   INT                          NULL,
-    user_statues_count   INT                          NULL,
-    stateName            VARCHAR(100)                 NULL,
-    countyName           VARCHAR(100)                 NULL,
-    cityName             VARCHAR(100)                 NULL,
-    country              VARCHAR(100)                 NULL,
-    bounding_box         VARCHAR(100)                 NULL
+    id                    BIGINT                       NOT NULL PRIMARY KEY,
+    created_at            DATETIME                     NULL,
+    text                  VARCHAR(500) CHARSET utf8mb4 NULL,
+    in_reply_to_status_id BIGINT                       NULL,
+    in_reply_to_user_id   BIGINT                       NULL,
+    favourites_count      INT                          NULL,
+    retweet_count         INT                          NULL,
+    lang                  VARCHAR(10)                  NULL,
+    retweeted             BOOLEAN                      NULL,
+    hashtags              VARCHAR(500) CHARSET utf8mb4 NULL,
+    user_mentions         VARCHAR(500) CHARSET utf8mb4 NULL,
+    user_id               BIGINT                       NULL,
+    user_name             VARCHAR(500) CHARSET utf8mb4 NULL,
+    user_screen_name      VARCHAR(500) CHARSET utf8mb4 NULL,
+    user_location         VARCHAR(500)                 NULL,
+    user_description      VARCHAR(500) CHARSET utf8mb4 NULL,
+    user_followers_count  INT                          NULL,
+    user_friends_count    INT                          NULL,
+    user_statues_count    INT                          NULL,
+    stateName             VARCHAR(100)                 NULL,
+    countyName            VARCHAR(100)                 NULL,
+    cityName              VARCHAR(100)                 NULL,
+    country               VARCHAR(100)                 NULL,
+    bounding_box          VARCHAR(500)                 NULL
 );
 
-CREATE FULLTEXT INDEX text_index ON TABLE_NAME (text);
-CREATE INDEX create_at_index ON TABLE_NAME (create_at);
-
+CREATE FULLTEXT INDEX text_index on TABLE_NAME (text);
+CREATE INDEX created_at_index ON TABLE_NAME (created_at);
