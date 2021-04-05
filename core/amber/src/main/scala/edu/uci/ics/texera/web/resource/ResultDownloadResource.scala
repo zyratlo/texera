058ac3120731583e26b1fa66382c8214dbb9b7cc
Chen He (Adam) <ChenHeSDE@outlook.com>
2021-04-05 12:36:10 +0800

    Google sheet type fix (#1068)
    This PR makes two changes:
    1. Fix the bug that some types fail to upload to google sheet. Google sheet API only allows specific type to upload (e.g. String and number). This PR solves the problems by converting all the types other than String and number into String before uploading.
    2. Move the workflow results to a specific folder when uploading to make the drive look tide.
    
    Co-authored-by: Yicong Huang <17627829+Yicong-Huang@users.noreply.github.com>

--- core/amber/src/main/scala/edu/uci/ics/texera/web/resource/ResultDownloadResource.scala
+++ core/amber/src/main/scala/edu/uci/ics/texera/web/resource/ResultDownloadResource.scala
@@ -1,8 +1,9 @@
 package edu.uci.ics.texera.web.resource
 
+import com.google.api.client.googleapis.json.GoogleJsonResponseException
 import com.google.api.client.util.Lists
 import com.google.api.services.drive.Drive
-import com.google.api.services.drive.model.Permission
+import com.google.api.services.drive.model.{File, FileList, Permission}
 import com.google.api.services.sheets.v4.Sheets
 import com.google.api.services.sheets.v4.model.{
   AppendValuesResponse,
@@ -20,11 +21,18 @@ import edu.uci.ics.texera.web.resource.WorkflowWebsocketResource.{
 import edu.uci.ics.texera.workflow.common.tuple.Tuple
 
 import java.util
+import scala.annotation.tailrec
+import scala.collection.JavaConverters._
 import scala.collection.mutable
+import scala.util.{Failure, Success, Try}
 
 object ResultDownloadResource {
 
-  private final val UPLOAD_SIZE = 100;
+  private final val UPLOAD_BATCH_SIZE = 100
+
+  private final val WORKFLOW_RESULT_FOLDER_NAME = "workflow_results"
+
+  @volatile private var WORKFLOW_RESULT_FOLDER_ID: String = _
 
   def apply(
       sessionId: String,
@@ -44,16 +52,16 @@ object ResultDownloadResource {
     }
 
     // By now the workflow should finish running. Only one operator should contain results
-    // TODO currently assume only one operator should contains the result
-    // TODO change status checking of the workflow
-    val OperatorWithResult = sessionResults(sessionId).count(p => p._2.nonEmpty)
-    if (OperatorWithResult == 0) {
+    // TODO: currently assume only one operator should contains the result
+    // TODO: change status checking of the workflow
+    val operatorWithResult = sessionResults(sessionId).count(p => p._2.nonEmpty)
+    if (operatorWithResult == 0) {
       return ResultDownloadResponse(
         request.downloadType,
         "",
         "The workflow contains no results"
       )
-    } else if (OperatorWithResult > 1) {
+    } else if (operatorWithResult > 1) {
       // more than one operator contains results means the workflow does not finish running.
       return ResultDownloadResponse(
         request.downloadType,
@@ -63,12 +71,12 @@ object ResultDownloadResource {
     }
 
     // convert the ITuple into tuple
-    // TODO currently only accept the tuple as input
+    // TODO: currently only accept the tuple as input
     val results: List[Tuple] =
       sessionResults(sessionId).values
         .find(p => p.nonEmpty)
         .get
-        .map(ituple => ituple.asInstanceOf[Tuple])
+        .map(iTuple => iTuple.asInstanceOf[Tuple])
     val schema = getSchema(results.head)
 
     // handle the request according to download type
@@ -120,6 +128,9 @@ object ResultDownloadResource {
         "Fail to create google sheet"
       )
 
+    val driveService: Drive = GoogleResource.getDriveService
+    moveToResultFolder(driveService, sheetId)
+
     // upload the schema
     val schemaContent: util.List[util.List[AnyRef]] = Lists.newArrayList()
     schemaContent.add(schema)
@@ -127,11 +138,10 @@ object ResultDownloadResource {
       uploadContent(sheetService, sheetId, schemaContent)
 
     // allow user to access this sheet in the service account
-    val drive: Drive = GoogleResource.getDriveService
     val sharePermission: Permission = new Permission()
       .setType("anyone")
       .setRole("reader")
-    drive
+    driveService
       .permissions()
       .create(sheetId, sharePermission)
       .execute()
@@ -161,19 +171,82 @@ object ResultDownloadResource {
   }
 
   /**
+    * move the workflow results to a specific folder
+    */
+  @tailrec
+  private def moveToResultFolder(
+      driveService: Drive,
+      sheetId: String,
+      retry: Boolean = true
+  ): Unit = {
+    Try(
+      driveService
+        .files()
+        .update(sheetId, null)
+        .setAddParents(WORKFLOW_RESULT_FOLDER_ID)
+        .execute()
+    ) match {
+      case Success(_) => // do nothing upon success
+      case Failure(exception: GoogleJsonResponseException) =>
+        if (retry) {
+          // This exception maybe caused by the full deletion of the target folder and
+          // the cached folder id is obsolete.
+          //  * note: by full deletion, the folder has to be deleted from trash as well.
+          // In this case, retrieve the folder id to try again.
+          retrieveResultFolderId(driveService)
+          moveToResultFolder(driveService, sheetId, retry = false)
+        } else {
+          // if the exception continues to show up then just throw it normally.
+          throw exception
+        }
+    }
+  }
+
+  private def retrieveResultFolderId(driveService: Drive): String =
+    synchronized {
+
+      val folderResult: FileList = driveService
+        .files()
+        .list()
+        .setQ(
+          s"mimeType = 'application/vnd.google-apps.folder' and name='${WORKFLOW_RESULT_FOLDER_NAME}'"
+        )
+        .setSpaces("drive")
+        .execute()
+
+      if (folderResult.getFiles.isEmpty) {
+        val fileMetadata: File = new File()
+        fileMetadata.setName(WORKFLOW_RESULT_FOLDER_NAME)
+        fileMetadata.setMimeType("application/vnd.google-apps.folder")
+        val targetFolder: File = driveService.files.create(fileMetadata).setFields("id").execute
+        WORKFLOW_RESULT_FOLDER_ID = targetFolder.getId
+      } else {
+        WORKFLOW_RESULT_FOLDER_ID = folderResult.getFiles.get(0).getId
+      }
+      WORKFLOW_RESULT_FOLDER_ID
+    }
+
+  /**
     * upload the result body to the google sheet
     */
   private def uploadResult(sheetService: Sheets, sheetId: String, result: List[ITuple]): Unit = {
     val content: util.List[util.List[AnyRef]] =
-      Lists.newArrayListWithCapacity(UPLOAD_SIZE)
+      Lists.newArrayListWithCapacity(UPLOAD_BATCH_SIZE)
     // use for loop to avoid copying the whole result at the same time
     for (tuple: ITuple <- result) {
       val tupleContent: util.List[AnyRef] =
-        tuple.asInstanceOf[Tuple].getFields
+        tuple
+          .asInstanceOf[Tuple]
+          .getFields
+          .stream()
+          .map(convertUnsupported)
+          .toArray
+          .toList
+          .asJava
       content.add(tupleContent)
 
-      if (content.size() == UPLOAD_SIZE) {
-        // TODO the response is from uploading is not checked.
+      if (content.size() == UPLOAD_BATCH_SIZE) {
+        // TODO: the response is from uploading is not checked.
         //  The design for the response seems not to be designed for error handling
         // it will throw error and stop if encounter error during uploading
         val response: AppendValuesResponse =
@@ -186,6 +259,24 @@ object ResultDownloadResource {
       val response: AppendValuesResponse =
         uploadContent(sheetService, sheetId, content)
     }
+  }
+
+  /**
+    * convert the tuple content into the type the Google Sheet API supports
+    */
+  private def convertUnsupported(content: AnyRef): AnyRef = {
+    content match {
+
+      // if null, use empty string to represent.
+      case null => ""
+
+      // Google Sheet API supports String and number(long, int, double and so on)
+      case _: String | _: Number => content
+
+      // convert all the other type into String
+      case _ => content.toString
+    }
+
   }
 
   /**
