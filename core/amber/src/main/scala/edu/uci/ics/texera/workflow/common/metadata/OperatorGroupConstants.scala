d5ac212f3c6a5bc0a872f0de0ece8ddd9c98ac73
sadeem <sadeem.alsudais@gmail.com>
2021-05-07 15:13:06 -0700

    Remove unused groups (#1114)
    Remove unused categories `Split` and `Database` 
    ![ezgif com-gif-maker](https://user-images.githubusercontent.com/9158114/117505892-a3246480-af39-11eb-8fa5-6425a47768a7.gif)

--- core/amber/src/main/scala/edu/uci/ics/texera/workflow/common/metadata/OperatorGroupConstants.scala
+++ core/amber/src/main/scala/edu/uci/ics/texera/workflow/common/metadata/OperatorGroupConstants.scala
@@ -4,10 +4,8 @@ object OperatorGroupConstants {
   final val SOURCE_GROUP = "Source"
   final val SEARCH_GROUP = "Search"
   final val ANALYTICS_GROUP = "Analytics"
-  final val SPLIT_GROUP = "Split"
   final val JOIN_GROUP = "Join"
   final val UTILITY_GROUP = "Utilities"
-  final val DATABASE_GROUP = "Database"
   final val UDF_GROUP = "User-defined Functions"
   final val VISUALIZATION_GROUP = "Visualization"
   final val RESULT_GROUP = "View Results"
@@ -20,13 +18,11 @@ object OperatorGroupConstants {
     GroupInfo(SOURCE_GROUP, 0),
     GroupInfo(SEARCH_GROUP, 1),
     GroupInfo(ANALYTICS_GROUP, 2),
-    GroupInfo(SPLIT_GROUP, 3),
-    GroupInfo(JOIN_GROUP, 4),
-    GroupInfo(UTILITY_GROUP, 5),
-    GroupInfo(DATABASE_GROUP, 6),
-    GroupInfo(UDF_GROUP, 7),
-    GroupInfo(VISUALIZATION_GROUP, 8),
-    GroupInfo(RESULT_GROUP, 9)
+    GroupInfo(JOIN_GROUP, 3),
+    GroupInfo(UTILITY_GROUP, 4),
+    GroupInfo(UDF_GROUP, 5),
+    GroupInfo(VISUALIZATION_GROUP, 6),
+    GroupInfo(RESULT_GROUP, 7)
   )
 
 }
