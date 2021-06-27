799858cb118901866d53db08ec3132000bda27e0
Albert Liu <ayliu5@uci.edu>
2021-06-27 09:44:50 -0700

    Add UserDictionaryService (User Presets Step 2) (#1162)
    This PR is extracted from #1041 as step 2 of the User Preset feature.
    
    rebase of picked commits pertaining to UserDictionaryService onto Albert-UserDictionaryResource
    
    UserDictionaryService provides a "dictionary", a persistent collection of string (key, value) pairs for clients that are logged in
    Changes from last review (for Yicong)
    - Much more code comments
    - using new API from User Presets Step 1
    - Get/Set/Delete methods return Observables instead of Promises now
    - Code should be much more readable because error handling and validation are separated into individual function definitions
    
    Co-authored-by: Yicong Huang <17627829+Yicong-Huang@users.noreply.github.com>
    Co-authored-by: Zuozhi Wang <zuozhiw@gmail.com>

--- core/amber/src/main/scala/edu/uci/ics/texera/web/resource/UserDictionaryResource.scala
+++ core/amber/src/main/scala/edu/uci/ics/texera/web/resource/UserDictionaryResource.scala
@@ -9,7 +9,7 @@ import io.dropwizard.jersey.sessions.Session
 
 import javax.servlet.http.HttpSession
 import javax.ws.rs._
-import javax.ws.rs.core.{MediaType, Response}
+import javax.ws.rs.core._
 import scala.jdk.CollectionConverters.asScalaBuffer
 
 /**
@@ -26,68 +26,54 @@ class UserDictionaryResource {
     SqlServer.createDSLContext.configuration
   )
 
-  /**
-    * This method either retrieves all of the current in-session user's dictionary
-    * as a json object or retrieves the value corresponding to the "key" attribute of the GetRequest.
-    * @param session HttpSession
-    * @param req GetRequest (key may be null for GetDictRequest)
-    * @return Response with a UserDictionaryResponse payload, 200 ok - payload: EntryResponse or DictResponse,
-    * 401 unauthorized - payload: ErrorResponse("Invalid session"),
-    * 400 bad request - payload: ErrorResponse("Malformed JSON payload"),
-    * 422 Unprocessable Entity - payload: ErrorResponse("No such entry") (if no entry exists for provided key)
-    */
-  @POST
-  @Path("/get")
-  def getValue(@Session session: HttpSession, req: UserDictionaryGetRequest): Response = {
-    val user = UserResource.getUser(session).orNull
-    if (user == null)
-      Response.status(Response.Status.UNAUTHORIZED).entity(ErrorResponse("Invalid session")).build()
-    else if (
-      req == null || req.requestType == UserDictionaryGetRequest.NULL_VALUE || (req.key == null && req.requestType == UserDictionaryGetRequest.GET_ENTRY)
-    ) {
-      // req.requestType is 0 when unspecified. It should always be specified in a proper GetRequest
-      Response
-        .status(Response.Status.BAD_REQUEST)
-        .entity(ErrorResponse("Malformed JSON payload"))
-        .build()
-    } else if (req.requestType == UserDictionaryGetRequest.GET_ENTRY) {
-      if (!dictEntryExists(user, req.key))
-        Response.status(422).entity(ErrorResponse("No such entry")).build()
-      else Response.ok(EntryResponse(getValueByKey(user, req.key))).build()
+  @GET
+  def getAllDict(@Session session: HttpSession): Map[String, String] = {
+    val user = UserResource.getUser(session)
+    if (user.isEmpty) {
+      throw new NotAuthorizedException("Request unauthorized: user not logged in")
+    }
+    getDict(user.get)
+  }
+
+  @GET
+  @Path("/{key}")
+  def getEntry(@Session session: HttpSession, @PathParam("key") key: String): String = {
+    val user = UserResource.getUser(session)
+    if (user.isEmpty) {
+      throw new NotAuthorizedException("Request unauthorized: user not logged in")
+    }
+    if (key == null || key.trim.isEmpty) {
+      throw new BadRequestException("key cannot be null or empty")
+    }
+    if (!dictEntryExists(user.get, key)) {
+      null
     } else {
-      Response.ok(DictResponse(getDict(user))).build()
+      getValueByKey(user.get, key)
     }
   }
 
   /**
     * This method creates or updates an entry in the current in-session user's dictionary based on
     * the "key" and "value" attributes of the PostRequest
-    * @param session HttpSession
-    * @param req PostRequest
-    * @return Response, 200 ok - payload: ConfirmationResponse("Created" or "Updated"),
-    * 401 unauthorized - payload: ErrorResponse("Invalid session"),
-    * 400 bad request - payload: ErrorResponse("Malformed JSON payload"),
     */
-  @POST
-  @Path("/set")
-  def setValue(
+  @PUT
+  @Path("/{key}")
+  def setEntry(
       @Session session: HttpSession,
-      req: UserDictionaryPostRequest
-  ): Response = {
-    val user = UserResource.getUser(session).orNull
-    if (user == null)
-      Response.status(Response.Status.UNAUTHORIZED).entity(ErrorResponse("Invalid session")).build()
-    else if (req == null || req.key == null || req.value == null) {
-      Response
-        .status(Response.Status.BAD_REQUEST)
-        .entity(ErrorResponse("Malformed JSON payload"))
-        .build()
-    } else if (dictEntryExists(user, req.key)) {
-      userDictionaryDao.update(new UserDictionary(user.getUid, req.key, req.value))
-      Response.ok(ConfirmationResponse("Updated")).build()
+      @PathParam("key") key: String,
+      @FormParam("value") value: String
+  ): Unit = {
+    val user = UserResource.getUser(session)
+    if (user.isEmpty) {
+      throw new NotAuthorizedException("Request unauthorized: user not logged in")
+    }
+    if (key == null || key.trim.isEmpty) {
+      throw new BadRequestException("key cannot be null or empty")
+    }
+    if (dictEntryExists(user.get, key)) {
+      userDictionaryDao.update(new UserDictionary(user.get.getUid, key, value))
     } else {
-      userDictionaryDao.insert(new UserDictionary(user.getUid, req.key, req.value))
-      Response.ok(ConfirmationResponse("Created")).build()
+      userDictionaryDao.insert(new UserDictionary(user.get.getUid, key, value))
     }
   }
 
@@ -96,27 +82,23 @@ class UserDictionaryResource {
     * the "key" attribute of the DeleteRequest
     * @param session HttpSession
     * @param req DeleteRequest
-    * @return Response, 200 ok - payload: "Deleted",
-    * 401 unauthorized - payload: "Invalid session",
-    * 400 bad request - payload: ErrorResponse("Malformed JSON payload"),
+    * @return
+    * 401 unauthorized -
+    * 400 bad request -
     * 422 Unprocessable Entity - payload: "no such entry" (if no entry exists for provided key)
     */
   @DELETE
-  @Path("/delete")
-  def deleteValue(@Session session: HttpSession, req: UserDictionaryDeleteRequest): Response = {
-    val user = UserResource.getUser(session).orNull
-    if (user == null)
-      Response.status(Response.Status.UNAUTHORIZED).entity(ErrorResponse("Invalid session")).build()
-    else if (req == null || req.key == null) {
-      Response
-        .status(Response.Status.BAD_REQUEST)
-        .entity(ErrorResponse("Malformed JSON payload"))
-        .build()
-    } else if (!dictEntryExists(user, req.key)) {
-      Response.status(422).entity(ErrorResponse("No such entry")).build()
-    } else {
-      deleteDictEntry(user, req.key)
-      Response.ok(ConfirmationResponse("Deleted")).build()
+  @Path("/{key}")
+  def deleteEntry(@Session session: HttpSession, @PathParam("key") key: String): Unit = {
+    val user = UserResource.getUser(session)
+    if (user.isEmpty) {
+      throw new NotAuthorizedException("Request unauthorized: user not logged in")
+    }
+    if (key == null || key.trim.isEmpty) {
+      throw new BadRequestException("key cannot be null or empty")
+    }
+    if (dictEntryExists(user.get, key)) {
+      deleteDictEntry(user.get, key)
     }
   }
 
@@ -177,66 +159,3 @@ class UserDictionaryResource {
     )
   }
 }
-
-/**
-  * This class represents the json structure of all incoming requests to UserDictionaryResource
-  */
-abstract class UserDictionaryRequest {
-  def key: String
-}
-
-case class UserDictionaryGetRequest(
-    key: String, // optional/nullable for requestType = GetRequest.GET_DICT
-    requestType: UserDictionaryGetRequest.UserDictionaryGetRequest // 1 - GET_ENTRY, 2 - GET_DICT, 0 - an error, (indicates requestType wasn't specified in JSON),
-) extends UserDictionaryRequest
-
-object UserDictionaryGetRequest extends Enumeration {
-  type UserDictionaryGetRequest = Value
-  val NULL_VALUE = Value(0) // default value for ints, always interpreted as none given
-  val GET_ENTRY = Value(1)
-  val GET_DICT = Value(2)
-}
-
-case class UserDictionaryPostRequest(
-    key: String,
-    value: String
-) extends UserDictionaryRequest
-
-case class UserDictionaryDeleteRequest(
-    key: String
-) extends UserDictionaryRequest
-
-/**
-  * This class represents the json structure of all outgoing responses from UserDictionaryResource
-  */
-abstract class UserDictionaryResponse {
-  def code: UserDictionaryResponse.UserDictionaryResponse
-}
-
-object UserDictionaryResponse extends Enumeration {
-  type UserDictionaryResponse = Value
-  val CODE_ERROR = Value(-1)
-  val CODE_ENTRY = Value(0)
-  val CODE_DICT = Value(1)
-  val CODE_CONFIRM = Value(2)
-}
-
-case class ErrorResponse(
-    message: String,
-    code: UserDictionaryResponse.UserDictionaryResponse = UserDictionaryResponse.CODE_ERROR
-) extends UserDictionaryResponse
-
-case class EntryResponse(
-    result: String,
-    code: UserDictionaryResponse.UserDictionaryResponse = UserDictionaryResponse.CODE_ENTRY
-) extends UserDictionaryResponse
-
-case class DictResponse(
-    result: Map[String, String],
-    code: UserDictionaryResponse.UserDictionaryResponse = UserDictionaryResponse.CODE_DICT
-) extends UserDictionaryResponse
-
-case class ConfirmationResponse(
-    result: String,
-    code: UserDictionaryResponse.UserDictionaryResponse = UserDictionaryResponse.CODE_CONFIRM
-) extends UserDictionaryResponse
---
+++ core/new-gui/src/app/common/service/user/user-dictionary/dictionary.service.ts
@@ -0,0 +1,146 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+import { AppSettings } from 'src/app/common/app-setting';
+import { UserService } from '../user.service';
+
+
+export type UserDictionary = {
+  [key: string]: string
+};
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DictionaryService {
+
+  public static readonly USER_DICTIONARY_ENDPOINT = 'users/dictionary';
+
+  private dictionaryChangedSubject = new Subject<void>();
+  private localUserDictionary: UserDictionary = {};
+
+  constructor(private http: HttpClient, private userService: UserService) {
+    if (this.userService.isLogin()) {
+      this.fetchAll();
+    }
+    this.userService.userChanged().subscribe(
+      () => {
+        if (this.userService.isLogin()) {
+          this.fetchAll();
+        } else {
+          this.updateDict({});
+        }
+      }
+    );
+  }
+
+  public getDict(): Readonly<UserDictionary> {
+    return this.localUserDictionary;
+  }
+
+  /**
+   * get a value from the backend.
+   * keys and values must be strings.
+   * @param key string key that uniquely identifies a value
+   * @returns string value corresponding to the key from the backend;
+   * throws Error("No such entry") (invalid key) or Error("Invalid session") (not logged in).
+   */
+  public fetchKey(key: string): Observable<string> {
+    if (! this.userService.isLogin()) {
+      throw new Error('user not logged in');
+    }
+    if (key.trim().length === 0) {
+      throw new Error('Dictionary Service: key cannot be empty');
+    }
+    const url = `${AppSettings.getApiEndpoint()}/${DictionaryService.USER_DICTIONARY_ENDPOINT}/${key}`;
+    const req = this.http.get<string>(url).do(res => this.updateEntry(key, res)).shareReplay(1);
+    req.subscribe(); // causes post request to be sent regardless caller's subscription
+    return req;
+  }
+
+  /**
+   * get the entire dictionary from the backend.
+   * @returns UserDictionary object with string attributes;
+   */
+  public fetchAll(): Observable<Readonly<UserDictionary>> {
+    if (! this.userService.isLogin()) {
+      throw new Error('user not logged in');
+    }
+    const url = `${AppSettings.getApiEndpoint()}/${DictionaryService.USER_DICTIONARY_ENDPOINT}`;
+    const req = this.http.get<UserDictionary>(url)
+      .do(res => this.updateDict(res))
+      .shareReplay(1);
+    req.subscribe(); // causes post request to be sent regardless caller's subscription
+    return req;
+  }
+
+  /**
+   * saves or updates (if it already exists) an entry (key-value pair) on the backend.
+   * keys and values must be strings.
+   * @param key string key that uniquely identifies a value
+   * @param value string value corresponding to the key from the backend
+   * @returns observable indicating the backend has been successfully updated
+   */
+  public set(key: string, value: string): Observable<void> {
+    if (! this.userService.isLogin()) {
+      throw new Error('user not logged in');
+    }
+    if (key.trim().length === 0) {
+      throw new Error('Dictionary Service: key cannot be empty');
+    }
+    const url = `${AppSettings.getApiEndpoint()}/${DictionaryService.USER_DICTIONARY_ENDPOINT}/${key}`;
+    const req = this.http.put<void>(url, { value: value })
+      .do(_ => this.updateEntry(key, value))
+      .shareReplay(1);
+    req.subscribe();
+    return req;
+  }
+
+  /**
+   * delete a value from the backend.
+   * keys and values must be strings.
+   * @param key string key that uniquely identifies a value
+   * @returns observable indicating the backend has been successfully updated
+   */
+  public delete(key: string): Observable<void> {
+    if (! this.userService.isLogin()) {
+      throw new Error('user not logged in');
+    }
+    if (key.trim().length === 0) {
+      throw new Error('Dictionary Service: key cannot be empty');
+    }
+    if (! (key in this.localUserDictionary)) {
+      return Observable.of();
+    }
+    const url = `${AppSettings.getApiEndpoint()}/${DictionaryService.USER_DICTIONARY_ENDPOINT}/${key}`;
+    const req = this.http.delete<void>(url)
+      .do(_ => this.updateEntry(key, undefined))
+      .shareReplay(1);
+    req.subscribe();
+    return req;
+  }
+
+  private updateEntry(key: string, value: string | undefined) {
+    if (key.trim().length === 0) {
+      throw new Error('Dictionary Service: key cannot be empty');
+    }
+    if (value === undefined) {
+      if (key in this.localUserDictionary) {
+        delete this.localUserDictionary[key];
+        this.dictionaryChangedSubject.next();
+      }
+    } else {
+      if (this.localUserDictionary[key] !== value) {
+        this.localUserDictionary[key] = value;
+        this.dictionaryChangedSubject.next();
+      }
+    }
+  }
+
+  private updateDict(newDict: UserDictionary) {
+    this.localUserDictionary = newDict;
+    this.dictionaryChangedSubject.next();
+  }
+
+}
