a649d9d10673ee89df1e3c1e22e7ce3a705dcaaf
Yicong Huang <17627829+Yicong-Huang@users.noreply.github.com>
2021-08-25 17:37:38 -0700

    Script and gitignore update (#1313)
    This PR updates some logistic files:
    1. Update `deploy-daemon.sh` to:
       a. handle `-s true` option to optionally skip compilation phase.
       b. perform `sbt package` instead of `sbt complie`.
       c. redirect `stdout` and `stderr` to `/dev/null` to output nothing with `nohup`.
    2. Update `.gitignore` to ignore:
       a. python generated files, such as `*.pkl` and `*.model`.
       b. archived logs `*.log.gz`.
       c. deployment-related credential files.

--- core/scripts/depoly-daemon.sh
+++ core/scripts/depoly-daemon.sh
@@ -1,27 +1,40 @@
-red=`tput setaf 1`
-green=`tput setaf 2`
-reset=`tput sgr0`
+red=$(tput setaf 1)
+green=$(tput setaf 2)
+reset=$(tput sgr0)
 
-echo "${green}Compiling Amber...${reset}"
-cd amber && sbt clean compile
-echo "${green}Amber compiled.${reset}"
-echo
+skipCompilation=false
+while getopts s: flag
+do
+    case "${flag}" in
+        s) skipCompilation="$OPTARG";;
+        *) exit 1
+    esac
+done
 
-echo "${green}Compiling GUI...${reset}"
-cd ../new-gui && yarn install && ng build --prod  --deploy-url=/ --base-href=/
-echo "${green}GUI compiled.${reset}"
-echo
+if [ ! "$skipCompilation" ]
+then
+  echo "${green}Compiling Amber...${reset}"
+  cd amber && sbt clean package
+  echo "${green}Amber compiled.${reset}"
+  echo
 
-cd ..
+  echo "${green}Compiling GUI...${reset}"
+  cd ../new-gui && yarn install && ng build --prod  --deploy-url=/ --base-href=/
+  echo "${green}GUI compiled.${reset}"
+  echo
+  cd ..
+fi
+
 echo "${green}Starting TexeraWebApplication in daemon...${reset}"
-setsid nohup ./scripts/server.sh 2>&1 &
+setsid nohup ./scripts/server.sh >/dev/null 2>&1 &
 echo "${green}Waiting TexeraWebApplication to launch on 8080...${reset}"
 while ! nc -z localhost 8080; do   
-	sleep 0.1 # wait for 1/10 of the second before check again
+	sleep 0.1 # wait 100ms before check again
 done
 echo "${green}TexeraWebApplication launched at $(pgrep -f TexeraWebApplication)${reset}"
 echo
 
 echo "${green}Starting TexeraRunWorker in daemon...${reset}"
-setsid nohup ./scripts/worker.sh 2>&1 &
+setsid nohup ./scripts/worker.sh >/dev/null 2>&1 &
+sleep 0.2 # wait for 200ms to get the pid
 echo "${green}TexeraRunWorker launched at $(pgrep -f TexeraRunWorker)${reset}"
