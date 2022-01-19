9f57afcdd736eb40c517f60c27ea22d0749e6602
Jingqi Yao <56903646+jessieeeovo@users.noreply.github.com>
2022-01-19 10:35:58 -0800

    Refactor line chart for aggregation (#1404)
    Refactoring the line chart operator based on template on bar chart (link to PR: https://github.com/Texera/texera/pull/1384)
    
    ## Demo
    Data used for demo:
    <img width="511" alt="Screen Shot 2022-01-18 at 12 39 59 PM" src="https://user-images.githubusercontent.com/56903646/150015055-4e96d3f8-b0e6-4ff9-b86a-4b2fdd8029d8.png">
    
    **1. If given the below parameters (only name column and no data column)**:
    <img width="337" alt="Screen Shot 2022-01-18 at 12 35 04 PM" src="https://user-images.githubusercontent.com/56903646/150014403-1eab77cb-c420-4084-82eb-99e4e5e40dee.png">
    Result looks like this (the occurrences of each name label is returned):
    <img width="1094" alt="Screen Shot 2022-01-18 at 12 34 49 PM" src="https://user-images.githubusercontent.com/56903646/150014373-410ca575-05cc-47ea-a2d9-ad81a5956bce.png">
    
    **2. If a data column is added**:
    <img width="332" alt="Screen Shot 2022-01-18 at 12 38 39 PM" src="https://user-images.githubusercontent.com/56903646/150014878-5ae7a789-9973-4b2d-8726-c59aba920398.png">
    Result sums the given data column over each name label:
    <img width="1090" alt="Screen Shot 2022-01-18 at 12 38 57 PM" src="https://user-images.githubusercontent.com/56903646/150014908-ce3ae194-f1ba-4c6d-9c56-bc2d44efa64c.png">
    
    
    3. **If multiple data columns is provided**:
    <img width="330" alt="Screen Shot 2022-01-18 at 12 39 21 PM" src="https://user-images.githubusercontent.com/56903646/150014969-ece2d415-28d3-4055-8790-9e26e1889cef.png">
    Result does the same thing, except on multiple columns:
    <img width="1078" alt="Screen Shot 2022-01-18 at 12 39 35 PM" src="https://user-images.githubusercontent.com/56903646/150015005-8d0d593d-9b4b-4c84-b331-386ecd6ff87e.png">
    
    **Chart style**
    If "spline" is chosen, the line chart will use curve to connect data points.
    <img width="331" alt="Screen Shot 2022-01-18 at 1 51 49 PM" src="https://user-images.githubusercontent.com/56903646/150024513-7a52b6ba-d52f-43df-bdf4-8790adca3c45.png">
    The result for multiple data columns for spline chart style:
    <img width="1081" alt="Screen Shot 2022-01-18 at 1 53 22 PM" src="https://user-images.githubusercontent.com/56903646/150024732-9a643e16-4d58-4c60-9abf-c858d3199cbb.png">
    
    
    
    Co-authored-by: Xiaozhen Liu <xiaozl3@uci.edu>

---
+++ core/amber/src/main/scala/edu/uci/ics/texera/workflow/operators/visualization/lineChart/LineChartOpExec.scala
@@ -0,0 +1,25 @@
+package edu.uci.ics.texera.workflow.operators.visualization.lineChart
+
+import edu.uci.ics.texera.workflow.common.operators.map.MapOpExec
+import edu.uci.ics.texera.workflow.common.tuple.Tuple
+import edu.uci.ics.texera.workflow.common.tuple.schema.OperatorSchemaInfo
+import edu.uci.ics.texera.workflow.operators.visualization.lineChart.LineChartOpDesc
+
+class LineChartOpExec(
+    opDesc: LineChartOpDesc,
+    operatorSchemaInfo: OperatorSchemaInfo
+) extends MapOpExec {
+
+  setMapFunc(this.processTuple)
+
+  def processTuple(t: Tuple): Tuple = {
+    val builder = Tuple.newBuilder(operatorSchemaInfo.outputSchema)
+    val inputSchema = t.getSchema
+    builder.add(inputSchema.getAttribute(opDesc.nameColumn), t.getField(opDesc.nameColumn))
+    for (i <- opDesc.resultAttributeNames.indices) {
+      val dataName = opDesc.resultAttributeNames.apply(i)
+      builder.add(inputSchema.getAttribute(dataName), t.getField(dataName))
+    }
+    builder.build
+  }
+}
