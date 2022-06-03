zyratlo/texera

commit 2944f001334695f3d0d7e6223401f47aa8c19321

Update to a faster CSV parser library (#1529)
This PR switchs the CSV parser library from [scala-csv](https://github.com/tototoshi/scala-csv) to [univocity-parser](https://github.com/uniVocity/univocity-parsers). 
This is because `scala-csv` is very very slow for csv data with large rows. This makes the system completely unresponsive when doing schema inference for CSV files.

Here's some simple comparison statistics of CSV parser libraries. The CSV file contains 2 HTML pages in each record.
|                      | 100 records                    | 1000 records                    |
|----------------------|-----------------------------|------------------------------|
| scala-csv (current)            | 250 **seconds** (estimated) | 2500 **seconds** (estimated) |
| apache-commons-csv   | 294 **ms**                      | 1865 **ms**                      |
| **univocity-parser-csv** | 153 **ms**                      | 894 **ms**                        |

We choose univocity-parser-csv because it's fastest. Here's another online comparsion as a reference: https://github.com/uniVocity/csv-parsers-comparison


Moreover, the speed of schema inference is also tested. We compare our own implementation vs the schema inference implementation in [spark-csv](https://github.com/databricks/spark-csv/blob/master/src/main/scala/com/databricks/spark/csv/util/InferSchema.scala). 

|                      | 1000 records |
|----------------------|--------------|
| our schema inference | 31 ms        |
| spark-csv inference  | 17 ms        |

Result shows spark-csv's implementation is faster than our own implementation, however, since this is not a bottleneck, we stick with our current implementation.

core/amber/src/main/scala/edu/uci/ics/texera/workflow/operators/source/scan/csv/CSVScanSourceOpDesc.scala
@@ -14,8 +14,9 @@ import edu.uci.ics.texera.workflow.common.tuple.schema.{
 }
 import edu.uci.ics.texera.workflow.operators.source.scan.ScanSourceOpDesc
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize
+import com.univocity.parsers.csv.{CsvFormat, CsvParser, CsvParserSettings}
 
-import java.io.IOException
+import java.io.{File, FileInputStream, IOException, InputStreamReader}
 import scala.jdk.CollectionConverters.asJavaIterableConverter
 
 class CSVScanSourceOpDesc extends ScanSourceOpDesc {
@@ -61,40 +62,37 @@ class CSVScanSourceOpDesc extends ScanSourceOpDesc {
     if (filePath.isEmpty) {
       return null
     }
-    implicit object CustomFormat extends DefaultCSVFormat {
-      override val delimiter: Char = customDelimiter.get.charAt(0)
+    val inputReader = new InputStreamReader(new FileInputStream(new File(filePath.get)))
+
+    val csvFormat = new CsvFormat()
+    csvFormat.setDelimiter(customDelimiter.get.charAt(0))
+    val csvSetting = new CsvParserSettings()
+    csvSetting.setMaxCharsPerColumn(-1)
+    csvSetting.setFormat(csvFormat)
+    csvSetting.setHeaderExtractionEnabled(hasHeader)
+    val parser = new CsvParser(csvSetting)
+    parser.beginParsing(inputReader)
+
+    var data: Array[Array[String]] = Array()
+    val readLimit = limit.getOrElse(INFER_READ_LIMIT).min(INFER_READ_LIMIT)
+    for (i <- 0 until readLimit) {
+      val row = parser.parseNext()
+      data = data :+ row
     }
-    var reader: CSVReader = CSVReader.open(filePath.get)(CustomFormat)
-    val firstRow: Array[String] = reader.iterator.next().toArray
-    reader.close()
+    parser.stopParsing()
+    inputReader.close()
 
-    // reopen the file to read from the beginning
-    reader = CSVReader.open(filePath.get)(CustomFormat)
-
-    val startOffset = offset.getOrElse(0) + (if (hasHeader) 1 else 0)
-    val endOffset =
-      startOffset + limit.getOrElse(INFER_READ_LIMIT).min(INFER_READ_LIMIT)
     val attributeTypeList: Array[AttributeType] = inferSchemaFromRows(
-      reader.iterator
-        .slice(startOffset, endOffset)
-        .map(seq => seq.toArray)
+      data.iterator.asInstanceOf[Iterator[Array[Object]]]
     )
+    val header: Array[String] =
+      if (hasHeader) parser.getContext.headers()
+      else (1 to attributeTypeList.length).map(i => "column-" + i).toArray
 
-    reader.close()
-
-    // build schema based on inferred AttributeTypes
-    Schema.newBuilder
-      .add(
-        firstRow.indices
-          .map((i: Int) =>
-            new Attribute(
-              if (hasHeader) firstRow.apply(i) else "column-" + (i + 1),
-              attributeTypeList.apply(i)
-            )
-          )
-          .asJava
-      )
-      .build
+    Schema
+      .newBuilder()
+      .add(header.indices.map(i => new Attribute(header(i), attributeTypeList(i))).asJava)
+      .build()
   }
 
 }

core/amber/build.sbt
@@ -192,6 +192,9 @@ libraryDependencies += "org.scalactic" %% "scalactic" % "3.2.2"
 // https://mvnrepository.com/artifact/com.github.tototoshi/scala-csv
 libraryDependencies += "com.github.tototoshi" %% "scala-csv" % "1.3.6"
 
+// https://mvnrepository.com/artifact/com.univocity/univocity-parsers
+libraryDependencies += "com.univocity" % "univocity-parsers" % "2.9.1"
+
 // https://mvnrepository.com/artifact/com.konghq/unirest-java
 libraryDependencies += "com.konghq" % "unirest-java" % "3.11.11"
 

core/amber/src/main/scala/edu/uci/ics/texera/workflow/common/tuple/schema/AttributeTypeUtils.scala
@@ -8,6 +8,7 @@ import java.text.SimpleDateFormat
 import java.time.Instant
 import scala.util.Try
 import scala.util.control.Exception.allCatch
+import scala.collection.convert.ImplicitConversions.`collection AsScalaIterable`
 
 object AttributeTypeUtils extends Serializable {
 
@@ -62,6 +63,10 @@ object AttributeTypeUtils extends Serializable {
       builder.add(attr, parseField(tuple.getField(attr.getName), attr.getType))
     )
     builder.build()
+  }
+
+  def parseFields(fields: Array[Object], schema: Schema): Array[Object] = {
+    parseFields(fields, schema.getAttributes.map(attr => attr.getType).toArray)
   }
 
   /**

core/amber/src/main/scala/edu/uci/ics/texera/workflow/operators/source/scan/csv/CSVScanSourceOpExec.scala
@@ -1,51 +1,76 @@
 package edu.uci.ics.texera.workflow.operators.source.scan.csv
 
-import com.github.tototoshi.csv.{CSVReader, DefaultCSVFormat}
+import com.univocity.parsers.csv.{CsvFormat, CsvParser, CsvParserSettings}
 import edu.uci.ics.texera.workflow.common.operators.source.SourceOperatorExecutor
 import edu.uci.ics.texera.workflow.common.tuple.Tuple
-import edu.uci.ics.texera.workflow.common.tuple.schema.{Attribute, AttributeTypeUtils, Schema}
+import edu.uci.ics.texera.workflow.common.tuple.schema.{AttributeTypeUtils, Schema}
 
-import scala.collection.convert.ImplicitConversions.`collection AsScalaIterable`
+import java.io.{File, FileInputStream, InputStreamReader}
 
 class CSVScanSourceOpExec private[csv] (val desc: CSVScanSourceOpDesc)
     extends SourceOperatorExecutor {
   val schema: Schema = desc.inferSchema()
-  var reader: CSVReader = _
-  var rows: Iterator[Seq[String]] = _
+  var inputReader: InputStreamReader = _
+  var parser: CsvParser = _
+
+  var nextRow: Array[String] = _
+
   override def produceTexeraTuple(): Iterator[Tuple] = {
 
-    var tuples = rows
-      .map(fields =>
+    val rowIterator = new Iterator[Array[String]] {
+      override def hasNext: Boolean = {
+        if (nextRow != null) {
+          return true
+        }
+        nextRow = parser.parseNext()
+        nextRow != null
+      }
+
+      override def next(): Array[String] = {
+        val ret = nextRow
+        nextRow = null
+        ret
+      }
+    }
+
+    var tupleIterator = rowIterator
+      .drop(desc.offset.getOrElse(0))
+      .map(row => {
         try {
-          val parsedFields: Array[Object] = AttributeTypeUtils.parseFields(
-            fields.toArray,
-            schema.getAttributes
-              .map((attr: Attribute) => attr.getType)
-              .toArray
-          )
+          val parsedFields: Array[Object] =
+            AttributeTypeUtils.parseFields(row.asInstanceOf[Array[Object]], schema)
           Tuple.newBuilder(schema).addSequentially(parsedFields).build
         } catch {
           case _: Throwable => null
         }
-      )
-      .filter(tuple => tuple != null)
+      })
+      .filter(t => t != null)
 
-    if (desc.limit.isDefined) tuples = tuples.take(desc.limit.get)
-    tuples
+    if (desc.limit.isDefined) tupleIterator = tupleIterator.take(desc.limit.get)
+
+    tupleIterator
   }
 
   override def open(): Unit = {
-    implicit object CustomFormat extends DefaultCSVFormat {
-      override val delimiter: Char = desc.customDelimiter.get.charAt(0)
-    }
-    reader = CSVReader.open(desc.filePath.get)(CustomFormat)
-    // skip line if this worker reads the start of a file, and the file has a header line
-    val startOffset = desc.offset.getOrElse(0) + (if (desc.hasHeader) 1 else 0)
+    inputReader = new InputStreamReader(new FileInputStream(new File(desc.filePath.get)))
 
-    rows = reader.iterator.drop(startOffset)
+    val csvFormat = new CsvFormat()
+    csvFormat.setDelimiter(desc.customDelimiter.get.charAt(0))
+    val csvSetting = new CsvParserSettings()
+    csvSetting.setMaxCharsPerColumn(-1)
+    csvSetting.setFormat(csvFormat)
+    csvSetting.setHeaderExtractionEnabled(desc.hasHeader)
+
+    parser = new CsvParser(csvSetting)
+    parser.beginParsing(inputReader)
   }
 
   override def close(): Unit = {
-    reader.close()
+    if (parser != null) {
+      parser.stopParsing()
+    }
+    if (inputReader != null) {
+      inputReader.close()
+    }
   }
 }

commit 618c60d8026df46105fe1eaf18e870d4d7f5d7b8

Fix CSV reader schema inference (#1534)
This PR fixes a bug introduced by #1529, where schema inference on csv files with less than 100 rows encounters an null pointer exception

core/amber/src/main/scala/edu/uci/ics/texera/workflow/operators/source/scan/csv/CSVScanSourceOpDesc.scala
@@ -1,7 +1,6 @@
 package edu.uci.ics.texera.workflow.operators.source.scan.csv
 
 import com.fasterxml.jackson.annotation.{JsonProperty, JsonPropertyDescription}
-import com.github.tototoshi.csv.{CSVReader, DefaultCSVFormat}
 import com.kjetland.jackson.jsonSchema.annotations.JsonSchemaTitle
 import edu.uci.ics.amber.engine.operators.OpExecConfig
 import edu.uci.ics.texera.workflow.common.operators.ManyToOneOpExecConfig
@@ -77,7 +76,9 @@ class CSVScanSourceOpDesc extends ScanSourceOpDesc {
     val readLimit = limit.getOrElse(INFER_READ_LIMIT).min(INFER_READ_LIMIT)
     for (i <- 0 until readLimit) {
       val row = parser.parseNext()
-      data = data :+ row
+      if (row != null) {
+        data = data :+ row
+      }
     }
     parser.stopParsing()
     inputReader.close()