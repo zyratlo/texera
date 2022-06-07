e2bb982eaef3fcbbcb6b358d89339347eb942588
Yicong Huang <17627829+Yicong-Huang@users.noreply.github.com>
2022-06-07 09:17:40 -0700

    Fix scripts (#1538)
    This PR fixes some minor issues:
    1. Changed the typo in `depoly-daemon` to `deploy-daemon`.
    2. Removed source in `python-photo-gen.sh`, now we use the current python environment of the shell. Also made the paths relative so the script can be executed anywhere.

--- core/scripts/python-proto-gen.sh
+++ core/scripts/python-proto-gen.sh
@@ -1,3 +1,10 @@
-source ../venv/bin/activate
+# assuming inside the pytexera executing Python ENV
 
-protoc --python_betterproto_out=../amber/src/main/python/proto -I=../amber/src/main/protobuf  $(find ../amber/src/main/protobuf -iname "*.proto") --proto_path=../amber/src/main/protobuf
+# dirs
+TEXERA_ROOT="$(git rev-parse --show-toplevel)"
+AMBER_DIR="$TEXERA_ROOT/core/amber"
+PYAMBER_DIR="$AMBER_DIR/src/main/python"
+PROTOBUF_DIR="$AMBER_DIR/src/main/protobuf"
+
+# proto-gen
+protoc --python_betterproto_out="$PYAMBER_DIR/proto" -I="$PROTOBUF_DIR"  $(find "$PROTOBUF_DIR" -iname "*.proto") --proto_path="$PROTOBUF_DIR"
