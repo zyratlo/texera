038c59c22be11bcb8b3a46d32687edbdc4843cc7
Xinyuan Lin <xinyual3@uci.edu>
2022-10-27 16:27:06 -0700

    Use google email as username (#1714)

--- core/amber/src/main/scala/edu/uci/ics/texera/web/resource/auth/GoogleAuthResource.scala
+++ core/amber/src/main/scala/edu/uci/ics/texera/web/resource/auth/GoogleAuthResource.scala
@@ -57,22 +57,22 @@ object GoogleAuthResource {
       // get the subject of the payload, use this value as a key to identify a user.
       val googleId = payload.getSubject
       // get the Google username of the user, will be used as Texera username
-      val googleUsername = payload.get("name").asInstanceOf[String]
+      val googleEmail = payload.get("email").asInstanceOf[String]
 
       // store Google user id in database if it does not exist
       Option(userDao.fetchOneByGoogleId(googleId)) match {
         case Some(user) =>
           // the user's Google username could have been updated (due to user's action)
           // we update the user name in such case to reflect the change.
-          if (user.getName != googleUsername) {
-            user.setName(googleUsername)
+          if (user.getName != googleEmail) {
+            user.setName(googleEmail)
             userDao.update(user)
           }
           user
         case None =>
           // create a new user with googleId
           val user = new User
-          user.setName(googleUsername)
+          user.setName(googleEmail)
           user.setGoogleId(googleId)
           userDao.insert(user)
           user
