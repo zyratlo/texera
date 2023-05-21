zyratlo/texera

commit e383ca88f8667c8fe70267946f3b31a2913b419e

Move all filters to backend (#1912)
- Move other five filters from frontend to backend:
  1. Date filter for creation date
  2. Date filter for last_modified_date
  3. WorkflowID filter
  4. ProjectID filter
  5. Operator filter

- Corresponding unit tests are added to ensure correct performance.
- We use `User.EMAIL` instead of `User.NAME` to filter workflow in
`getOwnerFilter` due to the changes made in #1839

core/amber/src/test/scala/edu/uci/ics/texera/web/resource/dashboard/file/WorkflowResourceSpec.scala
@@ -8,6 +8,7 @@ import edu.uci.ics.texera.web.model.jooq.generated.tables.pojos.{User, Workflow}
 import org.jooq.types.UInteger
 import edu.uci.ics.texera.web.model.jooq.generated.enums.UserRole
 import edu.uci.ics.texera.web.model.jooq.generated.tables.daos.{UserDao, WorkflowDao}
+import edu.uci.ics.texera.web.resource.dashboard.user.workflow.WorkflowResource
 import edu.uci.ics.texera.Utils
 import edu.uci.ics.texera.web.resource.dashboard.user.workflow.WorkflowResource.DashboardWorkflowEntry
 import org.jooq.Condition
@@ -20,8 +21,10 @@ import edu.uci.ics.texera.web.model.jooq.generated.Tables.{
   WORKFLOW_OF_USER,
   WORKFLOW_USER_ACCESS
 }
-import edu.uci.ics.texera.web.resource.dashboard.user.workflow.WorkflowResource
 
+import java.util.concurrent.TimeUnit
+import java.sql.Timestamp
+import java.text.{ParseException, SimpleDateFormat}
 import java.nio.file.Files
 import java.nio.charset.StandardCharsets
 import java.util
@@ -350,20 +353,176 @@ class WorkflowResourceSpec
   it should "return a proper condition for a single owner" in {
     val ownerList = new java.util.ArrayList[String](util.Arrays.asList("owner1"))
     val ownerFilter: Condition = workflowResource.getOwnerFilter(ownerList)
-    assert(ownerFilter.toString == USER.NAME.eq("owner1").toString)
+    assert(ownerFilter.toString == USER.EMAIL.eq("owner1").toString)
   }
 
   it should "return a proper condition for multiple owners" in {
     val ownerList = new java.util.ArrayList[String](util.Arrays.asList("owner1", "owner2"))
     val ownerFilter: Condition = workflowResource.getOwnerFilter(ownerList)
-    assert(ownerFilter.toString == USER.NAME.eq("owner1").or(USER.NAME.eq("owner2")).toString)
+    assert(ownerFilter.toString == USER.EMAIL.eq("owner1").or(USER.EMAIL.eq("owner2")).toString)
   }
 
   it should "return a proper condition for multiple owners with duplicates" in {
     val ownerList =
       new java.util.ArrayList[String](util.Arrays.asList("owner1", "owner2", "owner2"))
     val ownerFilter: Condition = workflowResource.getOwnerFilter(ownerList)
-    assert(ownerFilter.toString == USER.NAME.eq("owner1").or(USER.NAME.eq("owner2")).toString)
+    assert(ownerFilter.toString == USER.EMAIL.eq("owner1").or(USER.EMAIL.eq("owner2")).toString)
+  }
+
+  "getProjectFilter" should "return a noCondition when the input projectIds list is null" in {
+    val projectFilter: Condition = workflowResource.getProjectFilter(null)
+    assert(projectFilter.toString == noCondition().toString)
+  }
+
+  it should "return a noCondition when the input projectIds list is empty" in {
+    val projectFilter: Condition =
+      workflowResource.getProjectFilter(Collections.emptyList[UInteger]())
+    assert(projectFilter.toString == noCondition().toString)
+  }
+
+  it should "return a proper condition for a single projectId" in {
+    val projectIdList = new java.util.ArrayList[UInteger](util.Arrays.asList(UInteger.valueOf(1)))
+    val projectFilter: Condition = workflowResource.getProjectFilter(projectIdList)
+    assert(projectFilter.toString == WORKFLOW_OF_PROJECT.PID.eq(UInteger.valueOf(1)).toString)
+  }
+
+  it should "return a proper condition for multiple projectIds" in {
+    val projectIdList = new java.util.ArrayList[UInteger](
+      util.Arrays.asList(UInteger.valueOf(1), UInteger.valueOf(2))
+    )
+    val projectFilter: Condition = workflowResource.getProjectFilter(projectIdList)
+    assert(
+      projectFilter.toString == WORKFLOW_OF_PROJECT.PID
+        .eq(UInteger.valueOf(1))
+        .or(WORKFLOW_OF_PROJECT.PID.eq(UInteger.valueOf(2)))
+        .toString
+    )
+  }
+
+  it should "return a proper condition for multiple projectIds with duplicates" in {
+    val projectIdList = new java.util.ArrayList[UInteger](
+      util.Arrays.asList(UInteger.valueOf(1), UInteger.valueOf(2), UInteger.valueOf(2))
+    )
+    val projectFilter: Condition = workflowResource.getProjectFilter(projectIdList)
+    assert(
+      projectFilter.toString == WORKFLOW_OF_PROJECT.PID
+        .eq(UInteger.valueOf(1))
+        .or(WORKFLOW_OF_PROJECT.PID.eq(UInteger.valueOf(2)))
+        .toString
+    )
+  }
+
+  "getWorkflowIdFilter" should "return a noCondition when the input workflowIDs list is null" in {
+    val workflowIdFilter: Condition = workflowResource.getWorkflowIdFilter(null)
+    assert(workflowIdFilter.toString == noCondition().toString)
+  }
+
+  it should "return a noCondition when the input workflowIDs list is empty" in {
+    val workflowIdFilter: Condition =
+      workflowResource.getWorkflowIdFilter(Collections.emptyList[UInteger]())
+    assert(workflowIdFilter.toString == noCondition().toString)
+  }
+
+  it should "return a proper condition for a single workflowID" in {
+    val workflowIdList = new java.util.ArrayList[UInteger](util.Arrays.asList(UInteger.valueOf(1)))
+    val workflowIdFilter: Condition = workflowResource.getWorkflowIdFilter(workflowIdList)
+    assert(workflowIdFilter.toString == WORKFLOW.WID.eq(UInteger.valueOf(1)).toString)
+  }
+
+  it should "return a proper condition for multiple workflowIDs" in {
+    val workflowIdList = new java.util.ArrayList[UInteger](
+      util.Arrays.asList(UInteger.valueOf(1), UInteger.valueOf(2))
+    )
+    val workflowIdFilter: Condition = workflowResource.getWorkflowIdFilter(workflowIdList)
+    assert(
+      workflowIdFilter.toString == WORKFLOW.WID
+        .eq(UInteger.valueOf(1))
+        .or(WORKFLOW.WID.eq(UInteger.valueOf(2)))
+        .toString
+    )
+  }
+
+  it should "return a proper condition for multiple workflowIDs with duplicates" in {
+    val workflowIdList = new java.util.ArrayList[UInteger](
+      util.Arrays.asList(UInteger.valueOf(1), UInteger.valueOf(2), UInteger.valueOf(2))
+    )
+    val workflowIdFilter: Condition = workflowResource.getWorkflowIdFilter(workflowIdList)
+    assert(
+      workflowIdFilter.toString == WORKFLOW.WID
+        .eq(UInteger.valueOf(1))
+        .or(WORKFLOW.WID.eq(UInteger.valueOf(2)))
+        .toString
+    )
+  }
+
+  "getDateFilter" should "return a noCondition when the input startDate and endDate are empty" in {
+    val dateFilter: Condition = workflowResource.getDateFilter("creation", "", "")
+    assert(dateFilter.toString == noCondition().toString)
+  }
+
+  it should "return a proper condition for creation date type with specific start and end date" in {
+    val dateFilter: Condition =
+      workflowResource.getDateFilter("creation", "2023-01-01", "2023-12-31")
+    val dateFormat = new SimpleDateFormat("yyyy-MM-dd")
+    val startTimestamp = new Timestamp(dateFormat.parse("2023-01-01").getTime)
+    val endTimestamp =
+      new Timestamp(dateFormat.parse("2023-12-31").getTime + TimeUnit.DAYS.toMillis(1) - 1)
+    assert(
+      dateFilter.toString == WORKFLOW.CREATION_TIME.between(startTimestamp, endTimestamp).toString
+    )
+  }
+
+  it should "return a proper condition for modification date type with specific start and end date" in {
+    val dateFilter: Condition =
+      workflowResource.getDateFilter("modification", "2023-01-01", "2023-12-31")
+    val dateFormat = new SimpleDateFormat("yyyy-MM-dd")
+    val startTimestamp = new Timestamp(dateFormat.parse("2023-01-01").getTime)
+    val endTimestamp =
+      new Timestamp(dateFormat.parse("2023-12-31").getTime + TimeUnit.DAYS.toMillis(1) - 1)
+    assert(
+      dateFilter.toString == WORKFLOW.LAST_MODIFIED_TIME
+        .between(startTimestamp, endTimestamp)
+        .toString
+    )
+  }
+
+  it should "throw an IllegalArgumentException for invalid dateType" in {
+    assertThrows[IllegalArgumentException] {
+      workflowResource.getDateFilter("invalidType", "2023-01-01", "2023-12-31")
+    }
+  }
+
+  it should "throw a ParseException when endDate is invalid" in {
+    assertThrows[ParseException] {
+      workflowResource.getDateFilter("creation", "2023-01-01", "invalidDate")
+    }
+  }
+
+  "getOperatorsFilter" should "return a noCondition when the input operators list is empty" in {
+    val operatorsFilter: Condition =
+      workflowResource.getOperatorsFilter(Collections.emptyList[String]())
+    assert(operatorsFilter.toString == noCondition().toString)
+  }
+
+  it should "return a proper condition for a single operator" in {
+    val operatorsList = new java.util.ArrayList[String](util.Arrays.asList("operator1"))
+    val operatorsFilter: Condition = workflowResource.getOperatorsFilter(operatorsList)
+    val searchKey = "%\"operatorType\":\"operator1\"%"
+    assert(operatorsFilter.toString == WORKFLOW.CONTENT.likeIgnoreCase(searchKey).toString)
+  }
+
+  it should "return a proper condition for multiple operators" in {
+    val operatorsList =
+      new java.util.ArrayList[String](util.Arrays.asList("operator1", "operator2"))
+    val operatorsFilter: Condition = workflowResource.getOperatorsFilter(operatorsList)
+    val searchKey1 = "%\"operatorType\":\"operator1\"%"
+    val searchKey2 = "%\"operatorType\":\"operator2\"%"
+    assert(
+      operatorsFilter.toString == WORKFLOW.CONTENT
+        .likeIgnoreCase(searchKey1)
+        .or(WORKFLOW.CONTENT.likeIgnoreCase(searchKey2))
+        .toString
+    )
   }
 
 }

core/amber/src/main/scala/edu/uci/ics/texera/web/resource/dashboard/user/workflow/WorkflowResource.scala
@@ -1,6 +1,8 @@
 package edu.uci.ics.texera.web.resource.dashboard.user.workflow
 
 import edu.uci.ics.texera.web.SqlServer
+
+import scala.collection.mutable.Set
 import edu.uci.ics.texera.web.auth.SessionUser
 import edu.uci.ics.texera.web.model.jooq.generated.Tables._
 import edu.uci.ics.texera.web.model.jooq.generated.enums.WorkflowUserAccessPrivilege
@@ -16,6 +18,16 @@ import org.jooq.Condition
 import org.jooq.impl.DSL.{groupConcat, noCondition}
 import org.jooq.types.UInteger
 
+import javax.ws.rs.DefaultValue
+import java.sql.Timestamp
+import java.text.{ParseException, SimpleDateFormat}
+import java.util.Optional
+import java.util.concurrent.TimeUnit
+import javax.ws.rs.DefaultValue
+import java.sql.Timestamp
+import java.text.{ParseException, SimpleDateFormat}
+import java.util.Optional
+import java.util.concurrent.TimeUnit
 import javax.annotation.security.RolesAllowed
 import javax.ws.rs._
 import javax.ws.rs.core.MediaType
@@ -430,6 +442,7 @@ class WorkflowResource {
     *
     * This method utilizes MySQL Boolean Full-Text Searches
     * reference: https://dev.mysql.com/doc/refman/8.0/en/fulltext-boolean.html
+    *
     * @param sessionUser The authenticated user.
     * @param keywords    The search keywords.
     * @return A list of workflows that match the search term.
@@ -477,12 +490,22 @@ class WorkflowResource {
       }
     }
 
-    // Apply owner filter
-    val ownerFilter = getOwnerFilter(owners)
     // combine all filters with AND
     var optionalFilters: Condition = noCondition()
     optionalFilters = optionalFilters
-      .and(ownerFilter)
+      // Apply creation_time date filter
+      .and(getDateFilter("creation", creationStartDate, creationEndDate))
+      // Apply lastModified_time date filter
+      .and(getDateFilter("modification", modifiedStartDate, modifiedEndDate))
+      // Apply workflowID filter
+      .and(getWorkflowIdFilter(workflowIDs))
+      // Apply owner filter
+      .and(getOwnerFilter(owners))
+      // Apply operators filter
+      .and(getOperatorsFilter(operators))
+      // Apply projectId filter
+      .and(getProjectFilter(projectIds))
+
     try {
       val workflowEntries = context
         .select(
@@ -551,22 +574,140 @@ class WorkflowResource {
 
   /**
     * Helper function to retrieve the owner filter.
-    * Applies a filter based on the specified owner names.
+    * Applies a filter based on the specified owner emails.
     *
-    * @param owners The list of owner names to filter by.
+    * @param owners The list of owner emails to filter by.
     * @return The owner filter.
     */
   def getOwnerFilter(owners: java.util.List[String]): Condition = {
     var ownerFilter: Condition = noCondition()
-    val ownerSet: mutable.Set[String] = mutable.Set()
+    val ownerSet: Set[String] = Set()
     if (owners != null && !owners.isEmpty) {
       for (owner <- owners) {
         if (!ownerSet(owner)) {
           ownerSet += owner
-          ownerFilter = ownerFilter.or(USER.NAME.eq(owner))
+          ownerFilter = ownerFilter.or(USER.EMAIL.eq(owner))
         }
       }
     }
     ownerFilter
+  }
+
+  /**
+    * Helper function to retrieve the project filter.
+    * Applies a filter based on the specified project IDs.
+    *
+    * @param projectIds The list of owner names to filter by.
+    * @return The projectId filter.
+    */
+  def getProjectFilter(projectIds: java.util.List[UInteger]): Condition = {
+    var projectIdFilter: Condition = noCondition()
+    val projectIdSet: Set[UInteger] = Set()
+    if (projectIds != null && projectIds.nonEmpty) {
+      for (projectId <- projectIds) {
+        if (!projectIdSet(projectId)) {
+          projectIdSet += projectId
+          projectIdFilter = projectIdFilter.or(WORKFLOW_OF_PROJECT.PID.eq(projectId))
+
+        }
+      }
+    }
+    projectIdFilter
+  }
+
+  /**
+    * Helper function to retrieve the workflowID filter.
+    * Applies a filter based on the specified workflow IDs.
+    *
+    * @param workflowIDs The list of workflow IDs to filter by.
+    * @return The workflowID filter.
+    */
+  def getWorkflowIdFilter(workflowIDs: java.util.List[UInteger]): Condition = {
+    var workflowIdFilter: Condition = noCondition()
+    val workflowIdSet: Set[UInteger] = Set()
+    if (workflowIDs != null && !workflowIDs.isEmpty) {
+      for (workflowID <- workflowIDs) {
+        if (!workflowIdSet(workflowID)) {
+          workflowIdSet += workflowID
+          workflowIdFilter = workflowIdFilter.or(WORKFLOW.WID.eq(workflowID))
+
+        }
+      }
+    }
+    workflowIdFilter
+  }
+
+  /**
+    * Returns a date filter condition for the specified date range and date type.
+    *
+    * @param dateType  A string representing the type of date to filter by.
+    *                  Accepts "creation" for creation date or "modification" for modification date.
+    * @param startDate A string representing the start date of the filter range in "yyyy-MM-dd" format.
+    *                  If empty, the default value "1970-01-01" will be used.
+    * @param endDate   A string representing the end date of the filter range in "yyyy-MM-dd" format.
+    *                  If empty, the default value "9999-12-31" will be used.
+    * @return A Condition object that can be used to filter workflows based on the date range and type.
+    */
+  def getDateFilter(
+      dateType: String,
+      startDate: String,
+      endDate: String
+  ): Condition = {
+    var dateFilter: Condition = noCondition()
+
+    if (startDate.nonEmpty || endDate.nonEmpty) {
+      val start = if (startDate.nonEmpty) startDate else "1970-01-01"
+      val end = if (endDate.nonEmpty) endDate else "9999-12-31"
+      val dateFormat = new SimpleDateFormat("yyyy-MM-dd")
+      try {
+        val startTimestamp = new Timestamp(dateFormat.parse(start).getTime)
+        val endTimestamp =
+          if (end == "9999-12-31")
+            new Timestamp(
+              dateFormat.parse(end).getTime
+            )
+          else
+            new Timestamp(
+              dateFormat.parse(end).getTime + TimeUnit.DAYS.toMillis(1) - 1
+            )
+        dateType match {
+          case "creation" =>
+            dateFilter = WORKFLOW.CREATION_TIME.between(startTimestamp, endTimestamp)
+          case "modification" =>
+            dateFilter = WORKFLOW.LAST_MODIFIED_TIME.between(startTimestamp, endTimestamp)
+          case _ => throw new IllegalArgumentException("Invalid dateType value")
+        }
+      } catch {
+        case ex: ParseException =>
+          println("Invalid date format. Please follow this date format: yyyy-MM-dd")
+          throw ex
+      }
+    }
+    dateFilter
+  }
+
+  /**
+    * Helper function to retrieve the operators filter.
+    * Applies a filter based on the specified operators.
+    *
+    * @param operators The list of operators to filter by.
+    * @return The operators filter.
+    */
+  def getOperatorsFilter(operators: java.util.List[String]): Condition = {
+    var operatorsFilter: Condition = noCondition()
+    if (operators != null && operators.nonEmpty) {
+      for (operator <- operators) {
+        val quotes = "\""
+        val searchKey =
+          "%" + quotes + "operatorType" + quotes + ":" + quotes + s"$operator" + quotes + "%"
+        operatorsFilter = operatorsFilter.or(
+          WORKFLOW.CONTENT
+            .likeIgnoreCase(
+              searchKey
+            )
+        )
+      }
+    }
+    operatorsFilter
   }
 }

commit 047df0aa70afd0ef8622a3cae19f287f7a4a516d

Initialize MockTexeraDB using JDBC (#1919)
This PR addresses the issue of frequent test failures that started
occurring after commit #1912. The failure is caused by the
initialization of the mock Texera DB. Upon running the Texera DDL
script, the DB exits due to a known issue in the MariaDB4j library, as
documented in this GitHub issue:
https://github.com/MariaDB4j/MariaDB4j/issues/233.

To resolve this problem, I have identified several potential solutions:

1. Upgrade the library to version 3.0.0. However, this requires Java 17,
and migrating our codebase to Java 17 would involve a substantial
effort.
2. Downgrade the library to version 2.3.0. However, many of the
functionalities we rely on are not yet available in this version.
3. Upon further investigation, I discovered that the error comes from a
managed process in MariaDB4j which executes the SQL. To solve this
issue, we can execute the SQL script through JDBC, thereby avoiding the
usage of the managed process.

core/scripts/sql/texera_ddl.sql
@@ -15,7 +15,7 @@ DROP TABLE IF EXISTS `file_of_workflow`;
 DROP TABLE IF EXISTS `file_of_project`;
 DROP TABLE IF EXISTS `workflow_executions`;
 
-SET GLOBAL time_zone = '+00:00'; # this line is mandatory
+SET GLOBAL time_zone = '+00:00'; -- this line is mandatory
 
 CREATE TABLE IF NOT EXISTS user
 (
@@ -155,8 +155,8 @@ CREATE TABLE IF NOT EXISTS workflow_executions
     `vid`             INT UNSIGNED NOT NULL,
     `uid`             INT UNSIGNED NOT NULL,
     `status`          TINYINT NOT NULL DEFAULT 1,
-    `result`          TEXT, #pointer to volume
-    `starting_time`   TIMESTAMP                   NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `result`          TEXT, /* pointer to volume */
+    `starting_time`   TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     `last_update_time`   TIMESTAMP,
     `bookmarked`      BOOLEAN DEFAULT FALSE,
     `name`				VARCHAR(128) NOT NULL DEFAULT 'Untitled Execution',

core/amber/src/test/scala/edu/uci/ics/texera/web/MockTexeraDB.scala
@@ -5,16 +5,50 @@ import com.mysql.cj.jdbc.MysqlDataSource
 import org.jooq.DSLContext
 import org.jooq.impl.DSL
 import ch.vorburger.mariadb4j.{DB, DBConfigurationBuilder}
-import edu.uci.ics.amber.engine.common.AmberUtils
 import edu.uci.ics.texera.Utils
 
-import java.nio.charset.StandardCharsets
-import java.nio.file.{Files, Path, Paths}
+import java.io.{File, FileInputStream, InputStream}
+import java.nio.file.Path
+import java.sql.{Connection, DriverManager, SQLException, Statement}
+import java.util.Scanner
 
 trait MockTexeraDB {
 
   private var dbInstance: Option[DB] = None
   private var dslContext: Option[DSLContext] = None
+  private val database: String = "texera_db"
+  private val username: String = "root"
+  private val password: String = ""
+
+  def executeScriptInJDBC(path: Path): Unit = {
+    assert(dbInstance.nonEmpty)
+    val sqlFile = new File(path.toString)
+    val in = new FileInputStream(sqlFile)
+    val conn =
+      DriverManager.getConnection(dbInstance.get.getConfiguration.getURL(""), username, password)
+    importSQL(conn, in)
+    conn.close()
+  }
+
+  @throws[SQLException]
+  private def importSQL(conn: Connection, in: InputStream): Unit = {
+    val s = new Scanner(in)
+    s.useDelimiter(";")
+    var st: Statement = null
+    try {
+      st = conn.createStatement()
+      while ({
+        s.hasNext
+      }) {
+        var line = s.next
+        if (line.startsWith("/*!") && line.endsWith("*/")) {
+          val i = line.indexOf(' ')
+          line = line.substring(i + 1, line.length - " */".length)
+        }
+        if (line.trim.nonEmpty) st.execute(line)
+      }
+    } finally if (st != null) st.close()
+  }
 
   def getDSLContext: DSLContext = {
     dslContext match {
@@ -49,13 +83,6 @@ trait MockTexeraDB {
 
   def initializeDBAndReplaceDSLContext(): Unit = {
     assert(dbInstance.isEmpty && dslContext.isEmpty)
-    val database: String = "texera_db"
-    val username: String = "root"
-    val password: String = ""
-    val ddlPath = {
-      Utils.amberHomePath.resolve("../scripts/sql/texera_ddl.sql").toRealPath()
-    }
-    val content = new String(Files.readAllBytes(ddlPath), StandardCharsets.UTF_8)
 
     val config = DBConfigurationBuilder.newBuilder
       .setPort(0) // 0 => automatically detect free port
@@ -66,7 +93,6 @@ trait MockTexeraDB {
 
     val db = DB.newEmbeddedDB(config)
     db.start()
-    db.run(content)
 
     val dataSource = new MysqlDataSource
     dataSource.setUrl(config.getURL(database))
@@ -75,6 +101,12 @@ trait MockTexeraDB {
 
     dbInstance = Some(db)
     dslContext = Some(DSL.using(dataSource, SqlServer.SQL_DIALECT))
+
+    val ddlPath = {
+      Utils.amberHomePath.resolve("../scripts/sql/texera_ddl.sql").toRealPath()
+    }
+    executeScriptInJDBC(ddlPath)
+
     SqlServer.replaceDSLContext(dslContext.get)
   }
 }

core/amber/src/test/scala/edu/uci/ics/texera/web/resource/dashboard/file/WorkflowResourceSpec.scala
@@ -113,14 +113,11 @@ class WorkflowResourceSpec
 
   override protected def beforeAll(): Unit = {
     initializeDBAndReplaceDSLContext()
-    val db = getDBInstance
     // build fulltext indexes
     val fulltextIndexPath = {
       Utils.amberHomePath.resolve("../scripts/sql/update/fulltext_indexes.sql").toRealPath()
     }
-    val buildFulltextIndex =
-      new String(Files.readAllBytes(fulltextIndexPath), StandardCharsets.UTF_8)
-    db.run(buildFulltextIndex)
+    executeScriptInJDBC(fulltextIndexPath)
 
     // add test user directly
     val userDao = new UserDao(getDSLContext.configuration())