25f146e0a287615e01b4fd0c5a3532e4e4b52d08
Xinyuan Lin <xinyual3@uci.edu>
2023-06-13 18:06:37 -0700

    Merge JDBC config to backend (#1997)
    The database connection information used to be a separate config file.
    We want to keep fewer config files.
    Merging it into the backend config to make it easy to maintain.

--- core/amber/src/main/scala/edu/uci/ics/texera/web/SqlServer.java
+++ core/amber/src/main/scala/edu/uci/ics/texera/web/SqlServer.java
@@ -1,31 +1,21 @@
 package edu.uci.ics.texera.web;
 
 import com.mysql.cj.jdbc.MysqlDataSource;
-import com.typesafe.config.Config;
-import com.typesafe.config.ConfigFactory;
-import edu.uci.ics.texera.Utils;
+import edu.uci.ics.amber.engine.common.AmberUtils;
 import org.jooq.DSLContext;
 import org.jooq.SQLDialect;
 import org.jooq.impl.DSL;
 
-import java.nio.file.Path;
-
 public final class SqlServer {
-
     public static final SQLDialect SQL_DIALECT = SQLDialect.MYSQL;
     private static final MysqlDataSource dataSource;
-    public static Config jdbcConfig;
     public static DSLContext context;
 
     static {
-        Path jdbcConfPath = Utils.amberHomePath().resolve("../conf/jdbc.conf").toAbsolutePath();
-
-        jdbcConfig = ConfigFactory.parseFile(jdbcConfPath.toFile());
-
         dataSource = new MysqlDataSource();
-        dataSource.setUrl(jdbcConfig.getString("jdbc.url"));
-        dataSource.setUser(jdbcConfig.getString("jdbc.username"));
-        dataSource.setPassword(jdbcConfig.getString("jdbc.password"));
+        dataSource.setUrl(AmberUtils.amberConfig().getString("jdbc.url"));
+        dataSource.setUser(AmberUtils.amberConfig().getString("jdbc.username"));
+        dataSource.setPassword(AmberUtils.amberConfig().getString("jdbc.password"));
         context = DSL.using(dataSource, SQL_DIALECT);
     }
 
