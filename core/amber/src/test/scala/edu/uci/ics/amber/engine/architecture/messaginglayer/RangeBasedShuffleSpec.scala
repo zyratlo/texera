363d271d2083ccb78d23119eb130f11d3cae1254
Yicong Huang <17627829+Yicong-Huang@users.noreply.github.com>
2023-06-21 15:46:59 -0700

    Remove the deprecated Tuple.Builder (#2018)
    This PR is to follow up #1121 and finishes #1140. Now we remove all
    usage of the old `Tuple.Builder`, and migrated to `Tuple.BuilderV2`
    instead.

--- core/amber/src/test/scala/edu/uci/ics/amber/engine/architecture/messaginglayer/RangeBasedShuffleSpec.scala
+++ core/amber/src/test/scala/edu/uci/ics/amber/engine/architecture/messaginglayer/RangeBasedShuffleSpec.scala
@@ -4,17 +4,17 @@ import edu.uci.ics.amber.engine.architecture.sendsemantics.partitioners.RangeBas
 import edu.uci.ics.amber.engine.architecture.sendsemantics.partitionings.RangeBasedShufflePartitioning
 import edu.uci.ics.amber.engine.common.virtualidentity.ActorVirtualIdentity
 import edu.uci.ics.texera.workflow.common.tuple.Tuple
-import edu.uci.ics.texera.workflow.common.tuple.schema.AttributeType
+import edu.uci.ics.texera.workflow.common.tuple.schema.{Attribute, AttributeType, Schema}
 import org.scalamock.scalatest.MockFactory
 import org.scalatest.flatspec.AnyFlatSpec
 
 class RangeBasedShuffleSpec extends AnyFlatSpec with MockFactory {
 
-  val fakeID1 = ActorVirtualIdentity("rec1")
-  val fakeID2 = ActorVirtualIdentity("rec2")
-  val fakeID3 = ActorVirtualIdentity("rec3")
-  val fakeID4 = ActorVirtualIdentity("rec4")
-  val fakeID5 = ActorVirtualIdentity("rec5")
+  val fakeID1: ActorVirtualIdentity = ActorVirtualIdentity("rec1")
+  val fakeID2: ActorVirtualIdentity = ActorVirtualIdentity("rec2")
+  val fakeID3: ActorVirtualIdentity = ActorVirtualIdentity("rec3")
+  val fakeID4: ActorVirtualIdentity = ActorVirtualIdentity("rec4")
+  val fakeID5: ActorVirtualIdentity = ActorVirtualIdentity("rec5")
 
   val partitioning: RangeBasedShufflePartitioning =
     RangeBasedShufflePartitioning(
@@ -24,45 +24,51 @@ class RangeBasedShuffleSpec extends AnyFlatSpec with MockFactory {
       -400,
       600
     )
+  val attr: Attribute = new Attribute("Attr1", AttributeType.INTEGER)
+  val schema: Schema = Schema.newBuilder().add(attr).build()
 
-  val partitioner = RangeBasedShufflePartitioner(partitioning)
+  val partitioner: RangeBasedShufflePartitioner = RangeBasedShufflePartitioner(partitioning)
 
   "RangeBasedShuffleSpec" should "return 0 when value is less than rangeMin" in {
-    val tuple = Tuple.newBuilder().add("Attr1", AttributeType.INTEGER, -600).build()
+    val tuple = Tuple.newBuilder(schema).add(attr, -600).build()
     val idx = partitioner.getBucketIndex(tuple)
     assert(idx.next() == 0)
   }
 
   "RangeBasedShuffleSpec" should "return last receiver when value is more than rangeMax" in {
-    val tuple = Tuple.newBuilder().add("Attr1", AttributeType.INTEGER, 800).build()
+    val tuple = Tuple.newBuilder(schema).add(attr, 800).build()
     val idx = partitioner.getBucketIndex(tuple)
     assert(idx.next() == 4)
   }
 
   "RangeBasedShuffleSpec" should "find index correctly" in {
-    var tuple = Tuple.newBuilder().add("Attr1", AttributeType.INTEGER, -400).build()
+    var tuple = Tuple.newBuilder(schema).add(attr, -400).build()
     var idx = partitioner.getBucketIndex(tuple)
     assert(idx.next() == 0)
 
-    tuple = Tuple.newBuilder().add("Attr1", AttributeType.INTEGER, -200).build()
+    tuple = Tuple.newBuilder(schema).add(attr, -200).build()
     idx = partitioner.getBucketIndex(tuple)
     assert(idx.next() == 0)
 
-    tuple = Tuple.newBuilder().add("Attr1", AttributeType.INTEGER, -199).build()
+    tuple = Tuple.newBuilder(schema).add(attr, -199).build()
     idx = partitioner.getBucketIndex(tuple)
     assert(idx.next() == 1)
   }
 
   "RangeBasedShuffleSpec" should "handle different data types correctly" in {
-    var tuple = Tuple.newBuilder().add("Attr1", AttributeType.INTEGER, -90).build()
+    var tuple = Tuple.newBuilder(schema).add(attr, -90).build()
     var idx = partitioner.getBucketIndex(tuple)
     assert(idx.next() == 1)
 
-    tuple = Tuple.newBuilder().add("Attr1", AttributeType.DOUBLE, -90.5).build()
+    val doubleAttr: Attribute = new Attribute("Attr2", AttributeType.DOUBLE)
+    val doubleSchema: Schema = Schema.newBuilder().add(doubleAttr).build()
+    tuple = Tuple.newBuilder(doubleSchema).add(doubleAttr, -90.5).build()
     idx = partitioner.getBucketIndex(tuple)
     assert(idx.next() == 1)
 
-    tuple = Tuple.newBuilder().add("Attr1", AttributeType.LONG, -90L).build()
+    val longAttr: Attribute = new Attribute("Attr3", AttributeType.LONG)
+    val longSchema: Schema = Schema.newBuilder().add(longAttr).build()
+    tuple = Tuple.newBuilder(longSchema).add(longAttr, -90L).build()
     idx = partitioner.getBucketIndex(tuple)
     assert(idx.next() == 1)
   }
