92542ee89cf92621a21ddb1575e21cafca3400ab
Yicong Huang <17627829+Yicong-Huang@users.noreply.github.com>
2023-07-13 10:49:15 -0700

    Remove the deprecated protractor and karma-coverage-istanbul-reporter (#2058)
    This PR:
    
    - removes `protractor` as it has been [deprecated and will reach the
    end-of-life this summer](https://www.npmjs.com/package/protractor).
    - replaces `karma-coverage-istanbul-reporter` (deprecated after Angular
    11) with `karma-coverage`, as recommended by this
    [blog](https://sealights.atlassian.net/wiki/spaces/SUP/pages/2308538386/Javascript+-+karma-coverage-istanbul-reporter+usage+has+been+deprecated+since+version+11+Message+for+Angular+11).

--- core/new-gui/karma.conf.js
+++ core/new-gui/karma.conf.js
@@ -1,30 +1,30 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
-module.exports = function (config) {
+module.exports = function(config) {
   config.set({
     basePath: "",
     frameworks: ["jasmine", "@angular-devkit/build-angular"],
     plugins: [
       require("karma-jasmine"),
       require("karma-chrome-launcher"),
+      require("karma-coverage"),
       require("karma-jasmine-html-reporter"),
-      require("karma-coverage-istanbul-reporter"),
-      require("@angular-devkit/build-angular/plugins/karma"),
+      require("@angular-devkit/build-angular/plugins/karma")
     ],
     client: {
       clearContext: false, // leave Jasmine Spec Runner output visible in browser
       jasmine: {
-        random: false, // disable the random running order
-      },
+        random: false // disable the random running order
+      }
     },
-    coverageIstanbulReporter: {
-      dir: require("path").join(__dirname, "coverage"),
-      reports: ["html", "lcovonly"],
-      fixWebpackSourcePaths: true,
+    coverageReporter: {
+      dir: "build/reports/coverage",
+      reporters: [
+        { type: "lcov", subdir: "report-lcov" }
+      ]
     },
-
-    reporters: ["progress", "kjhtml"],
+    reporters: ["progress", "kjhtml", "coverage"],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
@@ -32,8 +32,8 @@ module.exports = function (config) {
     browsers: ["Chrome"],
     singleRun: false,
     captureTimeout: 60000,
-    browserDisconnectTimeout : 10000,
-    browserDisconnectTolerance : 3,
-    browserNoActivityTimeout : 60000,
+    browserDisconnectTimeout: 10000,
+    browserDisconnectTolerance: 3,
+    browserNoActivityTimeout: 60000
   });
 };
