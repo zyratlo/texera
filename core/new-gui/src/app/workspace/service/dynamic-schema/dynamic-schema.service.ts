zyratlo/texera

commit 3a23cf0b6f905a5e176683d33e5aa8315280ec5e

Fix optional field displaying "no longer a valid option" error message (#2101)
This PR addresses an issue in the current master where certain operators
contain an optional field annotated with `@AutofillAttributeName`. In
the backend code, this optional field is assigned a null value. However,
during schema propagation, the frontend performs a check to ensure that
the
value corresponds to a valid attribute name. The null value will be
converted to an empty string by the frontend, then an error message is
generated for the optional field, as an empty string is not present in
the enum of all attribute names.
<img width="344" alt="screen"
src="https://github.com/Texera/texera/assets/13672781/ad545c43-a150-4606-816a-aa9556ceb8ef">

This PR adds an empty string to the enum list if the field is not marked
as required.

---------

Co-authored-by: Yicong Huang <17627829+Yicong-Huang@users.noreply.github.com>

core/new-gui/src/app/workspace/service/dynamic-schema/schema-propagation/schema-propagation.service.ts
@@ -218,6 +218,23 @@ export class SchemaPropagationService {
       if (v.additionalEnumValue) {
         attrNames.push(v.additionalEnumValue);
       }
+
+      // ajv does not support null values, so it converts all the nulls to empty strings.
+      // https://github.com/ajv-validator/ajv/issues/1471
+      // the null -> "" change is done by Ajv.validate() with useDefault set to true.
+      // It is converted during the property editor form initialization and workflow validation, instead of during schema propagation.
+      if (v.title && !operatorSchema.jsonSchema.required?.includes(v.title)) {
+        if (v.default) {
+          if (typeof v.default !== "string") {
+            throw new Error("default value must be a string");
+          }
+          // We are adding the default value or "" into
+          // the enum list to pass the frontend check for optional properties.
+          attrNames.push(v.default);
+        } else {
+          attrNames.push("");
+        }
+      }
       return attrNames;
     };
 

core/new-gui/src/app/workspace/service/dynamic-schema/schema-propagation/schema-propagation.service.spec.ts
@@ -359,6 +359,9 @@ describe("SchemaPropagationService", () => {
       attr => attr.attributeName
     );
 
+    // since field "attribute" is not required, we need to allow empty value.
+    expectedEnum!.push("");
+
     expect(schema.jsonSchema!.properties).toEqual({
       listOfAggregations: {
         title: "list of aggregations",

commit ad36ac88c3617260225ea090e066322eeb964e6a

Fix for empty attribute name as an option for required fields (#2115)
This PR addressed a bug introduced by #2101, which shows an empty string
as an option column name for required fields. This is because the
attribute title (set by `@JsonSchemaTitle`) is used to check if the
attribute is required, while the required attribute list contains
variable names.
The erroneous behavior:
<img width="1146" alt="截屏2023-08-09 下午9 19 44"
src="https://github.com/Texera/texera/assets/13672781/6a3d0c57-f301-4f75-b71c-d08fd843e777">
The fix:
<img width="1045" alt="截屏2023-08-09 下午9 18 40"
src="https://github.com/Texera/texera/assets/13672781/ade22cf0-79ed-4f02-9357-a6c5bd2a5833">

core/new-gui/src/app/workspace/service/dynamic-schema/dynamic-schema.service.ts
@@ -175,7 +175,7 @@ export class DynamicSchemaService {
   public static mutateProperty(
     jsonSchemaToChange: CustomJSONSchema7,
     matchFunc: (propertyName: string, propertyValue: CustomJSONSchema7) => boolean,
-    mutationFunc: (propertyValue: CustomJSONSchema7) => CustomJSONSchema7
+    mutationFunc: (propertyName: string, propertyValue: CustomJSONSchema7) => CustomJSONSchema7
   ): CustomJSONSchema7 {
     // recursively walks the JSON schema property tree to find the property name
     const mutatePropertyRecurse = (jsonSchema: JSONSchema7) => {
@@ -190,7 +190,7 @@ export class DynamicSchemaService {
             return;
           }
           if (matchFunc(propertyName, propertyValue as CustomJSONSchema7)) {
-            objectProperty[propertyName] = mutationFunc(propertyValue as CustomJSONSchema7);
+            objectProperty[propertyName] = mutationFunc(propertyName, propertyValue as CustomJSONSchema7);
           } else {
             mutatePropertyRecurse(propertyValue);
           }

core/new-gui/src/app/workspace/service/dynamic-schema/schema-propagation/schema-propagation.service.ts
@@ -205,7 +205,7 @@ export class SchemaPropagationService {
 
     let newJsonSchema = operatorSchema.jsonSchema;
 
-    const getAttrNames = (v: CustomJSONSchema7): string[] | undefined => {
+    const getAttrNames = (attrName: string, v: CustomJSONSchema7): string[] | undefined => {
       const i = v.autofillAttributeOnPort;
       if (i === undefined || i === null || !Number.isInteger(i) || i >= inputAttributes.length) {
         return undefined;
@@ -223,7 +223,7 @@ export class SchemaPropagationService {
       // https://github.com/ajv-validator/ajv/issues/1471
       // the null -> "" change is done by Ajv.validate() with useDefault set to true.
       // It is converted during the property editor form initialization and workflow validation, instead of during schema propagation.
-      if (v.title && !operatorSchema.jsonSchema.required?.includes(v.title)) {
+      if (!operatorSchema.jsonSchema.required?.includes(attrName)) {
         if (v.default) {
           if (typeof v.default !== "string") {
             throw new Error("default value must be a string");
@@ -241,10 +241,10 @@ export class SchemaPropagationService {
     newJsonSchema = DynamicSchemaService.mutateProperty(
       newJsonSchema,
       (k, v) => v.autofill === "attributeName",
-      old => ({
+      (attrName, old) => ({
         ...old,
         type: "string",
-        enum: getAttrNames(old),
+        enum: getAttrNames(attrName, old),
         uniqueItems: true,
       })
     );
@@ -252,14 +252,14 @@ export class SchemaPropagationService {
     newJsonSchema = DynamicSchemaService.mutateProperty(
       newJsonSchema,
       (k, v) => v.autofill === "attributeNameList",
-      old => ({
+      (attrName, old) => ({
         ...old,
         type: "array",
         uniqueItems: true,
         items: {
           ...(old.items as CustomJSONSchema7),
           type: "string",
-          enum: getAttrNames(old),
+          enum: getAttrNames(attrName, old),
         },
       })
     );
@@ -276,7 +276,7 @@ export class SchemaPropagationService {
     newJsonSchema = DynamicSchemaService.mutateProperty(
       newJsonSchema,
       (k, v) => v.autofill === "attributeName",
-      old => ({
+      (attrName, old) => ({
         ...old,
         type: "string",
         enum: undefined,
@@ -287,7 +287,7 @@ export class SchemaPropagationService {
     newJsonSchema = DynamicSchemaService.mutateProperty(
       newJsonSchema,
       (k, v) => v.autofill === "attributeNameList",
-      old => ({
+      (attrName, old) => ({
         ...old,
         type: "array",
         uniqueItems: undefined,