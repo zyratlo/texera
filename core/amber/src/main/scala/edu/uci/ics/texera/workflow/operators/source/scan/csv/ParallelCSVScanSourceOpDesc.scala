zyratlo/texera

commit 6cadd8f7c530e56f69ee52fd23ba7e114f2435df

Refactor backend ports in compiler (#2299)
This PR refactors the ports on the backend.

### PortIdentity
Previously, we store ports in a list and used the index to refer to a
port. This is not useful when the ports are mapped from logical operator
to physical operator (the exact sequence of the ports may change), or
when supporting dynamic ports (removing a port would cause the index
change).

Now we introduce `PortIdentity` as the id of a port. The same port and
identity will be used across logical, physical, and in workers.

The exact structure of PortIdentity is shown below (generated by
protobuf):
``` 
final case class PortIdentity(
    id: Int = 0,
    internal: Boolean = false
)
```
A PortIdentity has a field for indicating `internal` or not. This is to
differentiate internal ports and external ports. Internal ports are
ports that are added between the physical operators of a logical
operator. External ports are those set on the logical operator and link
to other ports from other logical operators.

### New InputPort and OutputPort
The `InputPort` has been updated (generated by protobuf):
```
final case class InputPort(
    id: PortIdentity = PortIdentity(),
    displayName: String = "",
    allowMultiLinks: Boolean = false,
    dependencies: Seq[PortIdentity] = Seq.empty
 )
```
Notably, we moved `dependencies` from the PhysicalOp into InputPort. It
is now a sequence of PortIdentities which denotes the ports that this
port is depending on.

The `OutputPort` has minor changes and we will adding the `blocking`
property into it in the future.

### LogicalLink and PhysicalLink
Both links are updated to use PortIdentities instead of port index (Int)
accordingly:
```
case class LogicalLink(
    @JsonProperty("fromOpId") fromOpId: OperatorIdentity,
    fromPortId: PortIdentity,
    @JsonProperty("toOpId") toOpId: OperatorIdentity,
    toPortId: PortIdentity
)
```
```
final case class PhysicalLink(

core/amber/src/main/scala/edu/uci/ics/texera/workflow/operators/union/UnionOpDesc.scala
@@ -4,14 +4,10 @@ import com.google.common.base.Preconditions
 import edu.uci.ics.amber.engine.architecture.deploysemantics.PhysicalOp
 import edu.uci.ics.amber.engine.architecture.deploysemantics.layer.OpExecInitInfo
 import edu.uci.ics.amber.engine.common.virtualidentity.{ExecutionIdentity, WorkflowIdentity}
-import edu.uci.ics.texera.workflow.common.metadata.{
-  InputPort,
-  OperatorGroupConstants,
-  OperatorInfo,
-  OutputPort
-}
+import edu.uci.ics.texera.workflow.common.metadata.{OperatorGroupConstants, OperatorInfo}
 import edu.uci.ics.texera.workflow.common.operators.LogicalOp
 import edu.uci.ics.texera.workflow.common.tuple.schema.{OperatorSchemaInfo, Schema}
+import edu.uci.ics.amber.engine.common.workflow.{InputPort, OutputPort, PortIdentity}
 
 class UnionOpDesc extends LogicalOp {
 
@@ -33,7 +29,7 @@ class UnionOpDesc extends LogicalOp {
       "Union",
       "Unions the output rows from multiple input operators",
       OperatorGroupConstants.UTILITY_GROUP,
-      inputPorts = List(InputPort(allowMultiInputs = true)),
+      inputPorts = List(InputPort(PortIdentity(0), allowMultiLinks = true)),
       outputPorts = List(OutputPort())
     )
 

core/amber/src/main/scala/edu/uci/ics/texera/workflow/operators/unneststring/UnnestStringOpDesc.scala
@@ -5,15 +5,11 @@ import com.google.common.base.Preconditions
 import edu.uci.ics.amber.engine.architecture.deploysemantics.PhysicalOp
 import edu.uci.ics.amber.engine.architecture.deploysemantics.layer.OpExecInitInfo
 import edu.uci.ics.amber.engine.common.virtualidentity.{ExecutionIdentity, WorkflowIdentity}
-import edu.uci.ics.texera.workflow.common.metadata.{
-  InputPort,
-  OperatorGroupConstants,
-  OperatorInfo,
-  OutputPort
-}
+import edu.uci.ics.texera.workflow.common.metadata.{OperatorGroupConstants, OperatorInfo}
 import edu.uci.ics.texera.workflow.common.metadata.annotations.AutofillAttributeName
 import edu.uci.ics.texera.workflow.common.operators.flatmap.FlatMapOpDesc
 import edu.uci.ics.texera.workflow.common.tuple.schema.{AttributeType, OperatorSchemaInfo, Schema}
+import edu.uci.ics.amber.engine.common.workflow.{InputPort, OutputPort}
 
 class UnnestStringOpDesc extends FlatMapOpDesc {
   @JsonProperty(value = "Delimiter", required = true, defaultValue = ",")

core/amber/src/main/scala/edu/uci/ics/texera/workflow/operators/visualization/htmlviz/HtmlVizOpDesc.scala
@@ -6,25 +6,18 @@ import edu.uci.ics.amber.engine.architecture.deploysemantics.PhysicalOp
 import edu.uci.ics.amber.engine.architecture.deploysemantics.layer.OpExecInitInfo
 import edu.uci.ics.amber.engine.common.virtualidentity.{ExecutionIdentity, WorkflowIdentity}
 import edu.uci.ics.texera.workflow.common.metadata.annotations.AutofillAttributeName
-import edu.uci.ics.texera.workflow.common.metadata.{
-  InputPort,
-  OperatorGroupConstants,
-  OperatorInfo,
-  OutputPort
-}
+import edu.uci.ics.texera.workflow.common.metadata.{OperatorGroupConstants, OperatorInfo}
 import edu.uci.ics.texera.workflow.common.tuple.schema.{
   Attribute,
   AttributeType,
   OperatorSchemaInfo,
   Schema
 }
+import edu.uci.ics.amber.engine.common.workflow.{InputPort, OutputPort}
 import edu.uci.ics.texera.workflow.operators.visualization.{
   VisualizationConstants,
   VisualizationOperator
 }
-
-import java.util.Collections.singletonList
-import scala.collection.JavaConverters.asScalaBuffer
 
 /**
   * HTML Visualization operator to render any given HTML code
@@ -54,8 +47,8 @@ class HtmlVizOpDesc extends VisualizationOperator {
       "HTML visualizer",
       "Render the result of HTML content",
       OperatorGroupConstants.VISUALIZATION_GROUP,
-      asScalaBuffer(singletonList(InputPort(""))).toList,
-      asScalaBuffer(singletonList(OutputPort(""))).toList
+      inputPorts = List(InputPort()),
+      outputPorts = List(OutputPort())
     )
 
   override def getOutputSchema(schemas: Array[Schema]): Schema =

core/amber/src/main/scala/edu/uci/ics/texera/workflow/operators/visualization/urlviz/UrlVizOpDesc.scala
@@ -6,25 +6,18 @@ import edu.uci.ics.amber.engine.architecture.deploysemantics.PhysicalOp
 import edu.uci.ics.amber.engine.architecture.deploysemantics.layer.OpExecInitInfo
 import edu.uci.ics.amber.engine.common.virtualidentity.{ExecutionIdentity, WorkflowIdentity}
 import edu.uci.ics.texera.workflow.common.metadata.annotations.AutofillAttributeName
-import edu.uci.ics.texera.workflow.common.metadata.{
-  InputPort,
-  OperatorGroupConstants,
-  OperatorInfo,
-  OutputPort
-}
+import edu.uci.ics.texera.workflow.common.metadata.{OperatorGroupConstants, OperatorInfo}
 import edu.uci.ics.texera.workflow.common.tuple.schema.{
   Attribute,
   AttributeType,
   OperatorSchemaInfo,
   Schema
 }
+import edu.uci.ics.amber.engine.common.workflow.{InputPort, OutputPort}
 import edu.uci.ics.texera.workflow.operators.visualization.{
   VisualizationConstants,
   VisualizationOperator
 }
-
-import java.util.Collections.singletonList
-import scala.collection.JavaConverters.asScalaBuffer
 
 /**
   * URL Visualization operator to render any content in given URL link
@@ -66,8 +59,8 @@ class UrlVizOpDesc extends VisualizationOperator {
       "URL visualizer",
       "Render the content of URL",
       OperatorGroupConstants.VISUALIZATION_GROUP,
-      asScalaBuffer(singletonList(InputPort(""))).toList,
-      asScalaBuffer(singletonList(OutputPort(""))).toList
+      inputPorts = List(InputPort()),
+      outputPorts = List(OutputPort())
     )
 
   override def getOutputSchema(schemas: Array[Schema]): Schema =

core/new-gui/src/app/workspace/service/workflow-graph/model/workflow-graph.ts
@@ -572,6 +572,7 @@ export class WorkflowGraph {
     }
     const yoperator = this.sharedModel.operatorIDMap.get(operatorID) as YType<OperatorPredicate>;
     const operator = yoperator.toJSON();
+    console.log("found this op predicate", operator);
     return operator;
   }
 
@@ -836,7 +837,9 @@ export class WorkflowGraph {
     );
     portDescriptionSharedType.set(
       "dependencies",
-      createYTypeFromObject<Array<number>>((newProperty as PortProperty).dependencies) as unknown as Y.Array<number>
+      createYTypeFromObject<Array<{ id: number; internal: boolean }>>(
+        (newProperty as PortProperty).dependencies
+      ) as unknown as Y.Array<number>
     );
   }
 

core/new-gui/src/app/workspace/service/workflow-graph/util/workflow-util.service.ts
@@ -126,6 +126,7 @@ export class WorkflowUtilService {
         displayName: portInfo.displayName ?? "",
         allowMultiInputs: portInfo.allowMultiInputs ?? false,
         isDynamicPort: false,
+        dependencies: portInfo.dependencies ?? [],
       });
     }
 
@@ -167,5 +168,31 @@ export class WorkflowUtilService {
       workflow.content = jsonCast<WorkflowContent>(workflow.content);
     }
     return workflow;
+  }
+
+  public updateOperatorVersion(op: OperatorPredicate) {
+    const operatorType = op.operatorType;
+    const operatorSchema = this.operatorSchemaList.find(schema => schema.operatorType === operatorType);
+    if (operatorSchema === undefined) {
+      throw new Error(`operatorType ${operatorType} doesn't exist in operator metadata`);
+    }
+    // if (operatorSchema.operatorVersion!== op.operatorVersion) {
+    const inputPorts = [];
+    for (let i = 0; i < operatorSchema.additionalMetadata.inputPorts.length; i++) {
+      const portID = "input-" + i.toString();
+      const portInfo = operatorSchema.additionalMetadata.inputPorts[i];
+      inputPorts.push({
+        portID,
+        displayName: portInfo.displayName ?? "",
+        allowMultiInputs: portInfo.allowMultiInputs ?? false,
+        isDynamicPort: false,
+        dependencies: portInfo.dependencies ?? [],
+      });
+    }
+    return {
+      ...op,
+      operatorVersion: operatorSchema.operatorVersion,
+      inputPorts,
+    };
   }
 }

core/new-gui/src/app/workspace/types/operator-schema.interface.ts
@@ -14,6 +14,7 @@ export interface InputPortInfo
   extends Readonly<{
     displayName?: string;
     allowMultiInputs?: boolean;
+    dependencies?: { id: number; internal: boolean }[];
   }> {}
 
 export interface OutputPortInfo

commit 44359540ec88469cb4e0a84622eda74a0f8e7dff

Fix missing ports on Scala operator and allowMultiLinks (#2315)
This PR fixes 
1. the missed ports of scala operators, caused by #2299 
2. the `allowMultiLinks` parameter is mismatched on the frontend.

core/amber/src/main/scala/edu/uci/ics/texera/workflow/operators/source/scan/csv/ParallelCSVScanSourceOpDesc.scala
@@ -61,6 +61,8 @@ class ParallelCSVScanSourceOpDesc extends ScanSourceOpDesc {
               )
             })
           )
+          .withInputPorts(operatorInfo.inputPorts, inputPortToSchemaMapping)
+          .withOutputPorts(operatorInfo.outputPorts, outputPortToSchemaMapping)
           .withParallelizable(true)
 
       case None =>

core/amber/src/main/scala/edu/uci/ics/texera/workflow/operators/union/UnionOpDesc.scala
@@ -15,12 +15,15 @@ class UnionOpDesc extends LogicalOp {
       workflowId: WorkflowIdentity,
       executionId: ExecutionIdentity
   ): PhysicalOp = {
-    PhysicalOp.oneToOnePhysicalOp(
-      workflowId,
-      executionId,
-      operatorIdentifier,
-      OpExecInitInfo((_, _, _) => new UnionOpExec())
-    )
+    PhysicalOp
+      .oneToOnePhysicalOp(
+        workflowId,
+        executionId,
+        operatorIdentifier,
+        OpExecInitInfo((_, _, _) => new UnionOpExec())
+      )
+      .withInputPorts(operatorInfo.inputPorts, inputPortToSchemaMapping)
+      .withOutputPorts(operatorInfo.outputPorts, outputPortToSchemaMapping)
   }
 
   override def operatorInfo: OperatorInfo =

core/amber/src/main/scala/edu/uci/ics/texera/workflow/operators/unneststring/UnnestStringOpDesc.scala
@@ -39,12 +39,15 @@ class UnnestStringOpDesc extends FlatMapOpDesc {
       workflowId: WorkflowIdentity,
       executionId: ExecutionIdentity
   ): PhysicalOp = {
-    PhysicalOp.oneToOnePhysicalOp(
-      workflowId,
-      executionId,
-      operatorIdentifier,
-      OpExecInitInfo((_, _, _) => new UnnestStringOpExec(this))
-    )
+    PhysicalOp
+      .oneToOnePhysicalOp(
+        workflowId,
+        executionId,
+        operatorIdentifier,
+        OpExecInitInfo((_, _, _) => new UnnestStringOpExec(this))
+      )
+      .withInputPorts(operatorInfo.inputPorts, inputPortToSchemaMapping)
+      .withOutputPorts(operatorInfo.outputPorts, outputPortToSchemaMapping)
   }
 
   override def getOutputSchema(schemas: Array[Schema]): Schema = {

core/amber/src/main/scala/edu/uci/ics/texera/workflow/operators/visualization/htmlviz/HtmlVizOpDesc.scala
@@ -31,12 +31,15 @@ class HtmlVizOpDesc extends VisualizationOperator {
   ): PhysicalOp = {
     val outputSchema =
       operatorInfo.outputPorts.map(outputPort => outputPortToSchemaMapping(outputPort.id)).head
-    PhysicalOp.oneToOnePhysicalOp(
-      workflowId,
-      executionId,
-      operatorIdentifier,
-      OpExecInitInfo((_, _, _) => new HtmlVizOpExec(htmlContentAttrName, outputSchema))
-    )
+    PhysicalOp
+      .oneToOnePhysicalOp(
+        workflowId,
+        executionId,
+        operatorIdentifier,
+        OpExecInitInfo((_, _, _) => new HtmlVizOpExec(htmlContentAttrName, outputSchema))
+      )
+      .withInputPorts(operatorInfo.inputPorts, inputPortToSchemaMapping)
+      .withOutputPorts(operatorInfo.outputPorts, outputPortToSchemaMapping)
   }
 
   override def operatorInfo: OperatorInfo =

core/amber/src/main/scala/edu/uci/ics/texera/workflow/operators/visualization/urlviz/UrlVizOpDesc.scala
@@ -49,6 +49,8 @@ class UrlVizOpDesc extends VisualizationOperator {
         operatorIdentifier,
         OpExecInitInfo((_, _, _) => new UrlVizOpExec(urlContentAttrName, outputSchema))
       )
+      .withInputPorts(operatorInfo.inputPorts, inputPortToSchemaMapping)
+      .withOutputPorts(operatorInfo.outputPorts, outputPortToSchemaMapping)
   }
 
   override def operatorInfo: OperatorInfo =

core/new-gui/src/app/workspace/component/workflow-editor/workflow-editor.component.ts
@@ -1222,7 +1222,7 @@ export class WorkflowEditorComponent implements AfterViewInit, OnDestroy {
       if (portIndex >= 0) {
         const portInfo =
           this.dynamicSchemaService.getDynamicSchema(targetCellID).additionalMetadata.inputPorts[portIndex];
-        allowMultiInput = portInfo?.allowMultiInputs ?? false;
+        allowMultiInput = portInfo?.allowMultiLinks ?? false;
       }
     }
     return !(connectedLinksToTargetPort.length > 0 && !allowMultiInput);

core/new-gui/src/app/workspace/service/operator-metadata/mock-operator-metadata.data.ts
@@ -245,7 +245,7 @@ export const mockUnionSchema: OperatorSchema = {
     userFriendlyName: "Union",
     operatorDescription: "Union multiple inputs",
     operatorGroupName: "Analysis",
-    inputPorts: [{ allowMultiInputs: true }],
+    inputPorts: [{ allowMultiLinks: true }],
     outputPorts: [{}],
   },
   operatorVersion: "union1",

core/new-gui/src/app/workspace/service/workflow-graph/model/workflow-graph.ts
@@ -2,18 +2,18 @@ import { Observable, Subject } from "rxjs";
 import {
   Breakpoint,
   Comment,
-  PortDescription,
   CommentBox,
-  OperatorLink,
   LogicalPort,
+  OperatorLink,
   OperatorPredicate,
   PartitionInfo,
+  PortDescription,
   PortProperty,
 } from "../../../types/workflow-common.interface";
 import { isEqual } from "lodash-es";
 import { SharedModel } from "./shared-model";
-import { User, CoeditorState } from "../../../../common/type/user";
-import { createYTypeFromObject, YType, updateYTypeFromObject } from "../../../types/shared-editing.interface";
+import { CoeditorState, User } from "../../../../common/type/user";
+import { createYTypeFromObject, updateYTypeFromObject, YType } from "../../../types/shared-editing.interface";
 import { Awareness } from "y-protocols/awareness";
 import * as Y from "yjs";
 
@@ -571,9 +571,7 @@ export class WorkflowGraph {
       throw new Error(`operator ${operatorID} does not exist`);
     }
     const yoperator = this.sharedModel.operatorIDMap.get(operatorID) as YType<OperatorPredicate>;
-    const operator = yoperator.toJSON();
-    console.log("found this op predicate", operator);
-    return operator;
+    return yoperator.toJSON();
   }
 
   /**

core/new-gui/src/app/workspace/service/workflow-graph/util/workflow-util.service.ts
@@ -124,7 +124,7 @@ export class WorkflowUtilService {
       inputPorts.push({
         portID,
         displayName: portInfo.displayName ?? "",
-        allowMultiInputs: portInfo.allowMultiInputs ?? false,
+        allowMultiInputs: portInfo.allowMultiLinks ?? false,
         isDynamicPort: false,
         dependencies: portInfo.dependencies ?? [],
       });
@@ -184,7 +184,7 @@ export class WorkflowUtilService {
       inputPorts.push({
         portID,
         displayName: portInfo.displayName ?? "",
-        allowMultiInputs: portInfo.allowMultiInputs ?? false,
+        allowMultiInputs: portInfo.allowMultiLinks ?? false,
         isDynamicPort: false,
         dependencies: portInfo.dependencies ?? [],
       });

core/new-gui/src/app/workspace/types/operator-schema.interface.ts
@@ -13,7 +13,7 @@ import { CustomJSONSchema7 } from "./custom-json-schema.interface";
 export interface InputPortInfo
   extends Readonly<{
     displayName?: string;
-    allowMultiInputs?: boolean;
+    allowMultiLinks?: boolean;
     dependencies?: { id: number; internal: boolean }[];
   }> {}
 