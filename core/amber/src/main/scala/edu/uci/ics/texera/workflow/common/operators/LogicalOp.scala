ad10c353c27ac9c18d5e2acca99526b263f76925
Ran Ran <140530839+SlowlyRan@users.noreply.github.com>
2024-02-26 19:16:42 -0800

    Add Scattermatrix Visualization Operator (#2382)
    This pull request introduces a new feature: the Scatter Matrix
    Visualization Operator.
    This operator enables users to generate a scatter matrix, facilitating
    exploration of relationships between multiple attributes within a
    dataset. Additionally, users have the ability to color the points by
    category.
    For implementation details, this operator is a python-native operator.
    It accepts the user-specified data and generate corresponding python
    script. The figure is created with plotly express. More details about
    the scatterplot matrix drawing is available
    [here](https://plotly.com/python/splom/)
    Here is the demo:
    
    ![ScatterMatrix](https://github.com/Texera/texera/assets/140530839/6b2596bd-22a6-4cdc-9e76-dc4381fc80b4)

--- core/amber/src/main/scala/edu/uci/ics/texera/workflow/common/operators/LogicalOp.scala
+++ core/amber/src/main/scala/edu/uci/ics/texera/workflow/common/operators/LogicalOp.scala
@@ -78,6 +78,7 @@ import edu.uci.ics.texera.workflow.operators.visualization.dumbbellPlot.Dumbbell
 import edu.uci.ics.texera.workflow.operators.visualization.heatMap.HeatMapOpDesc
 import edu.uci.ics.texera.workflow.operators.visualization.lineChart.LineChartOpDesc
 import edu.uci.ics.texera.workflow.operators.visualization.scatter3DChart.Scatter3dChartOpDesc
+import edu.uci.ics.texera.workflow.operators.visualization.ScatterMatrixChart.ScatterMatrixChartOpDesc
 import edu.uci.ics.texera.workflow.operators.visualization.funnelPlot.FunnelPlotOpDesc
 import org.apache.commons.lang3.builder.{EqualsBuilder, HashCodeBuilder, ToStringBuilder}
 import org.apache.zookeeper.KeeperException.UnimplementedException
@@ -162,6 +163,7 @@ trait StateTransferFunc
     new Type(value = classOf[HierarchyChartOpDesc], name = "HierarchyChart"),
     new Type(value = classOf[DumbbellPlotOpDesc], name = "DumbbellPlot"),
     new Type(value = classOf[BoxPlotOpDesc], name = "BoxPlot"),
+    new Type(value = classOf[ScatterMatrixChartOpDesc], name = "ScatterMatrixChart"),
     new Type(value = classOf[HeatMapOpDesc], name = "HeatMap"),
     new Type(value = classOf[Scatter3dChartOpDesc], name = "Scatter3DChart"),
     new Type(value = classOf[FunnelPlotOpDesc], name = "FunnelPlot")
