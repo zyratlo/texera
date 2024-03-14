zyratlo/texera

commit 9cfcd7a35153ceaaa14bc24d814014b2ddcc3e51

Add onFinish to Scala/Java operators (#2463)
This PR aligns the Scala/Java Executor API design with the Python side.
Now Scala operators have the following APIs:

```
  def processTuple(tuple: Tuple, port: Int): Iterator[TupleLike]

  def onFinish(port: Int): Iterator[TupleLike] = Iterator.empty
```
There are also corresponding `processTupleMultiPort` and
`onFinishMultiPort`, which are only used by SplitOp. Those are to be
refactored.

For all existing Java/Scala Executors, we update the implementations to
use this API.

core/amber/src/main/scala/edu/uci/ics/texera/workflow/operators/sink/managed/ProgressiveSinkOpExec.scala
@@ -1,39 +1,26 @@
 package edu.uci.ics.texera.workflow.operators.sink.managed
 
-import edu.uci.ics.amber.engine.common.{ISinkOperatorExecutor, InputExhausted}
+import edu.uci.ics.amber.engine.common.SinkOperatorExecutor
 import edu.uci.ics.texera.workflow.common.IncrementalOutputMode._
 import edu.uci.ics.texera.workflow.common.tuple.Tuple
 import edu.uci.ics.texera.workflow.common.{IncrementalOutputMode, ProgressiveUtils}
 import edu.uci.ics.texera.workflow.operators.sink.storage.SinkStorageWriter
 
-class ProgressiveSinkOpExec(
-    outputMode: IncrementalOutputMode,
-    storage: SinkStorageWriter
-) extends ISinkOperatorExecutor {
-
-  override def open(): Unit = storage.open()
-
-  override def close(): Unit = storage.close()
-
-  override def consume(
-      tuple: Either[Tuple, InputExhausted],
+class ProgressiveSinkOpExec(outputMode: IncrementalOutputMode, storage: SinkStorageWriter)
+    extends SinkOperatorExecutor {
+  override def consumeTuple(
+      tuple: Tuple,
       input: Int
   ): Unit = {
-    tuple match {
-      case Left(t) =>
-        outputMode match {
-          case SET_SNAPSHOT =>
-            updateSetSnapshot(t)
-          case SET_DELTA =>
-            storage.putOne(t)
-        }
-      case Right(_) => // skip
+    outputMode match {
+      case SET_SNAPSHOT => updateSetSnapshot(tuple)
+      case SET_DELTA    => storage.putOne(tuple)
     }
   }
 
   private def updateSetSnapshot(deltaUpdate: Tuple): Unit = {
-    val (isInsertion, tupleValue) =
-      ProgressiveUtils.getTupleFlagAndValue(deltaUpdate)
+    val (isInsertion, tupleValue) = ProgressiveUtils.getTupleFlagAndValue(deltaUpdate)
+
     if (isInsertion) {
       storage.putOne(tupleValue)
     } else {

commit 0c4f4e09ef681fd28404bae99a45e38771507106

Fix initialization issue for Mongo sink storage  (#2471)
This PR fixed a bug introduced by #2463 where the open/close calls of
the sink operator are accidentally being removed.

core/amber/src/main/scala/edu/uci/ics/texera/workflow/operators/sink/managed/ProgressiveSinkOpExec.scala
@@ -8,6 +8,11 @@ import edu.uci.ics.texera.workflow.operators.sink.storage.SinkStorageWriter
 
 class ProgressiveSinkOpExec(outputMode: IncrementalOutputMode, storage: SinkStorageWriter)
     extends SinkOperatorExecutor {
+
+  override def open(): Unit = {
+    storage.open()
+  }
+
   override def consumeTuple(
       tuple: Tuple,
       input: Int
@@ -26,6 +31,10 @@ class ProgressiveSinkOpExec(outputMode: IncrementalOutputMode, storage: SinkStor
     } else {
       storage.removeOne(tupleValue)
     }
+  }
+
+  override def close(): Unit = {
+    storage.close()
   }
 
 }