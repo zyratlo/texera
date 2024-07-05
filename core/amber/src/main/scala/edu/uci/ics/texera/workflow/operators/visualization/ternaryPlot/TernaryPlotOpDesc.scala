2fee4dffc58101cbf3ea31ebb59652d72dd7f6bc
Ryan Zhang <97552093+zyratlo@users.noreply.github.com>
2024-07-04 22:14:02 -0700

    Add Ternary Plot Visualization Operator (#2710)
    # Purpose
    Implements the Ternary Plot Python native operator.
    
    # Description
    A ternary plot, also known as a ternary graph or triangular plot,
    represents the proportions of three variables that sum to a constant,
    typically 100%. The plot is shaped like an equilateral triangle, with
    each vertex representing one of the three variables and points within
    the triangle representing different ratios of the three.
    
    The ternary plot was implemented using the Plotly library in Python;
    more details can be found
    [here](https://plotly.com/python/ternary-plots/).
    
    # Changes
    Three files were updated/added:
    - TernaryPlotOpDesc.scala: implements the ternary plot visualization
    operator as a Python native operator
    - LogicalOp.scala: registers ternary plot
    - TernaryPlot.png: the icon used in the front end for ternary plot
    
    # Demo
    A screenshot of the ternary plot in the GUI is below:
    
    ![image](https://github.com/Texera/texera/assets/97552093/af74d0e2-7313-4362-a3e9-5de6c4f38f60)
    
    ---------
    
    Co-authored-by: Xinyuan Lin <xinyual3@uci.edu>

---
+++ core/amber/src/main/scala/edu/uci/ics/texera/workflow/operators/visualization/ternaryPlot/TernaryPlotOpDesc.scala
@@ -0,0 +1,125 @@
+package edu.uci.ics.texera.workflow.operators.visualization.ternaryPlot
+
+import com.fasterxml.jackson.annotation.{JsonProperty, JsonPropertyDescription}
+import com.kjetland.jackson.jsonSchema.annotations.JsonSchemaTitle
+import edu.uci.ics.texera.workflow.common.metadata.{OperatorGroupConstants, OperatorInfo}
+import edu.uci.ics.texera.workflow.common.metadata.annotations.AutofillAttributeName
+import edu.uci.ics.texera.workflow.common.operators.PythonOperatorDescriptor
+import edu.uci.ics.texera.workflow.common.tuple.schema.{Attribute, AttributeType, Schema}
+import edu.uci.ics.amber.engine.common.workflow.{InputPort, OutputPort}
+import edu.uci.ics.texera.workflow.operators.visualization.{
+  VisualizationConstants,
+  VisualizationOperator
+}
+
+/**
+  * Visualization Operator for Ternary Plots.
+  *
+  * This operator uses three data fields to construct a ternary plot.
+  * The points can optionally be color coded using a data field.
+  */
+
+class TernaryPlotOpDesc extends VisualizationOperator with PythonOperatorDescriptor {
+
+  // Add annotations for the first variable
+  @JsonProperty(value = "firstVariable", required = true)
+  @JsonSchemaTitle("Variable 1")
+  @JsonPropertyDescription("First variable data field")
+  @AutofillAttributeName var firstVariable: String = ""
+
+  // Add annotations for the second variable
+  @JsonProperty(value = "secondVariable", required = true)
+  @JsonSchemaTitle("Variable 2")
+  @JsonPropertyDescription("Second variable data field")
+  @AutofillAttributeName var secondVariable: String = ""
+
+  // Add annotations for the third variable
+  @JsonProperty(value = "thirdVariable", required = true)
+  @JsonSchemaTitle("Variable 3")
+  @JsonPropertyDescription("Third variable data field")
+  @AutofillAttributeName var thirdVariable: String = ""
+
+  // Add annotations for enabling color and selecting its associated data field
+  @JsonProperty(value = "colorEnabled", defaultValue = "false")
+  @JsonSchemaTitle("Categorize by Color")
+  @JsonPropertyDescription("Optionally color points using a data field")
+  var colorEnabled: Boolean = false
+
+  @JsonProperty(value = "colorDataField", required = false)
+  @JsonSchemaTitle("Color Data Field")
+  @JsonPropertyDescription("Specify the data field to color")
+  @AutofillAttributeName var colorDataField: String = ""
+
+  // Register chart type as a visualization operator
+  override def chartType: String = VisualizationConstants.HTML_VIZ
+
+  // OperatorInfo instance describing ternary plot
+  override def operatorInfo: OperatorInfo =
+    OperatorInfo(
+      userFriendlyName = "Ternary Plot",
+      operatorDescription = "Points are graphed on a Ternary Plot using 3 specified data fields",
+      operatorGroupName = OperatorGroupConstants.VISUALIZATION_GROUP,
+      inputPorts = List(InputPort()),
+      outputPorts = List(OutputPort())
+    )
+
+  /** Returns the output schema set as html-content */
+  override def getOutputSchema(schemas: Array[Schema]): Schema = {
+    Schema.builder().add(new Attribute("html-content", AttributeType.STRING)).build()
+  }
+
+  /** Returns a Python string that drops any tuples with missing values */
+  def manipulateTable(): String = {
+    // Check for any empty data field names
+    assert(firstVariable.nonEmpty && secondVariable.nonEmpty && thirdVariable.nonEmpty)
+    s"""
+       |        # Remove any tuples that contain missing values
+       |        table.dropna(subset=['$firstVariable', '$secondVariable', '$thirdVariable'], inplace = True)
+       |""".stripMargin
+  }
+
+  /** Returns a Python string that creates the ternary plot figure */
+  def createPlotlyFigure(): String = {
+    s"""
+       |        if '$colorEnabled' == 'true' and '$colorDataField' != "":
+       |            fig = px.scatter_ternary(table, a='$firstVariable', b='$secondVariable', c='$thirdVariable', color='$colorDataField')
+       |        else:
+       |            fig = px.scatter_ternary(table, a='$firstVariable', b='$secondVariable', c='$thirdVariable')
+       |""".stripMargin
+  }
+
+  /** Returns a Python string that yields the html content of the ternary plot */
+  override def generatePythonCode(): String = {
+    val finalCode =
+      s"""
+         |from pytexera import *
+         |
+         |import plotly.express as px
+         |import plotly.io
+         |
+         |class ProcessTableOperator(UDFTableOperator):
+         |
+         |    # Generate custom error message as html string
+         |    def render_error(self, error_msg):
+         |        return '''<h1>TernaryPlot is not available.</h1>
+         |                  <p>Reasons are: {} </p>
+         |               '''.format(error_msg)
+         |
+         |    @overrides
+         |    def process_table(self, table: Table, port: int) -> Iterator[Optional[TableLike]]:
+         |        if table.empty:
+         |            yield {'html-content': self.render_error("Input table is empty.")}
+         |            return
+         |        ${manipulateTable()}
+         |        if table.empty:
+         |            yield {'html-content': self.render_error("No valid rows left (every row has at least 1 missing value).")}
+         |            return
+         |        ${createPlotlyFigure()}
+         |        # Convert fig to html content
+         |        html = plotly.io.to_html(fig, include_plotlyjs = 'cdn', auto_play = False)
+         |        yield {'html-content':html}
+         |""".stripMargin
+    finalCode
+  }
+
+}
