ca68ad6587b800f05a879c7c29e874b63f9cb6cc
Paul Schatt <pschatt789@gmail.com>
2025-01-24 20:53:38 -0800

    Disable User System in front end config by default (#3228)
    ### Purpose:
    Fix a mistake due to a misunderstanding. userSystemEnabled flag should
    be set to false by default.
    
    Co-authored-by: Texera <texera@MacBook-Air.local>

--- core/gui/src/environments/environment.default.ts
+++ core/gui/src/environments/environment.default.ts
@@ -26,7 +26,7 @@ export const defaultEnvironment = {
   /**
    * whether user system is enabled
    */
-  userSystemEnabled: true,
+  userSystemEnabled: false,
 
   /**
    * whether local login is enabled
