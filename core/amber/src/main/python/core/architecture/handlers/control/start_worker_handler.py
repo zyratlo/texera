zyratlo/texera

commit 74da6167bdbd92a854bd88a9416851231ac8f86a

Add ChannelMarker support on pyAmber (#3245)
This PR:
1. Added ChannelMarkerManager to track and align ChannelMarkers on the
Python side.
2. Now messages to Python also attach ChannelIdentity to distinguish
different channels, previously we used ActorVirtualIdentity.
3. Due to the ChannelIdentity change, the generated proto message on
Python side has to assign the `is_control` field explicitly, otherwise
its hash is not consistent.
To give an example:
```
channel_id = ChannelIdentity(from_worker_id = ..., to_worker_id = ...)
new_channel_id = bytes(channel_id)
assert hash(channel_id) == hash(new_channel_id) // this will fail.
```
4. The InputQueue now also uses channel ID as a key, instead of using 2
fixed keys: CONTROL, DATA.

---------

Co-authored-by: Xinyuan Lin <xinyual3@uci.edu>

core/amber/src/main/python/core/architecture/handlers/control/start_worker_handler.py
@@ -1,3 +1,4 @@
+from proto.edu.uci.ics.amber.core import ChannelIdentity, ActorVirtualIdentity
 from proto.edu.uci.ics.amber.engine.architecture.rpc import (
     WorkerStateResponse,
     EmptyRequest,
@@ -19,7 +20,14 @@ class StartWorkerHandler(ControlHandler):
         if self.context.executor_manager.executor.is_source:
             self.context.state_manager.transit_to(WorkerState.RUNNING)
             self.context.input_queue.put(
-                DataElement(tag=InputManager.SOURCE_STARTER, payload=None)
+                DataElement(
+                    tag=ChannelIdentity(
+                        InputManager.SOURCE_STARTER,
+                        ActorVirtualIdentity(self.context.worker_id),
+                        False,
+                    ),
+                    payload=None,
+                )
             )
         state = self.context.state_manager.get_current_state()
         return WorkerStateResponse(state)

core/amber/src/main/protobuf/edu/uci/ics/amber/engine/common/ambermessage.proto
@@ -21,11 +21,11 @@ message ControlPayloadV2 {
 }
 
 message PythonDataHeader {
-  core.ActorVirtualIdentity tag = 1 [(scalapb.field).no_box = true];
+  core.ChannelIdentity tag = 1 [(scalapb.field).no_box = true];
   string payload_type = 2;
 }
 
 message PythonControlMessage {
-  core.ActorVirtualIdentity tag = 1 [(scalapb.field).no_box = true];
+  core.ChannelIdentity tag = 1 [(scalapb.field).no_box = true];
   ControlPayloadV2 payload = 2 [(scalapb.field).no_box = true];
 }

core/amber/src/main/python/core/architecture/handlers/actorcommand/backpressure_handler.py
@@ -15,7 +15,7 @@ from proto.edu.uci.ics.amber.engine.common import (
     Backpressure,
     ControlPayloadV2,
 )
-from proto.edu.uci.ics.amber.core import ActorVirtualIdentity
+from proto.edu.uci.ics.amber.core import ActorVirtualIdentity, ChannelIdentity
 
 
 class BackpressureHandler(ActorCommandHandler):
@@ -30,7 +30,9 @@ class BackpressureHandler(ActorCommandHandler):
             input_queue.enable_data(InternalQueue.DisableType.DISABLE_BY_BACKPRESSURE)
             input_queue.put(
                 ControlElement(
-                    tag=ActorVirtualIdentity(""),
+                    tag=ChannelIdentity(
+                        ActorVirtualIdentity("self"), ActorVirtualIdentity("self"), True
+                    ),
                     payload=set_one_of(
                         ControlPayloadV2,
                         ControlInvocation(

core/amber/src/main/python/core/architecture/handlers/control/add_input_channel_handler.py
@@ -7,5 +7,10 @@ from proto.edu.uci.ics.amber.engine.architecture.rpc import (
 
 class AddInputChannelHandler(ControlHandler):
     async def add_input_channel(self, req: AddInputChannelRequest) -> EmptyReturn:
+        if not req.channel_id.is_control:
+            # Explicitly set is_control to trigger lazy computation.
+            # If not set, it may be computed at different times,
+            # causing hash inconsistencies.
+            req.channel_id.is_control = False
         self.context.input_manager.register_input(req.channel_id, req.port_id)
         return EmptyReturn()

core/amber/src/main/python/core/architecture/managers/channel_marker_manager.py
@@ -1,0 +1,64 @@
+from collections import defaultdict
+from typing import Set, Dict
+
+from core.architecture.packaging.input_manager import Channel
+from proto.edu.uci.ics.amber.core import ActorVirtualIdentity, ChannelIdentity
+from proto.edu.uci.ics.amber.engine.architecture.rpc import (
+    ChannelMarkerPayload,
+    ChannelMarkerType,
+)
+
+
+class ChannelMarkerManager:
+    def __init__(self, actor_id: ActorVirtualIdentity, input_gateway):
+        self.actor_id = actor_id
+        self.input_gateway = input_gateway
+        self.marker_received: Dict[str, Set[ChannelIdentity]] = defaultdict(set)
+
+    def is_marker_aligned(
+        self, from_channel: ChannelIdentity, marker: ChannelMarkerPayload
+    ) -> bool:
+        """
+        Checks whether a channel marker has been received from all expected
+        input channels, determining whether further processing can proceed.
+
+        Args:
+            from_channel (ChannelIdentity): The channel from which the marker
+                was received.
+            marker (ChannelMarkerPayload): The marker payload containing its
+                type and scope.
+
+        Returns:
+            bool: True if the marker is considered aligned and processing can
+                  continue, False otherwise.
+        """
+        marker_id = marker.id
+        self.marker_received[marker_id].add(from_channel)
+
+        marker_received_from_all_channels = self.get_channels_within_scope(
+            marker
+        ).issubset(self.marker_received[marker_id])
+
+        if marker.marker_type == ChannelMarkerType.REQUIRE_ALIGNMENT:
+            epoch_marker_completed = marker_received_from_all_channels
+        elif marker.marker_type == ChannelMarkerType.NO_ALIGNMENT:
+            epoch_marker_completed = (
+                len(self.marker_received[marker_id]) == 1
+            )  # Only the first marker triggers
+        else:
+            raise ValueError(f"Unsupported marker type: {marker.marker_type}")
+
+        if marker_received_from_all_channels:
+            del self.marker_received[marker_id]  # Clean up if all markers are received
+
+        return epoch_marker_completed
+
+    def get_channels_within_scope(
+        self, marker: ChannelMarkerPayload
+    ) -> Dict["ChannelIdentity", "Channel"].keys:
+        upstreams = {
+            channel_id
+            for channel_id in marker.scope
+            if channel_id.to_worker_id == self.actor_id
+        }
+        return self.input_gateway.get_all_channel_ids() & upstreams

core/amber/src/main/python/core/architecture/managers/context.py
@@ -1,5 +1,8 @@
+from proto.edu.uci.ics.amber.core import ActorVirtualIdentity, ChannelIdentity
 from proto.edu.uci.ics.amber.engine.architecture.worker import WorkerState
+from typing import Optional
 from .console_message_manager import ConsoleMessageManager
+from .channel_marker_manager import ChannelMarkerManager
 from .debug_manager import DebugManager
 from .exception_manager import ExceptionManager
 from .marker_processing_manager import MarkerProcessingManager
@@ -26,6 +29,7 @@ class Context:
         self.worker_id = worker_id
         self.input_queue: InternalQueue = input_queue
         self.executor_manager = ExecutorManager()
+        self.current_input_channel_id: Optional[ChannelIdentity] = None
         self.tuple_processing_manager = TupleProcessingManager()
         self.marker_processing_manager = MarkerProcessingManager()
         self.exception_manager = ExceptionManager()
@@ -46,6 +50,9 @@ class Context:
         )
         self.output_manager = OutputManager(worker_id)
         self.input_manager = InputManager()
+        self.channel_marker_manager = ChannelMarkerManager(
+            ActorVirtualIdentity(worker_id), self.input_manager
+        )
         self.console_message_manager = ConsoleMessageManager()
         self.debug_manager = DebugManager(
             self.tuple_processing_manager.context_switch_condition

core/amber/src/main/python/core/architecture/managers/pause_manager.py
@@ -1,13 +1,13 @@
 from collections import defaultdict
 from enum import Enum
 
-from typing import Set, Dict, List
+from typing import Set, Dict
 
 from loguru import logger
 
 from . import state_manager
 from proto.edu.uci.ics.amber.engine.architecture.worker import WorkerState
-from proto.edu.uci.ics.amber.core import ActorVirtualIdentity
+from proto.edu.uci.ics.amber.core import ChannelIdentity
 from ...models import InternalQueue
 
 
@@ -17,6 +17,7 @@ class PauseType(Enum):
     SCHEDULER_TIME_SLOT_EXPIRED_PAUSE = 2
     DEBUG_PAUSE = 3
     EXCEPTION_PAUSE = 4
+    MARKER_PAUSE = 5
 
 
 class PauseManager:
@@ -31,7 +32,7 @@ class PauseManager:
     ):
         self._input_queue: InternalQueue = input_queue
         self._global_pauses: Set[PauseType] = set()
-        self._specific_input_pauses: Dict[PauseType, Set[ActorVirtualIdentity]] = (
+        self._specific_input_pauses: Dict[PauseType, Set[ChannelIdentity]] = (
             defaultdict(set)
         )
         self._state_manager = state_manager
@@ -47,15 +48,19 @@ class PauseManager:
             self._state_manager.transit_to(WorkerState.PAUSED)
 
     def pause_input_channel(
-        self, pause_type: PauseType, inputs: List[ActorVirtualIdentity]
+        self, pause_type: PauseType, channel_id: ChannelIdentity
     ) -> None:
-        # for now we do not have specific data queue for Python side.
-        raise NotImplementedError()
+        self._specific_input_pauses[pause_type].add(channel_id)
+        self._input_queue.disable(channel_id)
 
     def resume(self, pause_type: PauseType, change_state=True) -> None:
         if pause_type in self._global_pauses:
             self._global_pauses.remove(pause_type)
-        # del self._specific_input_pauses[pause_type]
+        if pause_type in self._specific_input_pauses:
+            # need to resume specific input channels
+            for channel_id in self._specific_input_pauses[pause_type]:
+                self._input_queue.enable(channel_id)
+            del self._specific_input_pauses[pause_type]
 
         # still globally paused no action, don't need to resume anything
         if self._global_pauses:
@@ -67,10 +72,6 @@ class PauseManager:
             if change_state and self._state_manager.confirm_state(WorkerState.PAUSED):
                 self._state_manager.transit_to(WorkerState.RUNNING)
             return
-
-        # need to resume specific input channels
-        # currently no use case in Python side. Not implemented.
-        raise NotImplementedError()
 
     def is_paused(self) -> bool:
         return bool(self._global_pauses) and self._state_manager.confirm_state(

core/amber/src/main/python/core/architecture/packaging/input_manager.py
@@ -5,7 +5,6 @@ from core.models.internal_marker import (
     InternalMarker,
     StartOfOutputPorts,
     EndOfOutputPorts,
-    SenderChange,
     EndOfInputPort,
     StartOfInputPort,
 )
@@ -59,6 +58,9 @@ class InputManager:
         self._current_channel_id: Optional[ChannelIdentity] = None
         self.started = False
 
+    def get_all_channel_ids(self) -> Dict["ChannelIdentity", "Channel"].keys:
+        return self._channels.keys()
+
     def add_input_port(self, port_id: PortIdentity, schema: Schema) -> None:
         if port_id.id is None:
             port_id.id = 0
@@ -85,29 +87,16 @@ class InputManager:
         self._ports[port_id].add_channel(channel)
 
     def process_data_payload(
-        self, from_: ActorVirtualIdentity, payload: DataPayload
+        self, from_: ChannelIdentity, payload: DataPayload
     ) -> Iterator[Union[Tuple, InternalMarker]]:
+
+        self._current_channel_id = from_
+
         # special case used to yield for source op
-        if from_ == InputManager.SOURCE_STARTER:
+        if from_.from_worker_id == InputManager.SOURCE_STARTER:
             yield EndOfInputPort()
             yield EndOfOutputPorts()
             return
-
-        current_channel_id = next(
-            (
-                channel_id
-                for channel_id, channel in self._channels.items()
-                if channel_id.from_worker_id == from_
-            ),
-            None,
-        )
-
-        if (
-            self._current_channel_id is None
-            or self._current_channel_id != current_channel_id
-        ):
-            self._current_channel_id = current_channel_id
-            yield SenderChange(current_channel_id)
 
         if isinstance(payload, DataFrame):
             yield from self._process_data(payload.frame)

commit 183dd439db5efb722387ba467100d0924293b376

Fix Missing Dummy Input Port on Python Source Operator  (#3272)
This PR fixes a bug introduced by #3245. Since we are now forcing
channel-level message handling on Python, the source operator's
kickstart message needs a special port and channel so that the internal
processing logic can recognize it.

core/amber/src/main/python/core/architecture/handlers/control/start_worker_handler.py
@@ -1,4 +1,9 @@
-from proto.edu.uci.ics.amber.core import ChannelIdentity, ActorVirtualIdentity
+from core.models import Schema
+from proto.edu.uci.ics.amber.core import (
+    ChannelIdentity,
+    ActorVirtualIdentity,
+    PortIdentity,
+)
 from proto.edu.uci.ics.amber.engine.architecture.rpc import (
     WorkerStateResponse,
     EmptyRequest,
@@ -19,13 +24,17 @@ class StartWorkerHandler(ControlHandler):
         logger.info("Starting the worker.")
         if self.context.executor_manager.executor.is_source:
             self.context.state_manager.transit_to(WorkerState.RUNNING)
+            input_channel_id = ChannelIdentity(
+                InputManager.SOURCE_STARTER,
+                ActorVirtualIdentity(self.context.worker_id),
+                False,
+            )
+            port_id = PortIdentity(0, False)
+            self.context.input_manager.add_input_port(port_id, Schema())
+            self.context.input_manager.register_input(input_channel_id, port_id)
             self.context.input_queue.put(
                 DataElement(
-                    tag=ChannelIdentity(
-                        InputManager.SOURCE_STARTER,
-                        ActorVirtualIdentity(self.context.worker_id),
-                        False,
-                    ),
+                    tag=input_channel_id,
                     payload=None,
                 )
             )