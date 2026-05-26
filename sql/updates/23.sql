bc8381c545be7870e3f9ee5bc979ce41b0ab7b8e
Ryan Zhang <zyratlo@gmail.com>
2026-05-26 10:07:56 -0700

    remove DROP TABLE statements from sql/updates/23.sql and switch to CREATE TABLE IF NOT EXISTS

--- sql/updates/23.sql
+++ sql/updates/23.sql
@@ -23,26 +23,19 @@
 SET search_path TO texera_db;
 
 -- ============================================
--- 2. Delete tables if they already exist
+-- 2. Create the tables to store notebook and mapping
 -- ============================================
 
 BEGIN;
 
-DROP TABLE IF EXISTS notebook CASCADE;
-DROP TABLE IF EXISTS workflow_notebook_mapping CASCADE;
-
--- ============================================
--- 3. Create the tables to store notebook and mapping
--- ============================================
-
-CREATE TABLE notebook (
+CREATE TABLE IF NOT EXISTS notebook (
     nid         SERIAL  NOT NULL PRIMARY KEY,
     wid         INT     NOT NULL,
     notebook    JSONB   NOT NULL,
     FOREIGN KEY (wid) REFERENCES workflow(wid) ON DELETE CASCADE
 );
 
-CREATE TABLE workflow_notebook_mapping (
+CREATE TABLE IF NOT EXISTS workflow_notebook_mapping (
     wid         INT     NOT NULL,
     vid         INT     NOT NULL,
     nid         INT     NOT NULL,
