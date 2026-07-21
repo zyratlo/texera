da972317e4b9a83922f8902333b3ac928f96ff84
Ryan Zhang <zyratlo@gmail.com>
2026-07-21 11:04:44 -0700

    remove unused FormlyRepeatDndComponent import from mini-map

--- frontend/src/app/workspace/component/workflow-editor/mini-map/mini-map.component.ts
+++ frontend/src/app/workspace/component/workflow-editor/mini-map/mini-map.component.ts
@@ -31,7 +31,6 @@ import { NzButtonComponent } from "ng-zorro-antd/button";
 import { NzWaveDirective } from "ng-zorro-antd/core/wave";
 import { ɵNzTransitionPatchDirective } from "ng-zorro-antd/core/transition-patch";
 import { NzIconDirective } from "ng-zorro-antd/icon";
-import { FormlyRepeatDndComponent } from "../../../../common/formly/repeat-dnd/repeat-dnd.component";
 import { JupyterPanelService } from "../../../service/jupyter-panel/jupyter-panel.service";
 import { GuiConfigService } from "../../../../common/service/gui-config.service";
 
@@ -47,7 +46,6 @@ import { GuiConfigService } from "../../../../common/service/gui-config.service"
     ɵNzTransitionPatchDirective,
     NzIconDirective,
     CdkDrag,
-    FormlyRepeatDndComponent,
     CommonModule,
   ],
 })
