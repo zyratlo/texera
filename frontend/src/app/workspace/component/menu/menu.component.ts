29029186b2dc0cf8b2c39153715b516ae6ae7d87
Ryan Zhang <zyratlo@gmail.com>
2026-07-24 13:55:06 -0700

    add expand jupyter panel button to top menu bar

--- frontend/src/app/workspace/component/menu/menu.component.ts
+++ frontend/src/app/workspace/component/menu/menu.component.ts
@@ -628,6 +628,19 @@ export class MenuComponent implements OnInit, OnDestroy {
     return this.config.env.pythonNotebookMigrationEnabled;
   }
 
+  // Emits whether the current workflow has an associated Jupyter notebook, used to
+  // show the expand button only when there is a notebook to expand.
+  public get jupyterNotebookExists$() {
+    return this.jupyterPanelService.jupyterNotebookExists$;
+  }
+
+  /**
+   * Expand and redisplay the Jupyter notebook panel.
+   */
+  public onClickExpandJupyterNotebookPanel(): void {
+    this.jupyterPanelService.openJupyterNotebookPanel();
+  }
+
   openImportNotebookModal(): void {
     const models$ = this.notebookMigrationService.getAvailableModels().pipe(
       tap({
