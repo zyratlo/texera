zyratlo/texera

commit 678f1e94fc3d8318872f76daea5c18c7dc837c62

More Specific Output Types for Aggregation Operator (#1840)
This PR fixes issue #1799. 

> Currently, the types of output columns from the aggregation operator
for max, min, and sum are java.lang.Double, regardless of the types of
input columns. The aggregation operator should make the types of input
and output columns consistent for these aggregation functions.

---------

Co-authored-by: Zuozhi Wang <zuozhiw@gmail.com>

core/amber/src/main/scala/edu/uci/ics/texera/workflow/operators/aggregate/AggregationOperation.scala
@@ -6,7 +6,9 @@ import edu.uci.ics.texera.workflow.common.metadata.annotations.AutofillAttribute
 import edu.uci.ics.texera.workflow.common.operators.aggregate.DistributedAggregation
 import edu.uci.ics.texera.workflow.common.tuple.Tuple
 import edu.uci.ics.texera.workflow.common.tuple.schema.AttributeTypeUtils.parseTimestamp
-import edu.uci.ics.texera.workflow.common.tuple.schema.{Attribute, AttributeType}
+import edu.uci.ics.texera.workflow.common.tuple.schema.{Attribute, AttributeType, Schema}
+
+import java.sql.Timestamp
 
 class AggregationOperation() {
 
@@ -25,23 +27,29 @@ class AggregationOperation() {
   var resultAttribute: String = _
 
   @JsonIgnore
-  def getAggregationAttribute: Attribute = {
-    if (this.aggFunction == AggregationFunction.COUNT)
-      new Attribute(resultAttribute, AttributeType.INTEGER)
-    else if (this.aggFunction == AggregationFunction.CONCAT)
-      new Attribute(resultAttribute, AttributeType.STRING)
-    else
-      new Attribute(resultAttribute, AttributeType.DOUBLE)
+  def getAggregationAttribute(inputSchema: Schema): Attribute = {
+    val attrType = inputSchema.getAttribute(attribute).getType
+    val resultAttrType = this.aggFunction match {
+      case AggregationFunction.SUM     => attrType
+      case AggregationFunction.COUNT   => AttributeType.INTEGER
+      case AggregationFunction.AVERAGE => AttributeType.DOUBLE
+      case AggregationFunction.MIN     => attrType
+      case AggregationFunction.MAX     => attrType
+      case AggregationFunction.CONCAT  => AttributeType.STRING
+      case _                           => throw new RuntimeException("unknown agg functionL " + this.aggFunction)
+    }
+    new Attribute(resultAttribute, resultAttrType)
   }
 
   @JsonIgnore
-  def getAggFunc(): DistributedAggregation[Object] = {
+  def getAggFunc(inputSchema: Schema): DistributedAggregation[Object] = {
+    val attrType = inputSchema.getAttribute(attribute).getType
     val aggFunc = aggFunction match {
       case AggregationFunction.AVERAGE => averageAgg()
       case AggregationFunction.COUNT   => countAgg()
-      case AggregationFunction.MAX     => maxAgg()
-      case AggregationFunction.MIN     => minAgg()
-      case AggregationFunction.SUM     => sumAgg()
+      case AggregationFunction.MAX     => maxAgg(attrType)
+      case AggregationFunction.MIN     => minAgg(attrType)
+      case AggregationFunction.SUM     => sumAgg(attrType)
       case AggregationFunction.CONCAT  => concatAgg()
       case _ =>
         throw new UnsupportedOperationException("Unknown aggregation function: " + aggFunction)
@@ -49,15 +57,24 @@ class AggregationOperation() {
     aggFunc.asInstanceOf[DistributedAggregation[Object]]
   }
 
-  private def sumAgg(): DistributedAggregation[java.lang.Double] = {
-    new DistributedAggregation[java.lang.Double](
-      () => 0,
+  private def sumAgg(attributeType: AttributeType): DistributedAggregation[Object] = {
+    if (
+      attributeType != AttributeType.INTEGER &&
+      attributeType != AttributeType.DOUBLE &&
+      attributeType != AttributeType.LONG &&
+      attributeType != AttributeType.TIMESTAMP
+    ) {
+      throw new UnsupportedOperationException(
+        "Unsupported attribute type for sum aggregation: " + attributeType
+      )
+    }
+    new DistributedAggregation[Object](
+      () => zero(attributeType),
       (partial, tuple) => {
-        val value = getNumericalValue(tuple)
-        partial + (if (value.isDefined) value.get else 0)
-
+        val value = tuple.getField[Object](attribute)
+        add(partial, value, attributeType)
       },
-      (partial1, partial2) => partial1 + partial2,
+      (partial1, partial2) => add(partial1, partial2, attributeType),
       partial => partial
     )
   }
@@ -100,27 +117,51 @@ class AggregationOperation() {
     )
   }
 
-  private def minAgg(): DistributedAggregation[java.lang.Double] = {
-    new DistributedAggregation[java.lang.Double](
-      () => Double.MaxValue,
+  private def minAgg(attributeType: AttributeType): DistributedAggregation[Object] = {
+    if (
+      attributeType != AttributeType.INTEGER &&
+      attributeType != AttributeType.DOUBLE &&
+      attributeType != AttributeType.LONG &&
+      attributeType != AttributeType.TIMESTAMP
+    ) {
+      throw new UnsupportedOperationException(
+        "Unsupported attribute type for min aggregation: " + attributeType
+      )
+    }
+    new DistributedAggregation[Object](
+      () => maxValue(attributeType),
       (partial, tuple) => {
-        val value = getNumericalValue(tuple)
-        if (value.isDefined && value.get < partial) value.get else partial
+        val value = tuple.getField[Object](attribute)
+        val comp = compare(value, partial, attributeType)
+        if (value != null && comp < 0) value else partial
       },
-      (partial1, partial2) => if (partial1 < partial2) partial1 else partial2,
-      partial => if (partial == Double.MaxValue) null else partial
+      (partial1, partial2) =>
+        if (compare(partial1, partial2, attributeType) < 0) partial1 else partial2,
+      partial => if (partial == maxValue(attributeType)) null else partial
     )
   }
 
-  private def maxAgg(): DistributedAggregation[java.lang.Double] = {
-    new DistributedAggregation[java.lang.Double](
-      () => Double.MinValue,
+  private def maxAgg(attributeType: AttributeType): DistributedAggregation[Object] = {
+    if (
+      attributeType != AttributeType.INTEGER &&
+      attributeType != AttributeType.DOUBLE &&
+      attributeType != AttributeType.LONG &&
+      attributeType != AttributeType.TIMESTAMP
+    ) {
+      throw new UnsupportedOperationException(
+        "Unsupported attribute type for max aggregation: " + attributeType
+      )
+    }
+    new DistributedAggregation[Object](
+      () => minValue(attributeType),
       (partial, tuple) => {
-        val value = getNumericalValue(tuple)
-        if (value.isDefined && value.get > partial) value.get else partial
+        val value = tuple.getField[Object](attribute)
+        val comp = compare(value, partial, attributeType)
+        if (value != null && comp > 0) value else partial
       },
-      (partial1, partial2) => if (partial1 > partial2) partial1 else partial2,
-      partial => if (partial == Double.MinValue) null else partial
+      (partial1, partial2) =>
+        if (compare(partial1, partial2, attributeType) > 0) partial1 else partial2,
+      partial => if (partial == maxValue(attributeType)) null else partial
     )
   }
 
@@ -152,5 +193,94 @@ class AggregationOperation() {
       }
     )
   }
+
+  // return a.compare(b),
+  // < 0 if a < b,
+  // > 0 if a > b,
+  //   0 if a = b
+  private def compare(a: Object, b: Object, attributeType: AttributeType): Int = {
+    if (a == null && b == null) {
+      return 0
+    } else if (a == null) {
+      return -1
+    } else if (b == null) {
+      return 1
+    }
+    attributeType match {
+      case AttributeType.INTEGER => a.asInstanceOf[Integer].compareTo(b.asInstanceOf[Integer])
+      case AttributeType.DOUBLE =>
+        a.asInstanceOf[java.lang.Double].compareTo(b.asInstanceOf[java.lang.Double])
+      case AttributeType.LONG =>
+        a.asInstanceOf[java.lang.Long].compareTo(b.asInstanceOf[java.lang.Long])
+      case AttributeType.TIMESTAMP =>
+        a.asInstanceOf[Timestamp].getTime.compareTo(b.asInstanceOf[Timestamp].getTime)
+      case _ =>
+        throw new UnsupportedOperationException(
+          "Unsupported attribute type for comparison: " + attributeType
+        )
+    }
+  }
+
+  private def add(a: Object, b: Object, attributeType: AttributeType): Object = {
+    if (a == null && b == null) {
+      return zero(attributeType)
+    } else if (a == null) {
+      return b
+    } else if (b == null) {
+      return a
+    }
+    attributeType match {
+      case AttributeType.INTEGER =>
+        Integer.valueOf(a.asInstanceOf[Integer] + b.asInstanceOf[Integer])
+      case AttributeType.DOUBLE =>
+        java.lang.Double.valueOf(
+          a.asInstanceOf[java.lang.Double] + b.asInstanceOf[java.lang.Double]
+        )
+      case AttributeType.LONG =>
+        java.lang.Long.valueOf(a.asInstanceOf[java.lang.Long] + b.asInstanceOf[java.lang.Long])
+      case AttributeType.TIMESTAMP =>
+        new Timestamp(a.asInstanceOf[Timestamp].getTime + b.asInstanceOf[Timestamp].getTime)
+      case _ =>
+        throw new UnsupportedOperationException(
+          "Unsupported attribute type for addition: " + attributeType
+        )
+    }
+  }
+
+  def zero(attributeType: AttributeType): Object =
+    attributeType match {
+      case AttributeType.INTEGER   => java.lang.Integer.valueOf(0)
+      case AttributeType.DOUBLE    => java.lang.Double.valueOf(0)
+      case AttributeType.LONG      => java.lang.Long.valueOf(0)
+      case AttributeType.TIMESTAMP => new Timestamp(0)
+      case _ =>
+        throw new UnsupportedOperationException(
+          "Unsupported attribute type for zero value: " + attributeType
+        )
+    }
+
+  def maxValue(attributeType: AttributeType): Object =
+    attributeType match {
+      case AttributeType.INTEGER   => Integer.MAX_VALUE.asInstanceOf[Object]
+      case AttributeType.DOUBLE    => java.lang.Double.MAX_VALUE.asInstanceOf[Object]
+      case AttributeType.LONG      => java.lang.Long.MAX_VALUE.asInstanceOf[Object]
+      case AttributeType.TIMESTAMP => new Timestamp(java.lang.Long.MAX_VALUE)
+      case _ =>
+        throw new UnsupportedOperationException(
+          "Unsupported attribute type for max value: " + attributeType
+        )
+    }
+
+  def minValue(attributeType: AttributeType): Object =
+    attributeType match {
+      case AttributeType.INTEGER   => Integer.MIN_VALUE.asInstanceOf[Object]
+      case AttributeType.DOUBLE    => java.lang.Double.MIN_VALUE.asInstanceOf[Object]
+      case AttributeType.LONG      => java.lang.Long.MIN_VALUE.asInstanceOf[Object]
+      case AttributeType.TIMESTAMP => new Timestamp(0)
+      case _ =>
+        throw new UnsupportedOperationException(
+          "Unsupported attribute type for min value: " + attributeType
+        )
+    }
 
 }

core/amber/src/main/scala/edu/uci/ics/texera/workflow/operators/aggregate/SpecializedAggregateOpDesc.scala
@@ -38,7 +38,7 @@ class SpecializedAggregateOpDesc extends AggregateOpDesc {
     }
     AggregateOpDesc.opExecPhysicalPlan(
       operatorIdentifier,
-      aggregations.map(agg => agg.getAggFunc()),
+      aggregations.map(agg => agg.getAggFunc(operatorSchemaInfo.inputSchemas(0))),
       groupByKeys,
       operatorSchemaInfo
     )
@@ -51,13 +51,6 @@ class SpecializedAggregateOpDesc extends AggregateOpDesc {
     Schema
       .newBuilder()
       .add(groupByKeys.map(key => schemas(0).getAttribute(key)).toArray: _*)
-      .build()
-  }
-
-  private def getFinalAggValueSchema: Schema = {
-    Schema
-      .newBuilder()
-      .add(aggregations.map(_.getAggregationAttribute).asJava)
       .build()
   }
 
@@ -79,7 +72,7 @@ class SpecializedAggregateOpDesc extends AggregateOpDesc {
     Schema
       .newBuilder()
       .add(getGroupByKeysSchema(schemas).getAttributes)
-      .add(getFinalAggValueSchema.getAttributes)
+      .add(aggregations.map(agg => agg.getAggregationAttribute(schemas(0))).asJava)
       .build()
   }
 

commit b0dd3ffc5a2356b9d2c62626d5e6e8a407c60f74

fix(workflow-operator): MAX aggregation checks the wrong empty-group sentinel (#7532)
### What changes were proposed in this PR?

One-line fix in `AggregationOperation.scala`: the `max` aggregation
starts its running maximum from the type's minimum value, so its final
"was this group empty?" check must compare against that same minimum
value, but it compared against the type's maximum value instead (the
line was copied from `min`, where that comparison is correct because
`min` starts from the maximum).

Because the check looked at the wrong sentinel, two results were
silently wrong while the workflow completed with no error:

- A group with only null values returned the sentinel itself instead of
`null`, e.g. `-2147483648` for INTEGER or `1970-01-01 00:00:00` for
TIMESTAMP.
- A true maximum equal to the type's maximum value was mistaken for "no
value seen" and discarded, so `max` over `{1, 5, 2147483647}` reported
`5` (the largest value from the other local aggregation worker) instead
of `2147483647`.

#### Before-and-after

Test data: group `g1` has only nulls, `g2` contains `{1, 5,
2147483647}`, `g3` contains `{10, 42}`.

<img width="1347" height="900" alt="Screenshot 2026-08-07 at 2 27 40 PM"
src="https://github.com/user-attachments/assets/faac65fd-e83f-4952-a0dd-b8109d56334d"
/>

Before the fix, `max(v)` grouped by `k` returns `-2147483648` for `g1`
(expected `null`) and `5` for `g2` (expected `2147483647`):

<img width="1341" height="866" alt="Screenshot 2026-08-07 at 2 27 51 PM"
src="https://github.com/user-attachments/assets/fdf7f045-aa34-44eb-adee-6e722839d7eb"
/>

`min(v)` on the same data is correct (`null` for `g1`, `1` for `g2`,
`10` for `g3`), confirming only `max`'s empty-group check is broken:

<img width="1342" height="868" alt="Screenshot 2026-08-07 at 2 28 00 PM"
src="https://github.com/user-attachments/assets/3df9c390-626f-4f01-9ca6-7e634927386b"
/>

After the fix, `max(v)` returns `null` for `g1`, `2147483647` for `g2`,
and `42` for `g3`:

<img width="1131" height="756" alt="Screenshot 2026-08-07 at 5 54 17 PM"
src="https://github.com/user-attachments/assets/68ba757c-8b4b-4682-b04e-8db740d1583c"
/>

### Any related issues, documentation, discussions?

Closes #7531

The regression was introduced by #1840, which generalised the hard-coded

common/workflow-operator/src/main/scala/org/apache/texera/amber/operator/aggregate/AggregationOperation.scala
@@ -237,7 +237,7 @@ class AggregationOperation {
       (partial1, partial2) =>
         if (AttributeTypeUtils.compare(partial1, partial2, attributeType) > 0) partial1
         else partial2,
-      partial => if (partial == AttributeTypeUtils.maxValue(attributeType)) null else partial
+      partial => if (partial == AttributeTypeUtils.minValue(attributeType)) null else partial
     )
   }
 

common/workflow-operator/src/test/scala/org/apache/texera/amber/operator/aggregate/AggregateOpSpec.scala
@@ -23,6 +23,8 @@ import org.apache.texera.amber.core.tuple.{Attribute, AttributeType, Schema, Tup
 import org.apache.texera.amber.util.JSONUtils.objectMapper
 import org.scalatest.funsuite.AnyFunSuite
 
+import java.sql.Timestamp
+
 class AggregateOpSpec extends AnyFunSuite {
 
   /** Helpers */
@@ -436,6 +438,67 @@ class AggregateOpSpec extends AnyFunSuite {
 
     val result = agg.finalAgg(partial).asInstanceOf[java.lang.Double].doubleValue()
     assert(result == maxValue)
+  }
+
+  test("MAX aggregation finds largest INTEGER and returns null when given no values") {
+    val schema = makeSchema("temperature" -> AttributeType.INTEGER)
+    val tuple1 = makeTuple(schema, 10)
+    val tuple2 = makeTuple(schema, -2)
+    val tuple3 = makeTuple(schema, 5)
+
+    val operation = makeAggregationOp(AggregationFunction.MAX, "temperature", "max_temp")
+    val agg = operation.getAggFunc(AttributeType.INTEGER)
+
+    // Empty case: never iterate, just finalize init
+    val emptyPartial = agg.init()
+    val emptyResult = agg.finalAgg(emptyPartial)
+    assert(emptyResult == null)
+
+    // Non-empty case
+    var partial = agg.init()
+    partial = agg.iterate(partial, tuple1)
+    partial = agg.iterate(partial, tuple2)
+    partial = agg.iterate(partial, tuple3)
+
+    val result = agg.finalAgg(partial).asInstanceOf[Number].intValue()
+    assert(result == 10)
+  }
+
+  test("MAX aggregation returns null when all values are null") {
+    val schema = makeSchema("temperature" -> AttributeType.INTEGER)
+
+    val operation = makeAggregationOp(AggregationFunction.MAX, "temperature", "max_temp")
+    val agg = operation.getAggFunc(AttributeType.INTEGER)
+
+    var partial = agg.init()
+    Seq.fill(3)(makeTuple(schema, null)).foreach(tp => partial = agg.iterate(partial, tp))
+    assert(agg.finalAgg(partial) == null)
+  }
+
+  test("MAX aggregation keeps the type's maximum value when it is the true maximum") {
+    // Regression: the finalizer used to mistake a partial equal to
+    // maxValue(attributeType) for the "no value seen" sentinel and emit null.
+    val cases: Seq[(AttributeType, Seq[Any], Any)] = Seq(
+      (AttributeType.INTEGER, Seq(1, 5, Int.MaxValue), Int.MaxValue),
+      (AttributeType.LONG, Seq(1L, 5L, Long.MaxValue), Long.MaxValue),
+      (AttributeType.DOUBLE, Seq(1.0, 5.0, Double.MaxValue), Double.MaxValue),
+      (
+        AttributeType.TIMESTAMP,
+        Seq(new Timestamp(1L), new Timestamp(Long.MaxValue)),
+        new Timestamp(Long.MaxValue)
+      )
+    )
+
+    for ((attrType, values, expected) <- cases) {
+      val schema = makeSchema("v" -> attrType)
+      val operation = makeAggregationOp(AggregationFunction.MAX, "v", "max_v")
+      val agg = operation.getAggFunc(attrType)
+
+      var partial = agg.init()
+      values.foreach(v => partial = agg.iterate(partial, makeTuple(schema, v)))
+
+      assert(agg.finalAgg(partial) == expected, s"MAX over $attrType must keep $expected")
+    }
   }
 
   test("AVERAGE aggregation ignores nulls and returns null when all values are null") {

common/workflow-operator/src/test/scala/org/apache/texera/amber/operator/aggregate/AggregationOperationSpec.scala
@@ -175,6 +175,35 @@ class AggregationOperationSpec extends AnyFlatSpec {
     assert(finalSum == 36, "single-pass SUM(1+2+3+10+20) == 36")
   }
 
+  it should
+    "keep the type's maximum value when partial MAX results are re-aggregated via getFinal" in {
+    // Regression: the local stage used to finalize a partial equal to
+    // maxValue(attributeType) to null, so the final stage reported the largest
+    // value from the other worker instead of the true maximum.
+    val workerOp = op(AggregationFunction.MAX, attribute = "v", resultAttribute = "max_v")
+    val workerAgg = workerOp.getAggFunc(AttributeType.INTEGER)
+
+    val w1State = Seq[AnyRef](Int.box(1), Int.box(Int.MaxValue))
+      .map(v => tupleOf("v", AttributeType.INTEGER, v))
+      .foldLeft(workerAgg.init())(workerAgg.iterate)
+    val w1Out = workerAgg.finalAgg(w1State)
+    assert(w1Out == Int.box(Int.MaxValue), "worker 1's true maximum is Int.MaxValue")
+
+    val w2State = Seq[AnyRef](Int.box(5), Int.box(2))
+      .map(v => tupleOf("v", AttributeType.INTEGER, v))
+      .foldLeft(workerAgg.init())(workerAgg.iterate)
+    val w2Out = workerAgg.finalAgg(w2State)
+    assert(w2Out == Int.box(5))
+
+    val finalOp = workerOp.getFinal
+    assert(finalOp.aggFunction == AggregationFunction.MAX)
+    val finalAgg = finalOp.getAggFunc(AttributeType.INTEGER)
+    val finalState = Seq(w1Out, w2Out)
+      .map(p => tupleOf("max_v", AttributeType.INTEGER, p))
+      .foldLeft(finalAgg.init())(finalAgg.iterate)
+    assert(finalAgg.finalAgg(finalState) == Int.box(Int.MaxValue))
+  }
+
   // --- AveragePartialObj -----------------------------------------------------
 
   "AveragePartialObj" should "expose its sum and count fields and support value equality" in {
@@ -336,6 +365,17 @@ class AggregationOperationSpec extends AnyFlatSpec {
     val seen = agg.iterate(agg.init(), tupleOf("v", AttributeType.INTEGER, Int.box(7)))
 
     // An empty partial is the sentinel maxValue, so it must lose the comparison.
+    assert(agg.finalAgg(agg.merge(seen, agg.init())) == Int.box(7))
+    assert(agg.finalAgg(agg.merge(agg.init(), seen)) == Int.box(7))
+    // Two empty partials still finalize to null (no rows anywhere).
+    assert(agg.finalAgg(agg.merge(agg.init(), agg.init())) == null)
+  }
+
+  "MAX aggregation merge" should "stay neutral when one side saw no values" in {
+    val agg = op(AggregationFunction.MAX).getAggFunc(AttributeType.INTEGER)
+    val seen = agg.iterate(agg.init(), tupleOf("v", AttributeType.INTEGER, Int.box(7)))
+
+    // An empty partial is the sentinel minValue, so it must lose the comparison.
     assert(agg.finalAgg(agg.merge(seen, agg.init())) == Int.box(7))
     assert(agg.finalAgg(agg.merge(agg.init(), seen)) == Int.box(7))
     // Two empty partials still finalize to null (no rows anywhere).